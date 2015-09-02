b32c96b9c8adf82870480b08a8f0d87476fd8a03
root <root@a33f24ba-b25e-4947-9187-be5d4a415dca>
2015-09-02 14:10:53 +0000

    Inicio repositorio Proyecto SIMWebPlus

---
+++ simwebpluscar/trunk/backend/controllers/UnidadDepartamentoController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use backend\models\UnidadDepartamento;
+use backend\models\UnidadDepartamentoSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * UnidadDepartamentoController implements the CRUD actions for UnidadDepartamento model.
+ */
+class UnidadDepartamentoController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all UnidadDepartamento models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new UnidadDepartamentoSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single UnidadDepartamento model.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new UnidadDepartamento model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new UnidadDepartamento();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id_unidad]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing UnidadDepartamento model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id_unidad]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing UnidadDepartamento model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the UnidadDepartamento model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $id
+     * @return UnidadDepartamento the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = UnidadDepartamento::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+
+
+    public function actionLists($id)
+    {
+       // die('hola, entro a list');
+        $countUnidad = UnidadDepartamento::find()->where(['id_departamento' => $id, 'inactivo' => 0])->count();
+
+        $unidades = UnidadDepartamento::find()->where(['id_departamento' => $id, 'inactivo' => 0])->all();
+
+        if ( $countUnidad > 0 ) {
+            foreach ($unidades as $unidad) {
+                echo "<option value='" . $unidad->id_unidad . "'>" . $unidad->descripcion . "</option>";
+            }
+        } else {
+            echo "<option> - </option>";
+        }
+    }
+
+}
---
+++ simwebpluscar/trunk/common/config/main-local.php
@@ -0,0 +1,58 @@
+<?php
+return [
+	'components' => [
+		'db'        => [
+			'class'    => 'yii\db\Connection',
+			//'dsn' => 'mysql:host=localhost;dbname=sim_teq',
+			'dsn' => 'mysql:host=192.168.30.7;dbname=sim_car_plus',
+			//'dsn' => 'mysql:host=pinky.ddns.net;dbname=sim_car',
+			'username' => 'adminyii',
+			'password' => 'adminyii2015',
+			//'username' => 'jperez',
+			//'password' => 'jperez',
+			//'username' => 'root',
+			//'password' => '',
+			'charset'  => 'utf8',
+		],
+		'dbsim' => [
+			'class'    => 'yii\db\Connection',
+			//'dsn' => 'mysql:host=localhost;dbname=sim_teq',
+			'dsn' => 'mysql:host=192.168.30.7;dbname=sim_car_plus',
+			//'dsn' => 'mysql:host=pinky.ddns.net;dbname=sim_car',
+			'username' => 'adminyii',
+			'password' => 'adminyii2015',
+			//'username' => 'jperez',
+			//'password' => 'jperez',
+			//'username' => 'root',
+			//'password' => '',
+			'charset'  => 'utf8',	
+		],
+		'dbswp'  => [
+			'class'    => 'yii\db\Connection',
+			//'dsn' => 'mysql:host=localhost;dbname=sim_teq',
+			'dsn' => 'mysql:host=192.168.30.7;dbname=sim_car_plus',
+			//'dsn' => 'mysql:host=pinky.ddns.net;dbname=sim_car',
+			'username' => 'adminyii',
+			'password' => 'adminyii2015',
+			//'username' => 'jperez',
+			//'password' => 'jperez',
+			//'username' => 'root',
+			//'password' => '',
+			'charset'  => 'utf8',
+		],
+		'mailer'    => [
+			'class'    => 'yii\swiftmailer\Mailer',
+			'viewPath' => '@common/mail',
+			// send all mails to a file by default. You have to set
+			// 'useFileTransport' to false and configure a transport
+			// for the mailer to send real emails.
+			'useFileTransport' => true,
+		],
+		'ente'   => [
+			'class' => 'common\classes\Ente'
+		],
+		'oficina' => [
+			'class'  => 'common\classes\Oficina'
+		],
+	],
+];
