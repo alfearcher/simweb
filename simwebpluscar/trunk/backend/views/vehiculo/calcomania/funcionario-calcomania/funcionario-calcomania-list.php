347cf7049c640748912f62e5f3ad8116d93b055c
hanselc <hanselc@a33f24ba-b25e-4947-9187-be5d4a415dca>
2015-09-18 21:34:41 +0000

    PARA TERMINAR DE DESARROLLA EL FIN DE SEMANA

--- simwebpluscar/trunk/backend/views/vehiculo/calcomania/funcionario-calcomania/funcionario-calcomania-list.php
+++ simwebpluscar/trunk/backend/views/vehiculo/calcomania/funcionario-calcomania/funcionario-calcomania-list.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\db\DataReader;
+use yii\data\ActiveDataProvider;
 use backend\models\vehiculo\calcomania\FuncionarioCalcomaniaSearch;
 
 /* @var $this yii\web\View */
@@ -11,48 +13,61 @@ use backend\models\vehiculo\calcomania\FuncionarioCalcomaniaSearch;
 // $this->title = Yii::t('backend', 'Funcionario Calcomania Forms');
 // $this->params['breadcrumbs'][] = $this->title;
 ?>
+<!-- VARIABLE QUE MANEJA EL MENSAJE DE ERROR -->
+<?= $msg ?>
 <div class="funcionario-calcomania-form-index">
+    <?= Html::beginForm( [ 'vehiculo/calcomania/funcionario-calcomania/automatic' ], 'post' );?>
+        <h1><?= Html::encode($this->title) ?></h1>
+        <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+        <p>
+            <?php //= Html::a(Yii::t('backend', 'Create Funcionario Calcomania Form'), ['create'], ['class' => 'btn btn-success']) ?>
+        </p>
 
-    <p>
-        <?php //= Html::a(Yii::t('backend', 'Create Funcionario Calcomania Form'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+        <p>
+            <?= Html::submitButton( 'Automatic Allocation', [ 'class' => 'btn btn-primary', 'name' => 'btn', 'id' => "btn" ] );?>
+        </p>
 
-    <p>
-        <?= Html::a(Yii::t('backend', 'Automatic Allocation'), ['automaticAllocation'], ['class' => 'btn btn-primary']) ?>
-    </p>
+        <div>
+            <?php
+                // echo "<pre>"; var_dump($dataProvider); echo "</pre>";
+            ?>        
+        </div>
 
-    <div>
-        <?php
-            // echo "<pre>"; var_dump($model[0]); echo "</pre>";
-        ?>        
-    </div>
+        <div class="container">        
+            <div class="container-fluid" style="width:1024px">
+                <div class="panel-body" >
+                 <?= GridView::widget([
+                            'dataProvider' => $dataProvider,
+                            'layout' => '{items}',
+                            'columns' => [
+                                ['class' => 'yii\grid\CheckboxColumn'],
+                                ['class' => 'yii\grid\SerialColumn'],
+                                'id_funcionario',          
+                                'funcionarioApellido',
+                                'funcionarioName',
+                                ['label' => 'Inicio',
+                                    'value' => function($data) {
+                                            return $data->distribucionRangoInicial;
+                                    }
+                                ],
+                                ['label' => 'Fin',
+                                    'value' => function($data) {
+                                            return $data->distribucionRangoFinal;
+                                    }
+                                ],
+                                ['label' => 'ci',
+                                    'value' => function($data) {
+                                            return $data->naturaleza .'-'. $data->ci;
+                                    }
+                                ],
 
-    <div class="container">        
-        <div class="container-fluid" style="width:1024px">
-            <div class="panel-body" >
-             <?= GridView::widget([
-                        'dataProvider' => $dataProvider,
-                        'columns' => [
-                            ['class' => 'yii\grid\CheckboxColumn'],
-                            ['class' => 'yii\grid\SerialColumn'],
-                            'id_funcionario',                            
-                            'funcionarioApellido',
-                            'funcionarioName',
-                            // 'loteCalcomaniaRangoInicial',
-                            ['label' => 'ci',
-                                'value' => function($data) {
-                                        return $data->naturaleza .'-'. $data->ci;
-                                }
+                                ['class' => 'yii\grid\ActionColumn','template' => '{view} {update}'],
                             ],
-
-                            ['class' => 'yii\grid\ActionColumn','template' => '{view} {update}'],
-                        ],
-                    ]); ?>
-                
+                        ]); ?>
+                    
+                </div>
             </div>
         </div>
-    </div>
+    <?= Html::endForm();?> 
 </div>
