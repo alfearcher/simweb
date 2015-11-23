8442753c35a2defdd34c8375e7991a56043ce734
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2015-11-23 04:32:46 +0000

    modelo base de sucursal

---
+++ simwebpluscar/trunk/common/models/aaee/Sucursal.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file Sucursal.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 19-09-2015
+ *
+ *  @class Sucursal
+ *  @brief Clase Modelo que maneja la politica
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *  @inherits
+ *
+ */
+
+	namespace common\models\aaee;
+
+ 	use Yii;
+	use yii\base\Model;
+	use yii\db\ActiveRecord;
+
+	/**
+	* 	Clase
+	*/
+	class Sucursal extends ActiveRecord
+	{
+
+		/**
+		 *	Metodo que retorna el nombre de la base de datos donde se tiene la conexion actual.
+		 * 	Utiliza las propiedades y metodos de Yii2 para traer dicha informacion.
+		 * 	@return Nombre de la base de datos
+		 */
+		public static function getDb()
+		{
+			return Yii::$app->db;
+		}
+
+
+		/**
+		 * 	Metodo que retorna el nombre de la tabla que utiliza el modelo.
+		 * 	@return Nombre de la tabla del modelo.
+		 */
+		public static function tableName()
+		{
+			return 'contribuyentes';
+		}
+
+
+	}
+
+?>
