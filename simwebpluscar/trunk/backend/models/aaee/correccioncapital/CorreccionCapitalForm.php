597f914b272474a7cf66d28099d0c7c5f2d0b0de
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2015-12-03 21:58:14 +0000

    modelo de correccion de capital

---
+++ simwebpluscar/trunk/backend/models/aaee/correccioncapital/CorreccionCapitalForm.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file CorreccionDomicilioFiscalForm.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 20-11-2015
+ *
+ *  @class CorreccionDomicilioFiscalForm
+ *  @brief Clase Modelo del formulario
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *  @inherits
+ *
+ */
+
+ 	namespace backend\models\aaee\correccioncapital;
+
+ 	use Yii;
+	use yii\base\Model;
+	use yii\data\ActiveDataProvider;
+	use backend\models\aaee\correccioncapital\CorreccionCapital;
+	use common\models\aaee\Sucursal;
+
+
+	/**
+	 * Clase principal
+	 */
+	class CorreccionCapitalForm extends CorreccionCapital
+	{
+		public $id_correcion;
+		public $nro_solicitud;
+		public $id_contribuyente;
+		public $capital_v;
+		public $capital_new;
+		public $fecha_hora;
+		public $usuario;
+		public $estatus;
+		public $origen;
+
+		public $naturaleza;
+		public $cedula;
+		public $tipo;
+
+
+		/**
+     	* @inheritdoc
+     	*/
+    	public function scenarios()
+    	{
+        	// bypass scenarios() implementation in the parent class
+        	return Model::scenarios();
+    	}
+
+
+
+    	/**
+    	 * [rules description]
+    	 * @return [type] [description]
+    	 */
+    	public function rules()
+    	{
+    		return [
+    			[['capital_new', 'id_contribuyente'],'required','message' => Yii::t('backend', '{attribute} is required')],
+    			//[['capital_new'], 'double', 'min' => 'capital_v', 'message' => Yii::t('backend', '{attribute} must be no less that 100')],
+    			//[['capital_new'], 'compare', 'compareValue' => 'capital_v', 'operator' => '>=', 'message' => Yii::t('backend', '{attribute} must be no less that ')],
+    			//['capital_new', 'format', Yii::$app->formatted->asDecimal($model->)]
+    		];
+    	}
+
+
+
+
+    	/**
+	    * Lista de atributos con sus respectivas etiquetas (labels), las cuales son las que aparecen en las vistas
+	    * @return returna arreglo de datos con los atributoe como key y las etiquetas como valor del arreglo.
+	    */
+	    public function attributeLabels()
+	    {
+	        return [
+	        	'id_correccion' => Yii::t('backend', 'Id. Record'),
+	            'id_contribuyente' => Yii::t('backend', 'Id. Taxpayer'),
+	            'nro_solicitud' => Yii::t('backend', 'Application Number'),
+	            'capital_v' => Yii::t('backend', 'Current Capital'),
+	            'capital_new' => Yii::t('backend', 'New Capital'),
+
+	        ];
+	    }
+
+
+	    	    /**
+	     * Metodo que retorna un dataProvider de los contribuyentes asociados a un rif (sucursales)
+	     * @param  [type] $naturalezaLocal [description]
+	     * @param  [type] $cedulaLocal     [description]
+	     * @param  [type] $tipoLocal       [description]
+	     * @return [type]                  [description]
+	     */
+	    public function getDataProviderSucursalesSegunRif($naturalezaLocal, $cedulaLocal, $tipoLocal, $tipoNaturalezaLocal)
+	    {
+	    	if ( trim($naturalezaLocal) != '' && $cedulaLocal > 0 ) {
+	    		if ( strlen($naturalezaLocal) == 1 ) {
+	    			$query = Sucursal::find();
+	    			$dataProvider = new ActiveDataProvider([
+	            		'query' => $query,
+	        		]);
+	    			$query->where('naturaleza =:naturaleza and cedula =:cedula and tipo =:tipo and tipo_naturaleza =:tipo_naturaleza and inactivo =:inactivo',[':naturaleza' => $naturalezaLocal,
+	    															':cedula' => $cedulaLocal,
+	    															':tipo' => $tipoLocal,
+	    															':tipo_naturaleza' => $tipoNaturalezaLocal,
+	    															':inactivo'=> '0'])->all();
+
+	    			return $dataProvider;
+	    		}
+	    	}
+	    	return false;
+	    }
+
+
+
+	   	/**
+	     * Metodo que retorna un dataProvider, recibiendo como parametro un arreglo de id contribuyentes.
+	     * @param $arrayIdContribuyente, array de id contribuyentes,
+	     * @return retorna un dataProvider.
+	     */
+	    public function getDataProviderSucursalesSegunId($arrayIdContribuyente = [])
+	    {
+	    	if ( is_array($arrayIdContribuyente) ) {
+		    	if ( count($arrayIdContribuyente) > 0 ) {
+		    		$query = Sucursal::find();
+		    		$dataProvider = new ActiveDataProvider([
+		            	'query' => $query,
+		        	]);
+		        	$query->where(['in', 'id_contribuyente', $arrayIdContribuyente]);
+
+		        	return $dataProvider;
+		    	}
+		    }
+		    return false;
+	    }
+
+
+
+
+
+	    /**
+	     * Metodo que genera el dataprovider de las correcciones realizadas.
+	     * @param $arrayIdCorreccion, array de id correccion guardados en el proceso.
+	     * @return returna un dataProvider o false sino logra crear la instancia.
+	     */
+	    public function getDataProviderCorreccionesCapital($arrayIdCorreccion = [])
+	    {
+	    	if ( is_array($arrayIdCorreccion) ) {
+		    	if ( count($arrayIdCorreccion) > 0 ) {
+		    		$query = CorreccionCapital::find();
+		    		$dataProvider = new ActiveDataProvider([
+		            	'query' => $query,
+		        	]);
+		        	$query->where(['in', 'id_correccion', $arrayIdCorreccion]);
+
+		        	return $dataProvider;
+		    	}
+		    }
+		    return false;
+	    }
+
+
+	}
+
+?>
