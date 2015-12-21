1c50669832f636766627298d000975368f38f2d7
ronnys <ronnys@a33f24ba-b25e-4947-9187-be5d4a415dca>
2015-12-21 18:49:23 +0000

    actualizando loggin frontend

--- simwebpluscar/trunk/frontend/views/site/login.php
+++ simwebpluscar/trunk/frontend/views/site/login.php
@@ -4,29 +4,62 @@ use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $model \common\models\LoginForm */
+/* @var $model app\models\LoginForm */
 
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Iniciar Sesion';
+//$this->params['breadcrumbs'][] = $this->title; // guia de la direccion de la pagina ejemplo: /home/registrar
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                </div>
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
-</div>
+    
+
+    <?php $form = ActiveForm::begin([
+        'id' => 'login-form', 
+        'options' => ['class' => 'form-horizontal'], // aqui estan las forma que tendra el formulario en la vista
+        'fieldConfig' => [
+            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+            'labelOptions' => ['class' => 'col-lg-1 control-label'], // aqui estan las obciones para el formulario de logueo
+        ],
+    ]); ?>
+
+
+<div class="col-sm-10">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<?= $this->title ?>
+			</div>
+			<div class="panel-body" >
+				<table class="table table-striped">
+					<tr>
+						<td>
+                            <?= $form->field($model, 'username') ?>
+							
+						 </td>
+				   </tr>
+				   <tr>
+						<td>
+                            <?= $form->field($model, 'password')->passwordInput() ?>
+							
+						 </td>
+				   </tr>
+				   
+				   <tr>
+						<td><div class="col-lg-offset-0 col-lg-5">
+                            <?= Html::submitButton('Iniciar Sesion', ['class' => 'btn btn-primary', 'name' => 'login-button']) // BOTON DE INICIAR SESION ?>
+							
+							<?= Html::a('Recuperar Password', ['opcion-funcionario/iniciarrecuperacionpasswordfuncionario'], ['class' => 'btn btn-primary']) //BOTON DE RECUPERAR CONTRASENA ?>
+                            </div>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+  </div>	
+
+
+    <?php ActiveForm::end(); ?>
+	
+	 <?php // <?= $form->field($model, 'rememberMe', ['template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",])->checkbox() ?>
+	                        
+						
