6796f85e8d302017fe339d0876aa9f8bd30bf17c
ronnys <ronnys@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-01-07 19:19:40 +0000

    carpeta para enviar email

---
+++ simwebpluscar/trunk/common/enviaremail/EnviarEmail.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ * 
+ *  > This library is free software; you can redistribute it and/or modify it under 
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free 
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion) 
+ *  > any later version.
+ *  > 
+ *  > This library is distributed in the hope that it will be usefull, 
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability 
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence 
+ *  > for more details.
+ *  > 
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**    
+ *  @file CrearUsuarioJuridico.php
+ *  
+ *  @author Manuel Alejandro Zapata Canelon
+ * 
+ *  @date 04/01/2016
+ * 
+ *  @class CrearUsuarioJuridico
+ *  @brief Modelo que instancia la conexion a base de datos para buscar datos en contribuyentes. 
+ * 
+ *  
+ * 
+ *  
+ *  
+ *  @property
+ *
+ *  
+ *  @method
+ * 
+ *  getDb
+ *  tableName
+ *  
+ *
+ *  @inherits
+ *  
+ */ 
+
+namespace common\enviaremail;
+
+use Yii;
+use yii\base\Model;
+use common\models\Users;
+use yii\db\ActiveRecord;
+
+
+class EnviarEmail extends Model{
+  
+    public function enviarEmail()
+    {
+
+        Yii::$app->mailer->compose()
+        ->setFrom('manuel1122@hotmail.com')
+        ->setTo('manuelz0510@gmail.com')
+        ->setSubject('sujeto')
+        ->setTextBody('texto del cuerpo')
+        ->setHtmlBody('cuerpo del html')
+        ->send();
+
+    }
+
+    }
+
+ ?>
