b71dfe8aec03db5b1b0eb6136ef611e0c963cfbd
ronnys <ronnys@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-01-08 15:33:08 +0000

    actualizando

--- simwebpluscar/trunk/common/config/main-local.php
+++ simwebpluscar/trunk/common/config/main-local.php
@@ -42,24 +42,24 @@ return [
 		],
 		'mailer'    => [
 			'class'    => 'yii\swiftmailer\Mailer',
+			'viewPath' => '@common/mail',
+			// send all mails to a file by default. You have to set
+			// 'useFileTransport' to false and configure a transport
+			// for the mailer to send real emails.
 
 
 
 			'transport' => [
 			   'class' => 'Swift_SmtpTransport',
 			   'host' => 'smtp.gmail.com',
-			   'username' => 'manuelz0510@gmail.com',
-			   'password' => 'Manuel051092',
+			   'username' => 'manuel1122@hotmail.com',
+			   'password' => 'manuza21',
 			   'port' => '587',
 			   'encryption' => 'tls',
 			   
 			],
 
-			'viewPath' => '@common/mail',
-			// send all mails to a file by default. You have to set
-			// 'useFileTransport' to false and configure a transport
-			// for the mailer to send real emails.
-
+			
 			
 
 			//'useFileTransport' => true,
