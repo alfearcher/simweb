beae63e6e9992f750bff31150f32d039c0540273
ronnys <ronnys@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-01-15 18:32:01 +0000

    actualizando formato de correo

--- simwebpluscar/trunk/common/enviaremail/EnviarEmail.php
+++ simwebpluscar/trunk/common/enviaremail/EnviarEmail.php
@@ -66,7 +66,7 @@ class EnviarEmail{
         ->setTo($email)
         ->setSubject('Usuario y contraseña')
         ->setTextBody('probando')
-        ->setHtmlBody('Usuario:' .$email.'<br>'.'Contraseña: '.$nuevaClave)
+        ->setHtmlBody('Usuario: ' .$email.'<br>'.'Contraseña: '.$nuevaClave)
         ->send();
 
     }
