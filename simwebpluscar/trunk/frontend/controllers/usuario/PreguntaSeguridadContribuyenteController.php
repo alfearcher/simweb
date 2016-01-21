b8e4d8eb99dbab740989315f428b487f51681e1e
ronnys <ronnys@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-01-21 20:14:01 +0000

    actualizando

--- simwebpluscar/trunk/frontend/controllers/usuario/PreguntaSeguridadContribuyenteController.php
+++ simwebpluscar/trunk/frontend/controllers/usuario/PreguntaSeguridadContribuyenteController.php
@@ -159,7 +159,7 @@ class PreguntaSeguridadContribuyenteController extends Controller
 
             // $transaccion->commit();
              // $url =  "<meta http-equiv='refresh' content='3; ".Url::toRoute(['/site/index']). "'>";  
-               echo MensajeController::actionMensaje('We have saved your security answers');
+               return MensajeController::actionMensaje('We have saved your security answers');
                return $this->redirect(['/site/index']);
              // die('guardo');
             }else {
