2deb60444c44b0752e05f78532bf42ae67ee930b
alvarof <alvarof@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-01-28 15:51:18 +0000

    modificacion

--- simwebpluscar/trunk/backend/models/inmueble/IntegracionInmueblesForm.php
+++ simwebpluscar/trunk/backend/models/inmueble/IntegracionInmueblesForm.php
@@ -239,7 +239,7 @@ class IntegracionInmueblesForm extends \yii\db\ActiveRecord
           if($buscar1->estado_catastro != $buscar2->estado_catastro ) {
 
              $this->addError($attribute, Yii::t('backend', 'The Contributor '.$buscar1[0]['id_contribuyente'].'  has already allocated about this property Cadastre. ')); 
-             if($buscar1->municipio_catastro != $buscar1->municipio_catastro ) {
+             if($buscar1->municipio_catastro != $buscar2->municipio_catastro ) {
 
                 $this->addError($attribute, Yii::t('backend', 'The Contributor '.$buscar1[0]['id_contribuyente'].'  has already allocated about this property Cadastre. ')); 
                 if($buscar1->parroquia_catastro != $buscar2->parroquia_catastro ) {
