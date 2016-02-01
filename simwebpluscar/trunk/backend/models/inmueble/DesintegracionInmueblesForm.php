27d4854fa984ecde9083e9c917584047cd1f3291
alvarof <alvarof@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-02-01 16:49:18 +0000

    modificar

--- simwebpluscar/trunk/backend/models/inmueble/DesintegracionInmueblesForm.php
+++ simwebpluscar/trunk/backend/models/inmueble/DesintegracionInmueblesForm.php
@@ -94,7 +94,7 @@ use backend\models\inmueble\Solvencias;
  * @property string $nivel_catastro
  * @property string $unidad_catastro
  */
-class IntegracionInmueblesForm extends \yii\db\ActiveRecord
+class DesintegracionInmueblesForm extends \yii\db\ActiveRecord
 {
 
     public $conn;
