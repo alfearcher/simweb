7c7c0cad77274c782e518581648848bf02dbd963
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-02-23 15:15:00 +0000

    actualizando layouts

--- simwebpluscar/trunk/frontend/views/layouts/boton-search-contribuyente.php
+++ simwebpluscar/trunk/frontend/views/layouts/boton-search-contribuyente.php
@@ -1,65 +1,63 @@
 <?php
-	 use kartik\icons\Icon;
-   use yii\helpers\Url;
-   use yii\helpers\Html;
-   use yii\bootstrap\Nav;
-   use yii\widgets\DetailView;
-   use frontend\models\usuario\Afiliacion;
-   use frontend\models\usuario\CrearUsuarioNatural;
-  	$typeIcon = Icon::FA;
-  	$typeLong = 'fa-2x';
+  use kartik\icons\Icon;
+    use yii\helpers\Url;
+    use yii\helpers\Html;
+    use yii\bootstrap\Nav;
+    use frontend\models\usuario\CrearUsuarioJuridico;
+    
+
+    $typeIcon = Icon::FA;
+    $typeLong = 'fa-2x';
 
     Icon::map($this, $typeIcon);
+ ?>
+<!-- <nav class="navbar navbar-default1">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right" >
 
+                <div class="search" id="search" >
+                    <i class="fa fa-search fa-2x"></i>search
+                </div>
 
-   $modelContribuyente = Yii::$app->user->identity;
+                <div class="undo" id="undo" style="padding-right: 15px;">
+                    <i class="fa fa-undo fa-2x"></i>undo
+                </div>
 
-   //die(var_dump($modelContribuyente));
-    $this->title = 'Informacion del Contribuyente';
+                <div class="contribuyente" style="padding-right: 5px;">
+                    <form>
+                        <div class="form-group" >
+                            <input type="text" class="form-control" id="contribuyente" readonly style="width: 500px;">
+                        </div>
+                    </form>
+                </div>
 
-   //   $id = $_SESSION['idContribuyente'];
-   //   $nombre = $_SESSION['nombre'];
-   //   $apellido = $_SESSION['apellido'];
+                <div class="id-cont" style="padding-right: 5px;">
+                    <form>
+                        <div class="form-group" >
+                            <input type="text" class="form-control" id="id-contribuyente" readonly style="width: 150px;">
+                        </div>
+                    </form>
+                </div>
 
-    
-   // $modelContribuyente = ['id_contribuyente' => $id, 'nombre' => $nombre.' '.$apellido];
-    
-    ?>
+                <div class="view-ok" id="view-ok" style="padding-right: 13px;">
+                    <i class="glyphicon glyphicon-user fa-2x"></i>
+                </div>
 
+                <div class="multa" id="multa" >
+                    <i class="fa fa-file-text-o fa-2x"></i>Multa<span class="badge">4</span>
+                </div>
 
-   <div class="row">
-      <div class="col-sm-5" style="margin-left:200px; width: 660px; height: 120px;">
-      <div class="panel panel-primary">
-       <div class="panel-heading">
-       <?= $this->title ?>
-       </div>
-      <div class="panel-body">
-     
-       <?= DetailView::widget([
-                                                       'model' => $modelContribuyente,
-                                                       
-                                                       'attributes' => [ 
-                                                       'id_contribuyente',
-                                                       // 'nombres' ,
-                                                        ],
-                                                        'options' =>[ 
-                                                        'style' => 'width:600px; margin-left:0px;',
-                                                        'class' =>'table table-hover table-bordered',
-                                                          
-       
-                                                        ],
-                                                        
-                             ]) ?> 
+                <div class="alerta" id="alerta" >
+                    <i class="fa fa-flag-o fa-2x"></i>alerta<span class="badge">2</span>
+                </div>
 
-
-      </div>
-      </div>
+            </ul>
         </div>
-        </div>
-      
-    
-<!--  </div>
+    </div>
+</nav> -->
 
+<!--  -->
 
 <!-- <nav class="navbar navbar-default1">
     <div class="container-fluid">
@@ -69,9 +67,77 @@
             </ul>
         </div>
     </div>
-</nav> 
+</nav> -->
 
 
 
+<?php
+
+    $buscar = CrearUsuarioJuridico::find()
+                                    ->where([
+
+                                        'id_contribuyente' => yii::$app->user->identity->id_contribuyente,
+                                        'inactivo' => 0,
+
+                                        ])
+                                    ->all();
+
+
+ 
+
+                                    
+
+    if ($buscar[0]['tipo_naturaleza'] == 0){
+
+        
+
+        $id_contribuyente = $buscar[0]['id_contribuyente'];
+        $contribuyente =$buscar[0]['nombres'].' '.$buscar[0]['apellidos'];
+
+
+
+    } if ($buscar[0]['tipo_naturaleza'] == 1){
+
+            $id_contribuyente = $buscar[0]['id_contribuyente'];
+           $contribuyente =$buscar[0]['razon_social'];
+
+        }
+
+
+
+        
+    
+
+    
+
+    //$menuItems1[] = ['label' => '<input type="text" value="'. $contribuyente . '" id="contribuyente" class="form-control" style="width: 25%;padding-right: 0px;">'];
+
+    
+    $menuItems1[] = ['label' => '<div class="contribuyente" style="padding-right: 5px; margin-left: 90px;">
+                    <form>
+                        <div class="form-group" >
+                            <input type="text" value="' . $contribuyente . '" class="form-control" id="contribuyente" readonly style="width: 450px;">
+                        </div>
+                    </form>
+                </div>'];
+
+    $menuItems1[] = ['label' => '<div class="id-cont" style="padding-right: 5px;">
+                    <form>
+                        <div class="form-group" >
+                            <input type="text" value="' . $id_contribuyente . '" class="form-control" id="id-contribuyente" readonly style="width: 150px;">
+                        </div>
+                    </form>
+                </div>'];
+
+
+
+    
+
+    echo Nav::widget([
+        'options' => ['class' => 'navbar navbar-right1',
+        ],
+        'items' => $menuItems1,
+        'encodeLabels' => false,
+    ]);
 
 ?>
--- simwebpluscar/trunk/frontend/views/layouts/boton-search.php
+++ simwebpluscar/trunk/frontend/views/layouts/boton-search.php
@@ -3,6 +3,7 @@
   	use yii\helpers\Url;
     use yii\helpers\Html;
     use yii\bootstrap\Nav;
+    
 
   	$typeIcon = Icon::FA;
   	$typeLong = 'fa-2x';
