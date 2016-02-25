907e6577cfc08ee0a27a12ef26d62040f6b425bb
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-02-25 16:01:38 +0000

    actualizando controlador para actualizar el password desde adentro de la cuenta

--- simwebpluscar/trunk/frontend/controllers/usuario/MostrarPreguntaSeguridadController.php
+++ simwebpluscar/trunk/frontend/controllers/usuario/MostrarPreguntaSeguridadController.php
@@ -62,6 +62,12 @@ use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use frontend\models\usuario\PreguntaSeguridadContribuyente;
 use frontend\models\usuario\MostrarPreguntaSeguridadForm;
+use frontend\models\usuario\ReseteoPasswordForm;
+use common\models\utilidades\Utilidad;
+use common\seguridad\Seguridad;
+use common\conexion\ConexionController;
+use frontend\models\usuario\CrearUsuarioNatural;
+use common\enviaremail\EnviarEmailCambioClave;
 
 /**
  * Site controller
@@ -78,6 +84,7 @@ class MostrarPreguntaSeguridadController extends Controller
 
     
    public $layout = 'layoutbase';
+   
     /**
      *
      * metodo que realiza la busqueda de las preguntas de seguridad del contribuyente una vez que este esta dentro de su cuenta,
@@ -87,38 +94,144 @@ class MostrarPreguntaSeguridadController extends Controller
      */
     public function actionBuscarMostrarPreguntaSeguridad()
     {
-    
+     
+        $datosContribuyente = yii::$app->user->identity;
 
-     // $datosContribuyente = $_SESSION['sesion'];
-
-      //die(var_dump($datosContribuyente->id_contribuyente));
-      //die(var_dump(yii::$app->user->identity));
-
-
-
-    $buscarPreguntas = PreguntaSeguridadContribuyente::find()
+        $buscarPreguntas = PreguntaSeguridadContribuyente::find()
                                                     ->where([
                                                     'id_contribuyente' => $datosContribuyente->id_contribuyente,
                                                     'inactivo' => 0,
 
                                                         ])
                                                     ->all();
-    //die(var_dump($buscarPreguntas));
-    //
-      $_SESSION['preguntaSeguridad'] = $buscarPreguntas;
-
+        
 
         $model = new MostrarPreguntaSeguridadForm();
 
-        return $this->render('/usuario/mostrar-pregunta-seguridad', [
-                                                                            'model' => $model,
-                                                                            'preguntaSeguridad' => $buscarPreguntas,
-            ]);
- 
+            $postData = Yii::$app->request->post();
+
+            if ( $model->load($postData) && Yii::$app->request->isAjax ){
+                  Yii::$app->response->format = Response::FORMAT_JSON;
+                  return ActiveForm::validate($model);
+            }
+
+            if ( $model->load($postData) ) {
+
+                if ($model->validate()){
+
+                   return $this->redirect (['/usuario/mostrar-pregunta-seguridad/reseteo-password',
+                                                                          'id_contribuyente' => $datosContribuyente->id_contribuyente,
+
+
+
+                                                                                                          ]);
+
+                }
+                
+
+              
+            }
+            return $this->render('/usuario/mostrar-pregunta-seguridad', [
+                                                        'model' => $model,
+                                                        'preguntaSeguridad' => $buscarPreguntas,
+            ]);  
+    }
 
     
 
-}
+
+    public function actionReseteoPassword()
+    {
+
+
+        $idContribuyente = yii::$app->user->identity->id_contribuyente;
+
+       $model = New ReseteoPasswordForm();
+
+        $postData = Yii::$app->request->post();
+
+        if ( $model->load($postData) && Yii::$app->request->isAjax ){
+                Yii::$app->response->format = Response::FORMAT_JSON;
+                return ActiveForm::validate($model);
+                }
+                if ( $model->load($postData) ) {
+
+                    if ($model->validate()){
+                 
+                        $actualizarPassword =  self::actualizarPasswordNatural($idContribuyente, $model->password1);
+
+                        if ($actualizarPassword == true){
+
+                            $consultaContribuyente = new CrearusuarioNatural();
+
+                            $consultaContribuyente = CrearUsuarioNatural::find()
+                                                                          ->where([
+                                                                         'id_contribuyente' => $idContribuyente,
+                                                                         'inactivo' => 0,
+                                                                          ])
+                                                                         ->one();
+
+                            $enviarEmail = new EnviarEmailCambioClave();
+                            $enviarEmail->EnviarEmailCambioClave($consultaContribuyente->email, $model->password1);
+                      
+                            return MensajeController::actionMensaje(Yii::t('frontend', 'We have sent you an email with your new password'));
+
+                        }
+                    
+                    }
+                        
+                
+              
+
+        }
+        return $this->render('/usuario/reseteo-password'   , ['model' => $model,
+                                                                    'id_contribuyente' => $idContribuyente,
+                                                                    ]);  die('llegue a reseteo password');
+    }
+
+    public function actualizarPasswordNatural($idContribuyente, $password1){
+
+      $tableName = 'afiliaciones';
+      $arregloCondition = ['id_contribuyente' => $idContribuyente]; 
+
+      $seguridad = new Seguridad();
+
+      $nuevaClave = $seguridad->randKey(6);
+
+      $salt = Utilidad::getUtilidad();
+
+      $password = $password1.$salt;
+
+      $password_hash = md5($password);
+         
+      $arregloDatos = ['password_hash' => $password_hash];
+
+      $conexion = new ConexionController();
+
+      $conn = $conexion->initConectar('db');
+         
+      $conn->open();
+
+      $transaccion = $conn->beginTransaction();
+
+          if ($conexion->modificarRegistroNatural($conn, $tableName, $arregloDatos, $arregloCondition)){
+
+              $transaccion->commit();
+              $conn->close();
+              return true;
+              
+          }else{ 
+         
+              $transaccion->rollback();
+              $conn->close();
+              return false;
+          }
+
+  }
+
+
+
+       
 
 
 
