3c1bec92895318dd94634ab81d0006042d7236ed
alvarof <alvarof@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-02-25 20:50:44 +0000

    modificacion

--- simwebpluscar/trunk/backend/views/inmueble/desintegracion-inmuebles-urbanos/_desintegracioninmueblesform.php
+++ simwebpluscar/trunk/backend/views/inmueble/desintegracion-inmuebles-urbanos/_desintegracioninmueblesform.php
@@ -200,6 +200,22 @@ $variablephp=$_COOKIE['variablephp'];
                                                                                                                     'onchange' => 'bloquea()'
                                                                                                                     ])->label(false) ?> 
                                                         </div>
+
+                                                        <div class="fecha-inicio">
+                                                        <?= $form->field($model, 'fecha_inicio')->widget(\yii\jui\DatePicker::classname(),['id' => 'fecha-inicio',
+                                                                                                                                            'clientOptions' => [
+                                                                                                                                                'maxDate' => '+0d', // Bloquear los dias en el calendario a partir del dia siguiente al actual.
+                                                                                                                                            ],
+                                                                                                                                            'language' => 'es-ES',
+                                                                                                                                            'dateFormat' => 'dd-MM-yyyy',
+                                                                                                                                            'options' => [
+                                                                                                                                                    'class' => 'form-control',
+                                                                                                                                                    'readonly' => true,
+                                                                                                                                                    'style' => 'background-color: white;',
+
+                                                                                                                                            ]
+                                                                                                                                            ])->label(false) ?>
+                                                    </div>
                                                     
                                                     </td>
                                                 </tr>
