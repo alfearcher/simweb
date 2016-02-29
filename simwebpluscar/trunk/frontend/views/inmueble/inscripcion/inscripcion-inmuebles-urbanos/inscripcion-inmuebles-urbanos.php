727d5a872d1d7b449700b1fbd5bb404d1c6f75ea
alvarof <alvarof@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-02-29 16:48:06 +0000

    modificacion

--- simwebpluscar/trunk/frontend/views/inmueble/inscripcion/inscripcion-inmuebles-urbanos/inscripcion-inmuebles-urbanos.php
+++ simwebpluscar/trunk/frontend/views/inmueble/inscripcion/inscripcion-inmuebles-urbanos/inscripcion-inmuebles-urbanos.php
@@ -76,7 +76,7 @@ function bloquea() {
 
                         <td style="max-width: 100px" align="letf">
                             <div class="col-sm-4">
-                            <?= $form->field($model, 'id_contribuyente')->textInput(['readonly'=>'readonly','value'=>Yii::$app->user->identity->id_contribuyente,'style' => 'width:80px;'])->label(false) ?>
+                            <?= $form->field($model, 'id_contribuyente')->textInput(['readonly'=>'readonly','value'=>178131,'style' => 'width:80px;'])->label(false) //Yii::$app->user->identity->id_contribuyente?>
                             </div> 
                         </td>
 
