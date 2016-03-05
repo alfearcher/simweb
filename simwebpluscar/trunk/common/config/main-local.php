234605e42f98ddec910fa22b187ea7fd5644e2d5
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-03-05 16:45:24 +0000

    se agrego un aparametro para controlar los eventos en las solicitudes

--- simwebpluscar/trunk/common/config/main-local.php
+++ simwebpluscar/trunk/common/config/main-local.php
@@ -3,15 +3,15 @@ return [
 	'components' => [
 		'db'        => [
 			'class'    => 'yii\db\Connection',
-			//'dsn' => 'mysql:host=localhost;dbname=sim_teq',
-			'dsn' => 'mysql:host=192.168.30.7;dbname=sim_car_plus',
+			'dsn' => 'mysql:host=localhost;dbname=sim_car_plus',
+			//'dsn' => 'mysql:host=192.168.30.7;dbname=sim_car_plus',
 			//'dsn' => 'mysql:host=pinky.ddns.net;dbname=sim_car',
-			'username' => 'adminyii',
-			'password' => 'adminyii2015',
+			//'username' => 'adminyii',
+			//'password' => 'adminyii2015',
 			//'username' => 'jperez',
 			//'password' => 'jperez',
-			//'username' => 'root',
-			//'password' => '',
+			'username' => 'root',
+			'password' => '',
 			'charset'  => 'utf8',
 		],
 		'dbsim' => [
@@ -25,7 +25,7 @@ return [
 			//'password' => 'jperez',
 			//'username' => 'root',
 			//'password' => '',
-			'charset'  => 'utf8',	
+			'charset'  => 'utf8',
 		],
 		'dbswp'  => [
 			'class'    => 'yii\db\Connection',
@@ -57,12 +57,8 @@ return [
 			   'password' => 'Manuel051092',
 			   'port' => '587',
 			   'encryption' => 'tls',
-			   
 			],
 
-
-			
-			
 
 			//'useFileTransport' => true,
 		],
@@ -71,6 +67,9 @@ return [
 		],
 		'oficina' => [
 			'class'  => 'common\classes\Oficina'
+		],
+		'solicitud' => [
+			'class' => 'common\classes\EventoSolicitud'
 		],
 	],
 ];
