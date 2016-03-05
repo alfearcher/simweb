35a9bd0a4a92bc7e5e940068f1187d0f3137d5ae
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-03-05 16:47:37 +0000

    prueba

--- simwebpluscar/trunk/common/config/main-local.php
+++ simwebpluscar/trunk/common/config/main-local.php
@@ -3,15 +3,15 @@ return [
 	'components' => [
 		'db'        => [
 			'class'    => 'yii\db\Connection',
-			'dsn' => 'mysql:host=localhost;dbname=sim_car_plus',
-			//'dsn' => 'mysql:host=192.168.30.7;dbname=sim_car_plus',
+			//'dsn' => 'mysql:host=localhost;dbname=sim_car_plus',
+			'dsn' => 'mysql:host=192.168.30.7;dbname=sim_car_plus',
 			//'dsn' => 'mysql:host=pinky.ddns.net;dbname=sim_car',
-			//'username' => 'adminyii',
-			//'password' => 'adminyii2015',
+			'username' => 'adminyii',
+			'password' => 'adminyii2015',
 			//'username' => 'jperez',
 			//'password' => 'jperez',
-			'username' => 'root',
-			'password' => '',
+			//'username' => 'root',
+			//'password' => '',
 			'charset'  => 'utf8',
 		],
 		'dbsim' => [
