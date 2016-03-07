beb7312045b3e42c2bd74aed082c6199f20c9ccd
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-03-07 21:09:43 +0000

    actualizacion

--- simwebpluscar/trunk/common/models/configuracion/solicitud/ParametroSolicitud.php
+++ simwebpluscar/trunk/common/models/configuracion/solicitud/ParametroSolicitud.php
@@ -4,6 +4,9 @@
 	use Yii;
 	use backend\models\configuracion\solicitud\ConfigurarSolicitud;
 	use backend\models\configuracion\tiposolicitud\TipoSolicitud;
+	use backend\models\impuesto\Impuesto;
+	use backend\models\configuracion\documentosolicitud\SolicitudDocumento;
+
 	/**
 	*
 	*/
@@ -16,29 +19,39 @@
 
 		public $configSolicitud;
 
-		public function __construct()
-		{}
 
-
-		/***/
+		/**
+		* Busca y configura la variable $config, con los datos de la entidad
+		* "config-solicitudes", creando un modelo de dicha entidad.
+		* @param $idConfig, long que identifica al registro de la entidad
+		* "config-solicitudes".
+		*/
 		public  function configurar($idConfig = 0)
 		{
-
 			$this->setIdConfig($idConfig);
 			$config = ConfigurarSolicitud::findOne($this->getIdConfig());
 			$this->setConfigSolicitud($config);
-
 		}
 
 
 
-		/***/
+		/**
+		* Returna los datos principales de la entidad "config-solicitudes".
+		* @return model de configuracion de la solicitud especifica. Especificada
+		* por la variables $this->configSolicitud.
+		*/
 		public function getConfigSolicitud()
 		{
 			return $this->configSolicitud;
 		}
 
 
+		/**
+		* Setea la variable con los datos principales de la configuracion de la
+		* solicitud, determinada por la entidad "config-solicitudes".
+		* $config, es un modelo de la entidad config-solicitudes.
+		* @param $config, variable que contiene el modelo de la entidad "config-solicitudes".
+		*/
 		public function setConfigSolicitud($config)
 		{
 			$this->configSolicitud = $config;
@@ -46,12 +59,19 @@
 
 
 
-		/***/
+		/**
+		* identificador de la entidad "config-solicitudes".
+		* Es decir de la configuracion de la solicitud.
+		*/
 		public function getIdConfig()
 		{
 			return $this->idConfigSolicitud;
 		}
 
+
+		/**
+		* Setea la variable $this->idConfigSolicitud con el identificador de la misma.
+		*/
 		public function setIdConfig($idConfig)
 		{
 			$this->idConfigSolicitud = $idConfig;
@@ -81,20 +101,57 @@
 
 
 
-		public function getTipoSolicitud()
-		{
-			$config = New ConfigurarSolicitud();
-			return $config->getDescripcionTipoSolicitud();
-		}
 
-
-
-		public function findConfigurarSolicitud()
+		/**
+		* Metodo que realiza una busqueda de los registros realizando un inner join
+		* entre config-solicitudes y config-tipos-solicitudes.
+		* "tipoSolicitud, es un metodo (getTipoSolicitud) de la clase ConfigurarSolicitud"
+		* para realizar la referencia a dicho metodo se utiliza el nombre del metodo
+		* sin el "get" y colocando el nombre en miniscula, si el nombre del metodo es
+		* la combinacion de dos o más palabras, se coloca la primera letra en minisculas
+		* y otras primeras letras en mayuscula.
+		* La relacion de ambas tablas se realiza se denomina "tipoSolicitud".
+		* @return $config, array con los campos ambas tablas.
+		*/
+		public function findConfiguracionSolicitudTipo()
 		{
 			$config = ConfigurarSolicitud::find()->where(['id_config_solicitud' => $this->getIdConfig()])
-			                                     ->with('tipoSolicitud')->one();
+			                                     ->with('tipoSolicitud')->asArray()->all();
 			return $config;
 		}
+
+
+		/**
+		* Se obtiene la descripcion del tipo de solcitud.
+		*/
+		public function getDescripcionTipoSolicitud()
+		{
+			$config = $this->findConfigurarSolicitud();
+			return $config[0]['tipoSolicitud']['descripcion'];
+		}
+
+
+
+		/**
+		* Metodo que realiza la busqueda de los registros utilizando inner join entre las entidades
+		* "config-solicitudes" e "impuestos". Donde la relacion can la entidad "impuestos" se realiza
+		* a traves de la clase ConfigurarSolicitud con el metodo getImpuestoSolicitud.
+		*/
+		public function findConfiguracionSolicitudImpuesto()
+		{
+			$configImpuesto = ConfigurarSolicitud::find()->where(['id_config_solicitud' => $this->getIdConfig()])
+			                        				     ->with('impuestoSolicitud')->asArray()->all();
+			return $configImpuesto;
+		}
+
+
+		public function getDescripcionImpuestoSolcitud()
+		{
+			$configImpuesto = $this->findConfiguracionSolicitudImpuesto();
+			return $configImpuesto[0]['impuestoSolicitud']['descripcion'];
+		}
+
+
 	}
 
 
