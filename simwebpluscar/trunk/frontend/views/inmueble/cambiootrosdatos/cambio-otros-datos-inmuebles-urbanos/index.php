598df1c712bf998f0bae54ef0d25fdd872286be0
alvarof <alvarof@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-03-08 20:29:24 +0000

    modificacion

--- simwebpluscar/trunk/frontend/views/inmueble/cambiootrosdatos/cambio-otros-datos-inmuebles-urbanos/index.php
+++ simwebpluscar/trunk/frontend/views/inmueble/cambiootrosdatos/cambio-otros-datos-inmuebles-urbanos/index.php
@@ -21,7 +21,7 @@ $this->title = Yii::t('backend', 'Property Urban');
  <?php $form = ActiveForm::begin([
     'method' => 'post',
     'id' => 'formulario',
-    //'action'=> '/inmueble/cambiootrosdatos/cambio-otros-datos-inmuebles-urbanos/view',
+    'action'=> '/inmueble/cambiootrosdatos/cambio-otros-datos-inmuebles-urbanos/view',
     'enableClientValidation' => false,
     'enableAjaxValidation' => true,
     'options' => ['class' => 'form-vertical'],]); ?>
