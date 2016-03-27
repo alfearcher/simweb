6554de6dd8cfa8bb7482e87df8f120d27c551fe1
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-03-27 01:46:15 +0000

    actualizacion

--- simwebpluscar/trunk/backend/models/aaee/declaracion/Declaracion.php
+++ simwebpluscar/trunk/backend/models/aaee/declaracion/Declaracion.php
@@ -62,6 +62,7 @@
 		private $_idContribuyente;
 		private $_fechaDesde;
 		private $_fechaHasta;
+		private $_idImpuesto;
 
 
 
@@ -113,11 +114,29 @@
 
 
 
+		/***/
 		public function setLapsoFecha($fechaDesde, $fechaHasta)
 		{
 			$this->setFechaDesde($fechaDesde);
 			$this->setFechaHasta($fechaHasta);
 		}
+
+
+
+		/***/
+		public function setIdImpuesto($idImpuesto)
+		{
+			$this->_idImpuesto = $idImpuesto;
+		}
+
+
+
+		/***/
+		public function getIdImpuesto()
+		{
+			return $this->_idImpuesto;
+		}
+
 
 
 
@@ -145,6 +164,70 @@
 				if ( count($modelfind) > 0 ) {
 					return $modelfind;
 				}
+			}
+			return null;
+		}
+
+
+
+
+		/***/
+		private function getRubroDetalle()
+		{
+			if ( $this->_añoImpositivo > 0 && $this->_idImpuesto > 0 ) {
+				$modelfind = ActEconIngreso::find()->where([
+														'id_impuesto' => $this->_idImpuesto,
+														'ano_impositivo' => $this->_añoImpositivo,
+														'exigibilidad_periodo' => $this->_periodo,
+													])
+				                            		->joinWith('rubroDetalle')
+				                            		->orderBy([
+				                            				'ano_impositivo' => SORT_ASC,
+				                            				'exigibilidad_periodo' => SORT_ASC,
+				                            				'rubro' => SORT_ASC,
+				                            	     ])
+				                            		->asArray()
+				                            		->all();
+				if ( count($modelfind) > 0 ) {
+					return $modelfind;
+				}
+			}
+			return null;
+		}
+
+
+
+
+		/***/
+		public function getIdImpuestoSegunAnoImpositivo($añoImpositivo = 0)
+		{
+			if ( $añoImpositivo > 0 ) {
+				$this->setAnoImpositivo($añoImpositivo);
+			}
+			$modelFind = ActEcon::find()->select('id_impuesto')
+										->where([
+											'id_contribuyente' => $this->_idContribuyente,
+											'ano_impositivo' => $this->_añoImpositivo,
+										])
+										->one();
+			if ( isset($modelFind) ) {
+				return $modelFind;
+			}
+			return null;
+		}
+
+
+
+		/***/
+		public function getRubroDeclarado($añoImpositivo, $periodo)
+		{
+			$this->setAnoImpositivo($añoImpositivo);
+			$this->setPeriodo($periodo);
+			$this->setIdImpuesto(0);
+			$model = $this->getIdImpuestoSegunAnoImpositivo();
+			if ( isset($model->id_impuesto) ) {
+				$this->setIdImpuesto($model->id_impuesto);
+				return $this->getRubroDetalle();
 			}
 			return null;
 		}
