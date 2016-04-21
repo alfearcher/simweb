73e76296284743308cd45c6c97a2d24f2e3eb160
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-04-21 20:13:38 +0000

    agregando modelos para calcomania de vehiculo

---
+++ simwebpluscar/trunk/backend/models/vehiculo/calcomania/deshabilitarfuncionario/FuncionarioSearch.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ * 
+ *  > This library is free software; you can redistribute it and/or modify it under 
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free 
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion) 
+ *  > any later version.
+ *  > 
+ *  > This library is distributed in the hope that it will be usefull, 
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability 
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence 
+ *  > for more details.
+ *  > 
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**    
+ *  @file FuncionarioSearch.php
+ *  
+ *  @author Manuel Alejandro Zapata Canelon
+ * 
+ *  @date 21/04/2016
+ * 
+ *  @class FuncionarioSearch
+ *  @brief Clase que contiene las rules para validacion y contiene metodo que realiza la busqueda con un dataprovider para verificar
+ *  los funcionarios activos.
+ * 
+ *  
+ * 
+ *  
+ *  
+ *  @property
+ *
+ *  
+ *  @method
+ *  rules
+ *  scenarios
+ *  search
+ *
+ *  
+ *
+ *  @inherits
+ *  
+ */ 
+namespace backend\models\vehiculo\calcomania\deshabilitarfuncionario;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+use backend\models\funcionario\FuncionarioCalcomania;
+
+
+/**
+ * FuncionarioSearch la clase que contiene el metodo que realiza la busqueda de los funcionarios activos
+ */
+class FuncionarioSearch extends Model
+{
+
+    
+   
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            
+        ]; 
+    } 
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search()
+    { 
+  
+
+        $idFuncionario = yii::$app->user->identity->id_user;
+
+
+        $query = FuncionarioCalcomania::find();
+
+                                
+                             
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+           // die(var_dump($dataProvider)),
+        ]);
+        $query->where([
+          
+            'estatus' => 0,
+            ]);
+        
+        return $dataProvider;
+
+       
+    }
+
+   
+}
