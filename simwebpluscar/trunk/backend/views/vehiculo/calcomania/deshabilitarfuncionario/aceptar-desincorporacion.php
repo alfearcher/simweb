fa8fb1cd3baa79c0c8a8392c23b2dbd0e4b62a0d
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-04-22 15:12:47 +0000

    actualizando

---
+++ simwebpluscar/trunk/backend/views/vehiculo/calcomania/deshabilitarfuncionario/aceptar-desincorporacion.php
@@ -0,0 +1,82 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use backend\models\registromaestro\TipoNaturaleza;
+use yii\helpers\ArrayHelper;
+
+
+
+
+$this->title = 'Verificacion de Deshabilitacion';
+?>
+ 
+
+
+
+<?php $form = ActiveForm::begin([
+   // 'method' => 'post',
+    'id' => 'formulario',
+    'enableClientValidation' => true,
+    'enableAjaxValidation' => false,
+    'options' => ['class' => 'form-horizontal'],
+        
+]);
+?>
+
+<div class="col-sm-6">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <?= $this->title ?>
+                </div>
+                    <div class="panel-body" >
+               
+                  
+
+
+                        <p>¿ESTA SEGURO QUE DESEA DESHABILITAR ESTOS FUNCIONARIOS?</p>    
+        
+                        
+                            
+
+
+            
+
+ <!-- Boton para aplicar la actualizacion -->
+                                    <div class="col-sm-4" >
+                                        
+                                            <?= Html::submitButton(Yii::t('frontend' , 'Yes'),
+                                                                                                      [
+                                                                                                        'id' => 'btn-search',
+                                                                                                        'class' => 'btn btn-success',
+                                                                                                        'name' => 'btn-search',
+                                                                                                   
+                                                                                                        'style' => 'height:30px;width:100px;margin-right:0px;',
+                                                                                                      ])
+                                            ?>
+                                        
+                                    </div>
+
+                                       <div class="col-sm-2" >
+                                        
+                                            <?= Html::a('No',['/vehiculo/calcomania/deshabilitarfuncionario/deshabilitar-funcionario/busqueda-funcionario'], ['class' => 'btn btn-primary','style' => 'height:30px;width:100px;margin-left:50px;' ]) //boton para volver al menu de seleccion tipo usuario ?>
+                                        
+                                    </div>
+                                   
+<!-- Fin de Boton para aplicar la actualizacion -->
+
+                                
+
+
+
+          </div>
+       </div>
+    </div>
+  </div> 
+</div> 
+    
+
+     
+
+<?php $form->end() ?>
+
