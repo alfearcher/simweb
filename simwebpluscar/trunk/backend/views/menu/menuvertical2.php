774d51b7c4fd1625c3480f64e75c5ee759f6d4d9
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-04-27 18:47:13 +0000

    agregando ruta para deshabilitacion de lote de calcomanias

--- simwebpluscar/trunk/backend/views/menu/menuvertical2.php
+++ simwebpluscar/trunk/backend/views/menu/menuvertical2.php
@@ -125,7 +125,7 @@ session_start();
 
 			        							['label' => 'Generar Lote de Calcomania','url' => ['/vehiculo/calcomania/generarlote/generar-lote-calcomania/generar-lote-calcomania']],
 
-			        							['label' => 'Deshabilitar Lote de Calcomanias','url' => ['/vehiculo/calcomania/deshabilitarlote/deshabilitar-lote-calcomania/vista-seleccion']],
+			        							['label' => 'Deshabilitar Lote de Calcomanias','url' => ['/vehiculo/calcomania/deshabilitarlote/deshabilitar-lote-calcomania/busqueda-lote-calcomania']],
 			        							['label' => 'Administrar Lote de Calcomanias Generadas','url' => ['vehiculo/calcomania/lote-calcomania/busqueda-lote']],
 			        							['label' => 'Asignar Calcomanias a funcionario (Distribuir entre funcionario responsables)','url' => ['vehiculo/calcomania/funcionario-calcomania/distribuir-lote']],
 			        							['label' => 'Administrar Entrega de calcomania','url' => '#'],
