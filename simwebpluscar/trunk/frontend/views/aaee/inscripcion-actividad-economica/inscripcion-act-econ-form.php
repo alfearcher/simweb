d4d7800410fd92ae9e33e704efe77ea4ce308ed8
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-09 22:52:24 +0000

    Actualizacion

--- simwebpluscar/trunk/frontend/views/aaee/inscripcion-actividad-economica/inscripcion-act-econ-form.php
+++ simwebpluscar/trunk/frontend/views/aaee/inscripcion-actividad-economica/inscripcion-act-econ-form.php
@@ -80,7 +80,7 @@
 	        		?>
 	        		<style type="text/css">
 						.col-sm-3 > ul > li > a:hover {
-							color: #337AB7;
+							background-color: #337AB7;
 						}
 					</style>
 	        	</div>
