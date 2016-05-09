6536d6b89aaa7f0b4dbab997f8da2e42b5dbc158
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-09 20:19:20 +0000

    prueba

--- simwebpluscar/trunk/frontend/controllers/aaee/inscripcionactecon/InscripcionActividadEconomicaController.php
+++ simwebpluscar/trunk/frontend/controllers/aaee/inscripcionactecon/InscripcionActividadEconomicaController.php
@@ -107,7 +107,7 @@
 			      	} else {
 
 			  		}
-die(var_dump(Yii::$app->user->identity->username));
+die(var_dump(Yii::$app->user->identity));
 		  			return $this->render('/aaee/inscripcion-actividad-economica/_create', [
 		  																'model' => $model,
 		  				]);
