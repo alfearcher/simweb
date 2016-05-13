98cf693fb16e8b2d41c0fae84c46ef5929f225c1
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-13 21:00:39 +0000

    Actualizacion

--- simwebpluscar/trunk/frontend/controllers/aaee/inscripcionactecon/InscripcionActividadEconomicaController.php
+++ simwebpluscar/trunk/frontend/controllers/aaee/inscripcionactecon/InscripcionActividadEconomicaController.php
@@ -99,7 +99,7 @@
 
 				$r = New SolicitudProcesoEvento($getData['id']);
 
-				$r->getProcesoQueGeneraSolicitudSegunEvento(Yii::$app->solicitud->crear());
+				$r->getEjecutaProcesoSolicitudSegunEvento(Yii::$app->solicitud->crear());
 
 
 				// identificador de la configuracion de la solicitud.
