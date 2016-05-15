52f839cb85a7655d8f3d179eb6715f26c95cb0b1
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-15 00:39:25 +0000

    prueba

--- simwebpluscar/trunk/common/models/configuracion/solicitud/SolicitudProcesoEvento.php
+++ simwebpluscar/trunk/common/models/configuracion/solicitud/SolicitudProcesoEvento.php
@@ -131,7 +131,7 @@
 			// Se espera los valores de la entidad "config-solic-tasas-multas".
 			$tasas = $this->getDetalleSolicitudTasaMulta($evento);
 			foreach ( $tasas as $tasa ) {
-die(var_dump($tasa['nro_veces_liquidar']));
+die(var_dump($tasa));
 				foreach ( $tasa as $key => $value ) {
 					if ( $key == 'id_impuesto' ) {
 						$miTasa = New TasaForm();
