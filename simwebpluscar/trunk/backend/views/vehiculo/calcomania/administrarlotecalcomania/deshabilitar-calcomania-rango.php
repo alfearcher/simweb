12153759006b7669b573f7f0ae3c7d8d43eb9a59
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-19 17:55:11 +0000

    actualizando

--- simwebpluscar/trunk/backend/views/vehiculo/calcomania/administrarlotecalcomania/deshabilitar-calcomania-rango.php
+++ simwebpluscar/trunk/backend/views/vehiculo/calcomania/administrarlotecalcomania/deshabilitar-calcomania-rango.php
@@ -95,7 +95,7 @@ $this->title = Yii::t('frontend', 'Select the Stickers');
 
     <p>
        
-        <?= Html::a(Yii::t('backend', 'Back'), ['/menu/menu-vertical'], ['class' => 'btn btn-danger']) ?>
+        <?= Html::a(Yii::t('backend', 'Back'), ['/menu/vertical'], ['class' => 'btn btn-danger']) ?>
     </p>
 
     <div class="col-sm-2" style="float:right; color:red; font: comic sans ms">
