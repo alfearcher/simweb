e3e76718e0bd011dafcfa6ac7dbca3476e41ae56
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-20 14:16:07 +0000

    Modulo que renderizara una vista para las solicitudes del actividad economica

---
+++ simwebpluscar/trunk/backend/controllers/aaee/solicitud/SolicitudViewController.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *	@copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *	> This library is free software; you can redistribute it and/or modify it under
+ *	> the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *	> Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *	> any later version.
+ *  >
+ *	> This library is distributed in the hope that it will be usefull,
+ *	> but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *	> or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *	> for more details.
+ *  >
+ *	> See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *	@file InscripcionActividadEconomicaController.php
+ *
+ *	@author Jose Rafael Perez Teran
+ *
+ *	@date 19-09-2015
+ *
+ *  @class InscripcionActividadEconomicaController
+ *	@brief Clase InscripcionActividadEconomicaController, inscripcion de contribuyentes
+ *	en el area del impuesto Actividad Economica
+ *
+ *
+ *	@property
+ *
+ *
+ *	@method
+ *
+ *
+ *	@inherits
+ *
+ */
+
+
+ 	namespace backend\controllers\aaee\solicitud;
+
+
+ 	use Yii;
+	use yii\filters\AccessControl;
+	use yii\web\Controller;
+	use yii\filters\VerbFilter;
+	use yii\widgets\ActiveForm;
+	use yii\web\Response;
+	use yii\helpers\Url;
+	use yii\web\NotFoundHttpException;
+	//use backend\models\aaee\inscripcionactecon\InscripcionActividadEconomicaSearch;
+	use common\conexion\ConexionController;
+	use backend\controllers\MenuController;
+
+	session_start();		// Iniciando session
+
+	/**
+	 *
+	 */
+	class SolicitudViewController extends Controller
+	{
+
+		/**
+		 * [actionIndex description]
+		 * @return [type] [description]
+		 */
+		public function actionDeterminarNivelAprobacion($model)
+		{
+			if ( isset($model) ) {
+				if ( $model->nivel_aprobacion == 2 ) {
+die(var_dump($model));
+				}
+			}
+		}
+
+
+
+
+
+
+
+
+	}
+?>
