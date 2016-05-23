492d0752d4e74b2056a1f4d72f6ee643d888dad5
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-23 17:09:12 +0000

    actualizando

--- simwebpluscar/trunk/backend/views/menu/menuvertical2.php
+++ simwebpluscar/trunk/backend/views/menu/menuvertical2.php
@@ -135,14 +135,9 @@ session_start();
 			        							['label' => 'Asignar Calcomanias a funcionario (Distribuir entre funcionario responsables)','url' => ['/vehiculo/calcomania/administrarcalcomaniafuncionario/administrar-calcomania-funcionario/seleccionar-funcionario']],
 
 
-			        							['label' => 'Administrar Entrega de calcomania','url' => ['/vehiculo/calcomania/asignarcalcomaniacontribuyente/asignar-calcomania-contribuyente/seleccionar-tipo']],
+		                                        ['label' => 'Administrar Entrega de calcomania','url' => ['/vehiculo/calcomania/asignarcalcomaniacontribuyente/asignar-calcomania-contribuyente/seleccionar-tipo']],
 
-
-			        							['label' => 'Administrar Entrega de calcomania','url' => '#'],
-
-			        							['label' => 'Administrar Entrega de calcomania','url' => ['/vehiculo/calcomania/asignarcalcomaniacontribuyente/asignar-calcomania-contribuyente/seleccionar-tipo-contribuyente']],
-
-			        							['label' => 'Cierre de Lote','url' => '#'],
+			        							['label' => 'Cierre de Lote','url' => ['/vehiculo/calcomania/cierrelote/cierre-lote-calcomania/busqueda-lote']],
 					        				],
 					        			],
 		        						['label' => 'Solvencia', 'url' => '#'],
