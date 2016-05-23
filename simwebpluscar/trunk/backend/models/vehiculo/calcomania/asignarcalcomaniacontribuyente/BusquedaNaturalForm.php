c7a025bd4946a671f2bb9209b2d0d31c288c46d0
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-23 16:56:45 +0000

    actualizando

--- simwebpluscar/trunk/backend/models/vehiculo/calcomania/asignarcalcomaniacontribuyente/BusquedaNaturalForm.php
+++ simwebpluscar/trunk/backend/models/vehiculo/calcomania/asignarcalcomaniacontribuyente/BusquedaNaturalForm.php
@@ -25,13 +25,10 @@
  *  
  *  @author Manuel Alejandro Zapata Canelon
  * 
- *  @date 14/05/2016
+ *  @date 13/05/2016
  * 
- *  @class BusquedaNaturalForm
- *  @brief Clase contiene las rules y metodos para la busqueda de contribuyentes naturales para la asignacion de calcomanias 
- * 
- *  
- * 
+ *  @class BusquedaNaturalForm.php
+ *  @brief Clase contiene las rules y metodos para la busqueda de contribuyentes naturales para luego asignarles la calcomania
  *  
  *  
  * @property
@@ -50,11 +47,13 @@ namespace backend\models\vehiculo\calcomania\asignarcalcomaniacontribuyente;
 use Yii;
 use yii\base\Model;
 use common\models\contribuyente\ContribuyenteBase;
-use backend\models\vehiculo\VehiculosSearch;
+use backend\models\vehiculo\VehiculosForm;
 use yii\data\ActiveDataProvider;
-use frontend\models\usuario\CrearUsuarioNatural;
-use common\models\calcomania\calcomaniaentregada\CalcomaniaEntregada;
-use common\models\calcomania\calcomaniamodelo\Calcomania;
+
+
+
+
+
 
 
 
@@ -64,44 +63,20 @@ class BusquedaNaturalForm extends Model
 
   public $naturaleza;
   public $cedula;
-  public $id;
-
-  const SCENARIO_SEARCH_NATURAL = 'search_natural';
-  const SCENARIO_SEARCH_ID = 'search_id';
 
  
-     public function scenarios()
-        {
-            //bypass scenarios() implementation in the parent class
-            //return Model::scenarios();
-            return [
-                self::SCENARIO_SEARCH_NATURAL => [
-                                'naturaleza',
-                                'cedula'
-                ],
-                self::SCENARIO_SEARCH_ID => [
-                                'id',
-                               
-                ],
-             
-            ];
-        }
- 
-
+    
     public function rules()
-    {   //validaciones requeridas para el formulario de busqueda de contribuyentes  
+    {   //validaciones requeridas para el formulario de registro de usuarios  
 
-    // die('llegue a las rules');
+     
 
         return [
-           [['naturaleza', 'cedula'],
-                'required', 'on' => 'search_natural', 'message' => Yii::t('backend', '{attribute} is required')],
-
-            [['id'],
-                'required', 'on' => 'search_id', 'message' => Yii::t('backend', '{attribute} is required')],
+            [['cedula','naturaleza'],'required'],
              
-            [['cedula','id'],'integer'],
+            [['cedula'],'integer'],
 
+            //['tipo', 'validarTipo'] //para utilizar en la busqueda de usuario juridico
             ['cedula', 'validarLongitud'],
              
             
@@ -114,7 +89,7 @@ class BusquedaNaturalForm extends Model
 
         $longitud = strlen($this->cedula);
 
-          if ($longitud > 8 ){
+          if ($longitud > 9 ){
             $this->addError($attribute, Yii::t('frontend', 'The rif must not have more than 9 characters'));
           } else if ($longitud < 6 ){ 
             $this->addError($attribute, Yii::t('frontend', 'The rif must not have less than 6 characters'));
@@ -122,177 +97,14 @@ class BusquedaNaturalForm extends Model
     
     }
 
-    public function buscarNatural($model)
-    {
+    // public function validarTipo($attribute,$params) //para utilizarlo en la busqueda de persona natural
+    // {
+    //     $longitud = strlen($this->tipo);
 
-
-        $buscar = CrearUsuarioNatural::find()
-                                    ->where([
-                                        'naturaleza' =>$model->naturaleza,
-                                        'cedula' => $model->cedula,
-                                        'tipo_naturaleza' => 0,
-                                        'inactivo' => 0,
-                                    ])
-                                    ->one();
-
-                if($buscar == true){
-                    
-                    return $buscar;
-                }else{
-                    return false;
-                }
-    }
-
-    public function buscarId($model)
-    {
-        //die(var_dump($model));
-         $buscar = CrearUsuarioNatural::find()
-                                    ->where([
-                                        'id_contribuyente' => $model->id,
-                                        'tipo_naturaleza' => 0,
-                                        'inactivo' => 0,
-                                    ])
-                                    ->one();
-
-                if($buscar == true){
-                    
-                    return $buscar;
-                }else{
-                    return false;
-                }
-    }
-
-
-
-
-    public function buscarVehiculo($model)
-    {
-     $query = VehiculosSearch::find();
-
-                                
-                             
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-           
-        ]);
-        $query->where([
-            'id_contribuyente' => $model->id_contribuyente,
-            'status_vehiculo' => 0,
-            ])
-  
-        ->all();
-       // die(var_dump($query));
-
-        //die(var_dump($query));
-        return $dataProvider;
-
-    }
-
-    public function buscarPlaca($model)
-    {
-
-
-        $buscar = VehiculosSearch::find()
-                                    ->where([
-                                        'id_vehiculo' =>$model,
-                                        'status_vehiculo' => 0,
-                                        
-                                    ])
-                                    ->all();
-
-                if($buscar == true){
-                    
-                    return $buscar;
-                }else{
-                    return false;
-                }
-    }
-
-    public function buscarPlacaCalcomania($model)
-    {
-       // die($model);
-
-
-        $buscar = CalcomaniaEntregada::find()
-                                    ->where([
-                                        'placa' =>$model,
-                                        'status' => 0,
-                                        
-                                    ])
-                                    ->all();
-
-                if($buscar == true){
-                    
-                    return true;
-                }else{
-                    return false;
-                }
-    }
-
-    public function buscarIdCalcomania($model)
-    {
-        //die($model);
-        $buscar = Calcomania::find()
-                            ->where([
-
-                                'nro_calcomania' => $model,
-                                'ano_impositivo' => date('Y'),
-                                ])
-                            ->all();
-
-            if($buscar == true){
-               // die(var_dump($buscar));
-                return $buscar;
-            }else{
-                return false;
-            }
-    }
-
-    public function verificarCalcomaniaEntregada($model)
-    {
-
-        
-        $buscar = CalcomaniaEntregada::find()
-                                    ->where([
-                                    'id_vehiculo' => $model,
-                                    'ano_impositivo' => date('Y'),
-                                    'status' => 0,
-
-                                      ])
-                                    ->all();
-
-                            if($buscar == true){
-                                return true;
-                            }else{
-                                return false;
-                            }
-    }
-
-    public function searchRango()
-    {
-        $datos = yii::$app->user->identity->id_funcionario;
-        
-        $query = Calcomania::find();
-        
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-           
-        ]);
-        $query->filterWhere([
-            'id_funcionario' => $datos,
-            'entregado' => 0,
-            'estatus' => 0,
-
-            ])
-        
-        ->all();
-
-
-       
-    
-  
-        return $dataProvider;
-    }
+    //         if($longitud > 1){
+    //             $this->addError($attribute, Yii::t('backend', 'Tipo must not have more than 1 character');
+    //         }
+    // }
       
     
     
@@ -302,46 +114,53 @@ class BusquedaNaturalForm extends Model
         return [
                 'cedula' => Yii::t('backend', 'Cedula'), 
                 'naturaleza' => Yii::t('backend', 'Naturaleza'),
-                'id' => yii::t('backend', 'ID'),
+                'tipo' => Yii::t('backend','Tipo'),
                 
         ];
     }
-    
 
-    //contiene todos los campos de la tabla funcionario calcomania
-    public function attributeFuncionarioCalcomania()
+    public function buscarNatural($model)
     {
-        return [
-                'id_funcionario',
-                'estatus',
-                'usuario',
-                'fecha_hora',
-        ];
+        //die(var_dump($model));
+        $buscar = ContribuyenteBase::find()
+                                 ->where([
+                                  'naturaleza' => $model->naturaleza,
+                                  'cedula' => $model->cedula,
+                                  'tipo_naturaleza' => 0,
+                                  'inactivo' => 0,
+
+                                  ])
+                                 ->all();
+
+                  if ($buscar == true){
+                    return $buscar;
+                  }else{
+                    return false;
+                  }
+             
     }
+    public function vehiculoSearch($model)
+    {  
+      //die(var_dump($model[0]->id_contribuyente));
+        $query = VehiculosForm::find();
 
-    public function attributeLoteCalcomaniasEntregadas()
-    {
-        return [
-                'id_contribuyente',
-                'id_vehiculo',
-                'nro_calcomania',
-                'ano_impositivo',
-                'fecha_entrega',
-                'login',
-                'tipo_entrega',
-                'observacion',
-                'planilla',
-                'status',
-                'placa',
-                
-                
-
-        ];
-    }
-
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+           
+        ]);
+        
+            $query->where([
+                'id_contribuyente' => $model[0]->id_contribuyente,
+               // die($model[0]->id_contribuyente),
+                'status_vehiculo' => 0,
+            ])
+  
+            ->all();
+      
+                return $dataProvider;
     
-
-
+       }
+  
 
     
       
