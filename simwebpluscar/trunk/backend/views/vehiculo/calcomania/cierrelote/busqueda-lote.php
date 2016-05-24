ac6c89741b067ec4c1996a4b78f50c617a1b9860
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-24 15:05:35 +0000

    actualizando

--- simwebpluscar/trunk/backend/views/vehiculo/calcomania/cierrelote/busqueda-lote.php
+++ simwebpluscar/trunk/backend/views/vehiculo/calcomania/cierrelote/busqueda-lote.php
@@ -2,6 +2,17 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
+use common\models\calcomania\calcomaniamodelo\Calcomania;
+
+
+    $busquedaAno = Calcomania::find()
+                            ->select('ano_impositivo')
+                            ->distinct()
+                            ->asArray()
+                            ->all();
+
+    
+    $listaAnoImpositivo = ArrayHelper::map($busquedaAno, 'ano_impositivo' , 'ano_impositivo' );
 
  
  
@@ -35,16 +46,17 @@ $this->title = 'Impositive Year Search';
 							
 								
 							<div class="row">
-							<div class="col-sm-3">
-                            <?= $form->field($model, 'ano_impositivo')->textInput(                          [ 'id'=> 'ano´_impositivo', 
-                                                                                                           
-                                                                                                            //'style' => 'width:280px;',
-                                                                                                           // 'value' => $datos[0]['placa']  ,
-                                                                                                           // die(var_dump($preguntaSeguridad[0]['pregunta'])),
-                                                                                                            'readOnly' =>false,                                                                                                          
-                                                                                                            ]); ?>
-						
-							</div>
+					           <div class="col-sm-5" style="padding-right: 12px;">
+                                        <div class="naturaleza">
+                                            <?= $form->field($model, 'ano_impositivo')->dropDownList($listaAnoImpositivo,[
+                                                                                                    'id' => 'ano_impositivo',
+                                                                                                    'prompt' => Yii::t('backend', 'Select'),
+                                                                                                    'style' => 'height:32px;width:150px;',
+                                                                                                    
+                                                                                                    ])
+                                            ?>
+                                        </div>
+                </div>
 							</div>
 						 
 						
