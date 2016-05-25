3a9ddf9ff3988ee5980564d2b80cff9de601a8b5
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-25 20:05:43 +0000

    agregando vehiculo search

---
+++ simwebpluscar/trunk/backend/models/vehiculo/VehiculoSearch.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace backend\models\vehiculo;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\vehiculo\VehiculosForm;
+use backend\models\vehiculo\TiposVehiculos;
+use backend\models\vehiculo\ClasesVehiculos;
+use backend\models\vehiculo\UsosVehiculos;
+
+/**
+ * VehiculoSearch represents the model behind the search form about `backend\models\VehiculosForm`.
+ */
+class VehiculoSearch extends VehiculosForm
+{
+    /**
+     * [getDescripcionTipoVehiculo description]
+     * @param  [type] $tipo [description]
+     * @return [type]       [description]
+     */
+    public function getDescripcionTipoVehiculo($tipo)
+    {
+        $model = TiposVehiculos::findOne($tipo);
+        return $model->descripcion;
+
+    }
+
+    /**
+     * [getDescripcionUsoVehiculo description]
+     * @param  [type] $uso [description]
+     * @return [type]      [description]
+     */
+    public function getDescripcionUsoVehiculo($uso)
+    {
+        $model = UsosVehiculos::findOne($uso);
+        return $model->descripcion;
+
+    }
+
+    /**
+     * [getDescripcionClaseVehiculo description]
+     * @param  [type] $clase [description]
+     * @return [type]        [description]
+     */
+    public function getDescripcionClaseVehiculo($clase)
+    {
+        $model = ClasesVehiculos::findOne($clase);
+        return $model->descripcion;
+
+    }
+   
+}
