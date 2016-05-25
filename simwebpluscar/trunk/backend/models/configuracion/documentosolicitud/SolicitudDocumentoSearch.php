46d2b5bbdd56ab100b6b45fa54a1f6be7f7e1f07
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-25 20:18:47 +0000

    Search de documentos a consignar

---
+++ simwebpluscar/trunk/backend/models/configuracion/documentosolicitud/SolicitudDocumentoSearch.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file SolicitudDocumentoSearch.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 25-05-2016
+ *
+ *  @class SolicitudDocumentoSearch
+ *  @brief Clase Modelo
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *  @inherits
+ *
+ */
+
+	namespace backend\models\configuracion\documentosolicitud;
+
+ 	use Yii;
+	use yii\base\Model;
+	use yii\data\ActiveDataProvider;
+	use backend\models\configuracion\documentosolicitud\SolicitudDocumento;
+
+	/**
+	* 	Clase
+	*/
+	class SolicitudDocumentoSearch extends SolicitudDocumento
+	{
+
+		public function getListaDocumentoSegunSolicitud()
+
+
+	}
+?>
