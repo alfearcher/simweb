74ac341d7c49f00fdbd42ac1e6bf2b470e97f1ae
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-30 17:51:13 +0000

    prueba

--- simwebpluscar/trunk/backend/models/configuracion/documentosolicitud/SolicitudDocumentoSearch.php
+++ simwebpluscar/trunk/backend/models/configuracion/documentosolicitud/SolicitudDocumentoSearch.php
@@ -104,7 +104,8 @@
 												   ->joinWith('solicitudDocumento', false)
 												   ->orderBy([
 												   		'id_documento' => SORT_ASC,
-												   	]);
+												   	])
+												   ->all();
 			return isset($modelFind) ? $modelFind : null;
 		}
 
