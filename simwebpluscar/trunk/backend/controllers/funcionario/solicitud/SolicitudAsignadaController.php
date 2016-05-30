c5f57d0c4896c869f771ccb0d8b907213fc401a9
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-05-30 18:24:33 +0000

    Colocando la rutina para obtener los Documentos y Requisitos que se mostraran en el procesamiento de la solicitud.

--- simwebpluscar/trunk/backend/controllers/funcionario/solicitud/SolicitudAsignadaController.php
+++ simwebpluscar/trunk/backend/controllers/funcionario/solicitud/SolicitudAsignadaController.php
@@ -63,6 +63,7 @@
 	use backend\models\impuesto\ImpuestoForm;
 	use backend\models\configuracion\tiposolicitud\TipoSolicitud;
 	use common\models\solicitudescontribuyente\DetalleSolicitudCreada;
+	use backend\models\configuracion\documentosolicitud\SolicitudDocumentoSearch;
 
 
 
@@ -186,6 +187,9 @@ die(var_dump($postData));
 						$viewDetalle = $detalle->getDatosSolicitudCreada();
 
 						if ( $viewDetalle !== false ) {
+							// Se buscan los Documentos y Requisitos de la Solicitud.
+							$modelDoc = New SolicitudDocumentoSearch($id);
+							$dataProviderDoc = $modelDoc->getDataProvider();
 							return $this->render('/funcionario/solicitud-asignada/_view', [
 																					'model' => $infoSolicitud,
 																					'caption' => $caption,
@@ -194,6 +198,8 @@ die(var_dump($postData));
 																					'url' => $url,
 																					'contribuyente' => $contribuyente,
 																					'viewDetalle' => $viewDetalle,
+																					'modelDoc' => $modelDoc,
+																					'dataProviderDoc' => $dataProviderDoc,
 								]);
 						} else {
 							// No se encontraron los detalles de la solicitud.
