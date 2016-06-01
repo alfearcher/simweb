5d4e063753c34467e8487df7d3206ee43b6e1153
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-06-01 19:49:14 +0000

    Actualizacion

--- simwebpluscar/trunk/common/models/planilla/PlanillaSearch.php
+++ simwebpluscar/trunk/common/models/planilla/PlanillaSearch.php
@@ -74,24 +74,37 @@
 
 
 
-		/***/
-		protected function findPlanilla()
-		{
-			$modelFind = PagoDetalle::find()->where('planilla =:planilla', [':planilla' => $this->_planilla])
-											->joinWith('pagos')
-											->orderBy([
-													'ano_impositivo' => SORT_ASC,
-													'trimestre' => SORT_ASC,
-												]);
-			return isset($modelFind) ? $modelFind : null;
-		}
-
-
-
-
+		/**
+		 * Metodo que permite obtener una informacion resumida de la planilla.
+		 * Dicha informacion esta representada por totales por los diferentes conceptos,
+		 * descripcion de los periodos, observacion presente en la planilla, descripcion del impuesto,
+		 * estatus de pago, numero de planilla, id del contribuyente.
+		 * @return Array Retorna un arreglo multidimensional con los datos mencionados.
+		 */
 		public function getResumenGeneral()
 		{
-			return $m = self::findPlanilla();
+			$query = New Query();
+
+			$query->select(['P.planilla',
+							'P.id_contribuyente',
+							'SUM(D.monto) as sum_monto',
+							'SUM(D.recargo) as sum_recargo',
+							'SUM(D.interes) as sum_interes',
+							'SUM(D.descuento) as sum_descuento',
+							'SUM(D.monto_reconocimiento) as sum_monto_reconocimiento',
+							'D.pago',
+							'I.descripcion as descripcion_impuesto',
+							'D.descripcion',
+							'E.unidad'
+							])
+				  ->from('pagos as P')
+				  ->join('INNER JOIN', 'pagos_detalle as D', 'P.id_pago = D.id_pago')
+				  ->join('INNER JOIN', 'impuestos as I', 'D.impuesto = I.impuesto')
+				  ->join('INNER JOIN', 'exigibilidades as E', 'D.exigibilidad_pago = E.exigibilidad')
+				  ->where('planilla =:planilla', [':planilla' => $this->_planilla])
+				  ->groupBy('P.planilla');
+
+			return $query->one();
 		}
 
 
