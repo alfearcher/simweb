720358943c3d0716a8d4a3513f099dc5f50d1693
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-06-07 00:46:15 +0000

    prueba

--- simwebpluscar/trunk/backend/views/planilla/planilla-detalle.php
+++ simwebpluscar/trunk/backend/views/planilla/planilla-detalle.php
@@ -73,7 +73,7 @@
 	?>
 
 	<meta http-equiv="refresh">
-    <div class="panel panel-default"  style="width: 200%;">
+    <div class="panel panel-default"  style="width: 140%;">
         <div class="panel-heading">
         	<div class="row">
         		<div class="col-sm-4" style="padding-top: 10px;">
