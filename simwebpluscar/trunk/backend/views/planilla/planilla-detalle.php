57375bedf4ea38a13c47070e822cb6d388221475
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-06-14 17:29:33 +0000

    Actualizar

--- simwebpluscar/trunk/backend/views/planilla/planilla-detalle.php
+++ simwebpluscar/trunk/backend/views/planilla/planilla-detalle.php
@@ -182,9 +182,13 @@
 				                            //
 				                            'value' => function($data) {
 				                            	if ( $data['pago'] == 0 ) {
-				                            		return Html::tag('strong', 'NO', ['class' => 'label label-danger']);
+				                            		return Html::tag('strong', Html::tag('h3',
+						                            								   	 'NO',
+						                            								   	 ['class' => 'label label-danger']));
 				                            	} elseif ( $data['pago'] == 1) {
-				                            		return Html::tag('strong', $data['fecha_pago'], ['class' => 'success']);
+				                            		return Html::tag('strong', Html::tag('h3',
+						                            			                     	 'SI',
+						                            			                     	 ['class' => 'success']));
 				                            	}
 				                            },
 				                        ],
