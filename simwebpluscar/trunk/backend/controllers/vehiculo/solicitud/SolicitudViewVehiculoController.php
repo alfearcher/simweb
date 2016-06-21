c4d589e3b926d4150dc5d36de85911a9a53aed1f
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-06-21 20:43:41 +0000

    actualizando

---
+++ simwebpluscar/trunk/backend/controllers/vehiculo/solicitud/SolicitudViewVehiculoController.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ *	@copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *	> This library is free software; you can redistribute it and/or modify it under
+ *	> the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *	> Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *	> any later version.
+ *  >
+ *	> This library is distributed in the hope that it will be usefull,
+ *	> but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *	> or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *	> for more details.
+ *  >
+ *	> See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *	@file SolicitudViewnActividadEconomicaController.php
+ *
+ *	@author Jose Rafael Perez Teran
+ *
+ *	@date 19-09-2015
+ *
+ *  @class SolicitudViewnActividadEconomicaController
+ *	@brief Clase
+ *
+ *
+ *	@property
+ *
+ *
+ *	@method
+ *
+ *
+ *	@inherits
+ *
+ */
+
+
+ 	namespace backend\controllers\vehiculo\solicitud;
+
+
+ 	use Yii;
+	use yii\filters\AccessControl;
+	use yii\web\Controller;
+	use yii\filters\VerbFilter;
+	use yii\widgets\ActiveForm;
+	use yii\web\Response;
+	use yii\helpers\Url;
+	use yii\web\NotFoundHttpException;
+	use frontend\models\vehiculo\solicitudes\SlVehiculos;
+	use frontend\models\vehiculo\solicitudes\SlVehiculosForm;
+	// use common\conexion\ConexionController;
+	use backend\controllers\MenuController;
+	use backend\models\vehiculo\VehiculoSearch;
+
+
+	//session_start();		// Iniciando session
+
+
+	/**
+	 * Clase que permite renderizar las visatas detalles de las solicitudes por tipo de solicitud.
+	 * Las solicitudes aqui representan las del impuesto de Actividades Economicas. El resultado es
+	 * una vista con la informacion del detalle de la solicitud. El constructor de la clase recibe
+	 * el modelo que posee entre sus datos el tipo de solicitud o id-tipo-solicitud.
+	 */
+	class SolicitudViewVehiculoController extends Controller
+	{
+
+		private $model;
+
+
+
+		/**
+		 * Constructor de la clase.
+		 * @param model $mod modelo de la solicitud creada.
+		 */
+		public function __construct($mod)
+		{
+			$this->model = $mod;
+		}
+
+
+		/**
+		 * Metodo que sirve de indice de renderizacion para la vista detalle particular.
+		 * Aqui segun el parametro tipo de solicitud se renderiza hacia un metodo que se
+		 * encargara de buscar los detalles de la solicitud y generar una vista.
+		 * @return View Retorna una vista con la informacion del detalle de la solicitud o
+		 * en caso de no encontrar nada false.
+		 */
+		public function actionInicioView()
+		{
+			//die('llego');
+			if ( isset($this->model) && isset($_SESSION['idContribuyente']) ) {
+
+				if ( $this->model->tipo_solicitud == 32 ) {
+
+					return self::actionMostarSolicitudInscripcionVehiculo();
+
+				} elseif ( $this->model->tipo_solicitud == 35 ) {
+
+					return self::actionMostarSolicitudCambioPropietarioVendedorVehiculo();
+
+				} elseif ( $this->model->tipo_solicitud == 3 ) {
+
+				} elseif ( $this->model->tipo_solicitud == 4 ) {
+
+				} elseif ( $this->model->tipo_solicitud == 6 ) {
+
+				} elseif ( $this->model->tipo_solicitud == 7 ) {
+
+				} elseif ( $this->model->tipo_solicitud == 8 ) {
+
+				} elseif ( $this->model->tipo_solicitud == 10 ) {
+
+				} elseif ( $this->model->tipo_solicitud == 12 ) {
+
+				}
+			}
+
+			return false;
+		}
+
+
+
+		/**
+		 * Metodo particular que se encarga de buscar los datos de la solicitud particular sobre
+		 * "Inscripcion de Vehiculos", y de renderizar una vista del detalle de la solicitud
+		 * Se utiliza un parametro adicional "nivel de aprobacion", este determinara un nivel mas
+		 * determinante de la vista.
+		 * El nivel de aprobacion 3 renderizara un formulario con los datos originales de la solicitud
+		 * inhabilitados y solo permitira la edicion de los campos que no fueron cargados en dicha
+		 * solicitud, esto con la intencion de que el funcionario pueda complementar dicha informacion.
+		 * @return View Retorna un vista con la informacion de la solicitud sino encuentra dicha
+		 * informacion retornara false.
+		 * ---
+		 * nivel de aprobacion 1: No aplica.
+		 * nivel de aprobacion 2: la vista no permite la edicion de los campos.
+		 * 	- Esquema de esta vista:
+		 *  	* Nombre del campo : Valor del campo
+		 * nivel de aprobacion 3: Muestra inhabilitado los datos suministrados previamente y habilita
+		 * aquellos campos que no fueron cargados inicialmente.  
+		 */
+		private function actionMostarSolicitudInscripcionVehiculo()
+		{
+			if ( $this->model->nivel_aprobacion == 2 ) {
+					$modelSearch = New SlVehiculosForm($this->model->id_contribuyente);
+					$model = $modelSearch->findInscripcion($this->model->nro_solicitud);
+
+					$search = new VehiculoSearch();
+
+
+					return $this->render('/vehiculo/solicitudes/inscripcion/view-solicitud', [
+													'caption' => Yii::t('frontend', 'Request Nro. ' . $this->model->nro_solicitud),
+													'model' => $model,
+													'search' => $search,
+
+						]);
+			}
+
+			return false;
+		}
+
+
+			/**
+		 * Metodo particular que se encarga de buscar los datos de la solicitud particular sobre
+		 * "Cambio de propietarios del Vehiculo como vendedor", y de renderizar una vista del detalle de la solicitud
+		 * Se utiliza un parametro adicional "nivel de aprobacion", este determinara un nivel mas
+		 * determinante de la vista.
+		 * El nivel de aprobacion 3 renderizara un formulario con los datos originales de la solicitud
+		 * inhabilitados y solo permitira la edicion de los campos que no fueron cargados en dicha
+		 * solicitud, esto con la intencion de que el funcionario pueda complementar dicha informacion.
+		 * @return View Retorna un vista con la informacion de la solicitud sino encuentra dicha
+		 * informacion retornara false.
+		 * ---
+		 * nivel de aprobacion 1: No aplica.
+		 * nivel de aprobacion 2: la vista no permite la edicion de los campos.
+		 * 	- Esquema de esta vista:
+		 *  	* Nombre del campo : Valor del campo
+		 * nivel de aprobacion 3: Muestra inhabilitado los datos suministrados previamente y habilita
+		 * aquellos campos que no fueron cargados inicialmente.  
+		 */
+		private function actionMostarSolicitudCambioPropietarioVendedorVehiculo()
+		{
+			if ( $this->model->nivel_aprobacion == 2 ) {
+					$modelSearch = New SlVehiculosForm($this->model->id_contribuyente);
+					$model = $modelSearch->findInscripcion($this->model->nro_solicitud);
+
+					$search = new VehiculoSearch();
+
+
+					return $this->render('/vehiculo/solicitudes/inscripcion/view-solicitud-propietario-vendedor', [
+													'caption' => Yii::t('frontend', 'Request Nro. ' . $this->model->nro_solicitud),
+													'model' => $model,
+													'search' => $search,
+
+						]);
+			}
+
+			return false;
+		}
+
+
+
+
+
+
+	}
+?>
