1db89750bfd1d6b530a3977a646eae61fb793d1f
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-06-28 20:14:01 +0000

    agregando vistas para mostrar detalles de solicitud como comprador y vendedor de vehiculos

--- simwebpluscar/trunk/backend/views/vehiculo/solicitudes/cambiopropietario/view-solicitud-cambio-propietario-vendedor.php
+++ simwebpluscar/trunk/backend/views/vehiculo/solicitudes/cambiopropietario/view-solicitud-cambio-propietario-vendedor.php
@@ -21,13 +21,13 @@
  */
 
  /**
- *  @file view-solicitud-cambio-propietario.php
+ *  @file view-solicitud-cambio-propietario-vendedor.php
  *
  *  @author Manuel Zapata
  *
  *  @date 21-06-2016
  *
- *  @view view-solicitud-cambio-propietario.php
+ *  @view view-solicitud-cambio-propietario-vendedor.php
  *
  *
  *  @property
@@ -59,56 +59,56 @@
 						'model' => $model,
 						
 		    			'attributes' => [
-		    				'nro_solicitud',
-		    				'id_contribuyente',
-		    				'placa',
-		    				'marca',
-		    				'modelo',
-		    				'color',
+		    				'id_propietario',
+		    				'id_comprador',
+		    				// 'placa',
+		    				// 'marca',
+		    				// 'modelo',
+		    				// 'color',
 		    			
-		    				'precio_inicial',
-		    				'fecha_inicio',
-		    				'ano_compra',
-		    				'ano_vehiculo',
-		    				'no_ejes',
-		    				'liquidado',
-		    				'status_vehiculo',
-		    				'exceso_cap',
-		    				'medida_cap',
-		    				'capacidad',
-		    				'nro_puestos',
-		    				'peso',
+		    				// 'precio_inicial',
+		    				// 'fecha_inicio',
+		    				// 'ano_compra',
+		    				// 'ano_vehiculo',
+		    				// 'no_ejes',
+		    				// 'liquidado',
+		    				// 'status_vehiculo',
+		    				// 'exceso_cap',
+		    				// 'medida_cap',
+		    				// 'capacidad',
+		    				// 'nro_puestos',
+		    				// 'peso',
 		    				
-		    				'serial_motor',
-		    				'serial_carroceria',
-		    				'nro_calcomania',
+		    				// 'serial_motor',
+		    				// 'serial_carroceria',
+		    				// 'nro_calcomania',
 		    				
-		    				'nro_cilindros',
-		    				'fecha_hora',
+		    				// 'nro_cilindros',
+		    				// 'fecha_hora',
 		    			
-		    				[
-		    					'label' => 'Uso Vehiculo',
+		    				// [
+		    				// 	'label' => 'Uso Vehiculo',
 
-		    					'value' => //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
-		    					 VehiculoSearch::getDescripcionUsoVehiculo($model->uso_vehiculo),
+		    				// 	'value' => //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+		    				// 	 VehiculoSearch::getDescripcionUsoVehiculo($model->uso_vehiculo),
 		    						
-		    				],
+		    				// ],
 
-		    				[
-		    					'label' => 'Tipo Vehiculo',
+		    				// [
+		    				// 	'label' => 'Tipo Vehiculo',
 
-		    					'value' => //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
-		    					 VehiculoSearch::getDescripcionTipoVehiculo($model->tipo_vehiculo),
+		    				// 	'value' => //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+		    				// 	 VehiculoSearch::getDescripcionTipoVehiculo($model->tipo_vehiculo),
 		    						
-		    				],
+		    				// ],
 
-		    				[
-		    					'label' => 'Clase Vehiculo',
+		    				// [
+		    				// 	'label' => 'Clase Vehiculo',
 
-		    					'value' => //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
-		    					 VehiculoSearch::getDescripcionClaseVehiculo($model->clase_vehiculo),
+		    				// 	'value' => //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+		    				// 	 VehiculoSearch::getDescripcionClaseVehiculo($model->clase_vehiculo),
 		    						
-		    				],
+		    				// ],
 
 		        				
 		    			],
