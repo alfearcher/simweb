e28c79dafb56be7fc4ba39a08ac27d0039aeec9d
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-07-13 20:40:26 +0000

    Vista con lista de solicitudes creadas

---
+++ simwebpluscar/trunk/frontend/views/solicitud/busqueda-solicitud/solicitud-creada-list.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file solicitud-creada-list.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 13-07-2016
+ *
+ *  @view solicitud-creada-list.php
+ *  @brief vista que muestra un listado de solicitudes creadas y pendientes.
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *
+ *  @inherits
+ *
+ */
+
+	use yii\helpers\Html;
+	use yii\helpers\Url;
+	use yii\helpers\ArrayHelper;
+	use yii\widgets\ActiveForm;
+	use kartik\icons\Icon;
+	use yii\web\View;
+	use yii\jui\DatePicker;
+	use backend\controllers\menu\MenuController;
+
+?>
+<div class="solicitud-creada-list">
+	<?php
+		$form = ActiveForm::begin([
+			'id' => 'id-solicitud-creada-list',
+		    'method' => 'post',
+			'enableClientValidation' => true,
+			'enableAjaxValidation' => true,
+			'enableClientScript' => true,
+		]);
+	?>
+	<?= $form->field($model, 'id_contribuyente')->hiddenInput([
+															'id' => 'id-contribuyente',
+															'value' => $idContribuyente,
+														])->label(false);?>
+
+	<meta http-equiv="refresh">
+    <div class="panel panel-default"  style="width: 80%;">
+        <div class="panel-heading">
+        	<div class="row">
+        		<div class="col-sm-4" style="padding-top: 10px;">
+        			<h4><?= Html::encode($caption) ?></h4>
+        		</div>
+        		<div class="col-sm-3" style="width: 30%; float:right; padding-right: 50px;">
+        			<style type="text/css">
+						.col-sm-3 > ul > li > a:hover {
+							background-color: #F5F5F5;
+						}
+    			</style>
+	        		<?= MenuController::actionMenuSecundario($opciones); ?>
+	        	</div>
+        	</div>
+        </div>
+		<div class="panel-body">
+			<div class="container-fluid">
+				<div class="col-sm-12">
+
+					<div class="row" style="border-bottom: 0.5px solid #ccc;">
+						<h4><strong><?= Html::encode($caption) ?></strong></h4>
+					</div>
+
+
+
+
+<!-- Fin de busqueda de todos los funcionarios -->
+
+				</div>
+			</div>	<!-- Fin de container-fluid -->
+		</div>		<!-- Fin de panel-body -->
+	</div>			<!-- Fin de panel panel-default -->
+
+	<?php ActiveForm::end(); ?>
+</div>
+
+
