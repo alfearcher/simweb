bdcafbf06247a8417a4706a01fdfcecab63d0fa0
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-07-21 13:59:37 +0000

    Se agrego metodo para obtener la descripcion del estatus de la solicitud

--- simwebpluscar/trunk/backend/models/aaee/inscripcionactecon/InscripcionActividadEconomica.php
+++ simwebpluscar/trunk/backend/models/aaee/inscripcionactecon/InscripcionActividadEconomica.php
@@ -1,44 +1,44 @@
-<?php 
+<?php
 /**
  *  @copyright © by ASIS CONSULTORES 2012 - 2016
  *  All rights reserved - SIMWebPLUS
  */
 
  /**
- * 
- *  > This library is free software; you can redistribute it and/or modify it under 
- *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free 
- *  > Software Foundation; either version 2 of the Licence, or (at your opinion) 
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
  *  > any later version.
- *  > 
- *  > This library is distributed in the hope that it will be usefull, 
- *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability 
- *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence 
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
  *  > for more details.
- *  > 
+ *  >
  *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
  *
  */
 
- /**    
+ /**
  *  @file InscripcionActividadEconomicaForm.php
- *  
+ *
  *  @author Jose Rafael Perez Teran
- * 
+ *
  *  @date 19-09-2015
- * 
+ *
  *  @class InscripcionActividadEconomica
- *  @brief Clase Modelo que maneja la politica de validaciones del formulario que se 
+ *  @brief Clase Modelo que maneja la politica de validaciones del formulario que se
  * 	@brief utiliza la
- * 
- *  
+ *
+ *
  *  @property
  *
- *  
+ *
  *  @method
- *  
+ *
  *  @inherits
- *  
+ *
  */
 
 	namespace backend\models\aaee\inscripcionactecon;
@@ -46,6 +46,7 @@
  	use Yii;
 	use yii\base\Model;
 	use yii\db\ActiveRecord;
+	use common\models\solicitudescontribuyente○\SolicitudesContribuyente;
 
 	/**
 	* 	Clase
@@ -71,6 +72,18 @@
 		public static function tableName()
 		{
 			return 'sl_inscripciones_act_econ';
+		}
+
+
+
+		/**
+		 * Metodo para obtener la descripcion del estatus de la solicitud.
+		 * @param  integer $estatus indica el estatus de la solicitud-inscripcion.
+		 * @return string retorna la descripcion de la solicitud-inscripcion.
+		 */
+		public function getDescripcionEstatusInscripcion($estatus)
+		{
+			return SolicitudesContribuyente::getDescripcionEstatus($estatus);
 		}
 
 
