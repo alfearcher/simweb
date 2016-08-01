aff21e0416c3daa5021c617e2e1f5c6f2da8d177
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-08-01 01:13:56 +0000

    Actualizacion

--- simwebpluscar/trunk/backend/models/aaee/correcciondomicilio/CorreccionDomicilioFiscalForm.php
+++ simwebpluscar/trunk/backend/models/aaee/correcciondomicilio/CorreccionDomicilioFiscalForm.php
@@ -125,7 +125,7 @@
     			['fecha_hora', 'default', 'value' => date('Y-m-d H:i:s')],
     			['estatus', 'default', 'value' => 0],
 	     		['usuario', 'default', 'value' => Yii::$app->user->identity->login, 'on' => 'frontend'],
-	     		['usuario', 'default', 'value' => Yii::$app->user->identity->username, 'on' => 'backend'],
+	     		//['usuario', 'default', 'value' => Yii::$app->user->identity->username, 'on' => 'backend'],
 	     		['origen', 'default', 'value' => 'WEB', 'on' => 'frontend'],
 	     		['origen', 'default', 'value' => 'LAN', 'on' => 'backend'],
 
