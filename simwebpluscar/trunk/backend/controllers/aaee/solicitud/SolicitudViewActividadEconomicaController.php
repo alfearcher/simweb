d6a34a2dabafa25d44ef8abec9e4d8e5c0101993
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-08-08 22:45:27 +0000

    Actualizar

--- simwebpluscar/trunk/backend/controllers/aaee/solicitud/SolicitudViewActividadEconomicaController.php
+++ simwebpluscar/trunk/backend/controllers/aaee/solicitud/SolicitudViewActividadEconomicaController.php
@@ -58,6 +58,7 @@
 	use backend\models\aaee\correcciondomicilio\CorreccionDomicilioFiscalSearch;
 	use backend\models\aaee\correccioncedularif\CorreccionCedulaRifSearch;
 	use backend\models\aaee\correccioncapital\CorreccionCapitalSearch;
+	use backend\models\aaee\correccionreplegal\CorreccionRepresentanteLegalSearch;
 	use backend\controllers\MenuController;
 
 	//session_start();		// Iniciando session
@@ -108,6 +109,10 @@
 				} elseif ( $this->_model->tipo_solicitud == 13 ) {
 
 					return self::actionMostarSolicitudCorreccionDomicilioFiscal();
+
+				} elseif ( $this->_model->tipo_solicitud == 14 ) {
+
+					return self::actionMostarSolicitudCorreccionRepresentanteLegal();
 
 				} elseif ( $this->_model->tipo_solicitud == 15 ) {
 
@@ -325,6 +330,44 @@
 		}
 
 
+
+
+		/**
+		 * Metodo particular que se encarga de buscar los datos de la solicitud particular sobre
+		 * "Correccion de Representante Legal", y de renderizar una vista del detalle de la solicitud
+		 * Se utiliza un parametro adicional "nivel de aprovacion", este determinara un nivel mas
+		 * determinante de la vista.
+		 * El nivel de aprobacion 3 renderizara un formulario con los datos originales de la solicitud
+		 * inhabilitados y solo permitira la edicion de los campos que no fueron cargados en dicha
+		 * solicitud, esto con la intencion de que el funcionario pueda complementar dicha informacion.
+		 * @return view retorna un vista con la informacion de la solicitud sino encuentra dicha
+		 * informacion retornara false.
+		 * ---
+		 * nivel de aprobacion 1: No aplica.
+		 * nivel de aprobacion 2: la vista no permite la edicion de los campos.
+		 * 	- Esquema de esta vista:
+		 *  	* Nombre del campo : Valor del campo
+		 * nivel de aprobacion 3: Muestra inhabilitado los datos suministrados previamente y habilita
+		 * aquellos campos que no fueron cargados inicialmente.
+		 */
+		private function actionMostarSolicitudCorreccionRepresentanteLegal()
+		{
+			if ( $this->_model->nivel_aprobacion == 2 ) {
+					$modelSearch = New CorreccionRepresentanteLegalSearch($this->_model->id_contribuyente);
+					$model = $modelSearch->findSolicitudCorreccionRepresentanteLegal($this->_model->nro_solicitud);
+					$dataProvider = $modelSearch->getDataProviderSolicitud($this->_model->nro_solicitud);
+					if ( isset($model) ) {
+						return $this->render('@backend/views/aaee/correccion-representante-legal/view-solicitud', [
+														'caption' => Yii::t('frontend', 'Request Nro. ' . $this->_model->nro_solicitud),
+														'model' => $model,
+														'dataProvider' => $dataProvider,
+
+							]);
+					}
+			}
+
+			return false;
+		}
 
 
 
