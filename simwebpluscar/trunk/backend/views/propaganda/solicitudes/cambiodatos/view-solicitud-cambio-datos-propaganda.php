5fb68468820460c7f730bad3d9def21ab8e1807f
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-08-24 20:42:11 +0000

    actualizando

--- simwebpluscar/trunk/backend/views/propaganda/solicitudes/cambiodatos/view-solicitud-cambio-datos-propaganda.php
+++ simwebpluscar/trunk/backend/views/propaganda/solicitudes/cambiodatos/view-solicitud-cambio-datos-propaganda.php
@@ -57,7 +57,8 @@
 <div class="row" style="width: 50%;">
 	<div class="info-solicitud">
 		<div class="row">
-			<h3><?= Html::encode($caption) ?></h3>
+			<div class="col-sm-5">
+			<h3><?= Html::encode('Datos Antiguos') ?></h3>
 				<?= DetailView::widget([
 						'model' => $model,
 						
@@ -200,6 +201,154 @@
 		    			],
 					])
 				?>
+			</div>
+
+			<div class="col-sm-5" style="margin-left: 70px; ">
+				<h3><?= Html::encode('Datos Nuevos') ?></h3>
+				<?= DetailView::widget([
+
+						'model' => $model,
+						
+		    			'attributes' => [
+		    				'ano_impositivo',
+		    				'direccion',
+		    				'nombre_propaganda',
+		    				'nro_solicitud',
+
+
+		    				
+
+
+						   	[
+					        'label' => 'Clase Propaganda',
+
+					        'value' => $model->descripcionClasePropaganda->descripcion, //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+					            
+					            
+					     
+					         ],
+
+					        	  [
+					        'label' => 'Tipo Propaganda',
+
+					        'value' => $model->descripcionTipoPropaganda->descripcion, //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+					            
+					            
+					     
+					        ],
+
+					        [
+					        'label' => 'Uso Propaganda',
+
+					        'value' => $model->descripcionUsoPropaganda->descripcion, //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+					            
+					            
+					     
+					        ],
+
+					        [
+					        'label' => 'Medio de Difusion',
+
+					        'value' => $model->descripcionMedioDifusionPropaganda->descripcion, //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+					            
+					            
+					     
+					        ],
+
+					            [
+					        'label' => 'Medio de Transporte',
+
+					        'value' => $model->descripcionMedioTransportePropaganda->descripcion, //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+					            
+					            
+					     
+					        ],
+
+					        'fecha_desde',
+					        'cantidad_tiempo',
+					        'cantidad_base',
+					        'alto',
+					        'ancho',
+					        'profundidad',
+					        'unidad',
+
+					        [
+					        'label' => 'Cigarrillos',
+					     	
+					        'value' => $model['cigarros'] == 1 ? 'SI' : 'NO',
+					        ],
+		    				
+		    				[
+					        'label' => 'Bebidas Alcoholicas',
+					     	
+					        'value' => $model['bebidas_alcoholicas'] == 1 ? 'SI' : 'NO',
+					        ],
+
+					        [
+					        'label' => 'Idioma',
+					     	
+					        'value' => $model['idioma'] == 1 ? 'SI' : 'NO',
+					        ],
+		    			
+		    				'observacion',
+
+		    				'fecha_fin',
+
+		    				'usuario',
+
+
+		    				
+		    				// 'color',
+		    			
+		    				// 'precio_inicial',
+		    				// 'fecha_inicio',
+		    				// 'ano_compra',
+		    				// 'ano_vehiculo',
+		    				// 'no_ejes',
+		    				// 'liquidado',
+		    				// 'status_vehiculo',
+		    				// 'exceso_cap',
+		    				// 'medida_cap',
+		    				// 'capacidad',
+		    				// 'nro_puestos',
+		    				// 'peso',
+		    				
+		    				// 'serial_motor',
+		    				// 'serial_carroceria',
+		    				// 'nro_calcomania',
+		    				
+		    				// 'nro_cilindros',
+		    				// 'fecha_hora',
+		    			
+		    				// [
+		    				// 	'label' => 'Uso Vehiculo',
+
+		    				// 	'value' => //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+		    				// 	 VehiculoSearch::getDescripcionUsoVehiculo($model->uso_vehiculo),
+		    						
+		    				// ],
+
+		    				// [
+		    				// 	'label' => 'Tipo Vehiculo',
+
+		    				// 	'value' => //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+		    				// 	 VehiculoSearch::getDescripcionTipoVehiculo($model->tipo_vehiculo),
+		    						
+		    				// ],
+
+		    				// [
+		    				// 	'label' => 'Clase Vehiculo',
+
+		    				// 	'value' => //$search->getDescripcionUsoVehiculo($model->uso_vehiculo),//function($model){
+		    				// 	 VehiculoSearch::getDescripcionClaseVehiculo($model->clase_vehiculo),
+		    						
+		    				// ],
+
+		        				
+		    			],
+					])
+				?>
+			</div>
 		</div>
 	</div>
 </div>
