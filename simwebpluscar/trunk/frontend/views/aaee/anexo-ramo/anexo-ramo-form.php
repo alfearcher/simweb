afd279a55a0b63fa33614e804096522a6c526d72
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-08-28 05:33:40 +0000

    Actualizar

---
+++ simwebpluscar/trunk/frontend/views/aaee/anexo-ramo/anexo-ramo-form.php
@@ -0,0 +1,234 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file anexo-ramo-form.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 27-08-2016
+ *
+ *  @view anexo-ramo-form.php
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *
+ *  @inherits
+ *
+ */
+
+ 	//use yii\web\Response;
+ 	use kartik\icons\Icon;
+ 	use yii\grid\GridView;
+	use yii\helpers\Html;
+	use yii\helpers\Url;
+	use yii\helpers\ArrayHelper;
+	use yii\widgets\ActiveForm;
+	use yii\web\View;
+	use yii\jui\DatePicker;
+	use yii\widgets\Pjax;
+	use yii\widgets\DetailView;
+	use backend\controllers\utilidad\documento\DocumentoRequisitoController;
+
+	$typeIcon = Icon::FA;
+  	$typeLong = 'fa-2x';
+
+    Icon::map($this, $typeIcon);
+
+ ?>
+
+
+ <div class="anexo-ramo-form">
+ 	<?php
+
+ 		$form = ActiveForm::begin([
+ 			'id' => 'id-anexo-ramo-form',
+ 			'method' => 'post',
+ 			'enableClientValidation' => true,
+ 			'enableAjaxValidation' => false,
+ 			'enableClientScript' => true,
+ 		]);
+ 	?>
+
+	<?=$form->field($model, 'id_contribuyente')->hiddenInput(['value' => $datos['id_contribuyente']])->label(false);?>
+
+	<meta http-equiv="refresh">
+    <div class="panel panel-primary"  style="width: 110%;">
+        <div class="panel-heading">
+        	<h3><?= Html::encode($this->title) ?></h3>
+        </div>
+
+<!-- Cuerpo del formulario -->
+        <div class="panel-body" style="background-color: #F9F9F9;">
+        	<div class="container-fluid">
+        		<div class="col-sm-12">
+<!--  -->
+		        	<div class="row">
+						<div class="panel panel-success" style="width: 103%;margin-left: -15px;">
+							<div class="panel-heading">
+					        	<span><?= Html::encode(Yii::t('frontend', 'Info of Taxpayer')) ?></span>
+					        </div>
+					        <div class="panel-body">
+					        	<div class="row">
+<!-- Datos del Contribuyente -->
+									<div class="col-sm-4" style="margin-left: 15px;width: 90%">
+										<div class="row">
+											<div class="id-contribuyente">
+												<?= DetailView::widget([
+														'model' => $model,
+									    				'attributes' => [
+										    				[
+										    					'label' => $model->getAttributeLabel('id_contribuyente'),
+										    					'value' => $datos['id_contribuyente'],
+										    				],
+										    				[
+										    					'label' => $model->getAttributeLabel('dni'),
+										    					'value' => $datos['naturaleza'] . '-'. $datos['cedula'] . '-'. $datos['cedula'],
+										    				],
+										    				[
+										    					'label' => $model->getAttributeLabel('razon_social'),
+										    					'value' => $datos['razon_social'],
+										    				],
+										    				[
+										    					'label' => $model->getAttributeLabel('domicilio_fiscal'),
+										    					'value' => $datos['domicilio_fiscal'],
+										    				],
+										    				[
+										    					'label' => $model->getAttributeLabel('id_sim'),
+										    					'value' => $datos['id_sim'],
+										    				],
+										    			],
+													])
+												?>
+											</div>
+										</div>
+									</div>
+<!-- Fin de datos del Contribuyente -->
+
+								</div> 		<!-- Fin de row -->
+							</div>
+						</div>
+					</div>
+
+
+					<div class="row">
+						<div class="panel panel-success" style="width: 103%;margin-left: -15px;">
+							<div class="panel-heading">
+					        	<span><?= Html::encode(Yii::t('frontend', 'Fiscal Year/Period')) ?></span>
+					        </div>
+					        <div class="panel-body">
+					        	<div class="row">
+									<div class="col-sm-2" style="width: 12%;">
+										<div class="ano-impositivo">
+					                		<?= $form->field($model, 'ano_impositivo')->dropDownList($listaAño,[
+	                																	 			'id' => 'id-ano-impositivo',
+	                																	 			'name' => 'ano-impositivo',
+	                																	 			'style' => 'width: 100%;',
+	                                                                     				 			'prompt' => Yii::t('backend', 'Select..'),
+	                                                                     				 			'onchange' => '$.post( "' . Yii::$app->urlManager
+                                                                                                                                             ->createUrl('/aaee/anexoramo/anexo-ramo/lista-periodo') . '&id=' . '" + $(this).val(), function( data ) {
+                                                                                                                                                                                                            $( "select#id-periodo" ).html( data );
+                                                                                                                                                                                                        });'
+				                                                                			])->label(false)
+					    					?>
+										</div>
+	        						</div>
+
+									<div class="col-sm-2" style="width: 8%;margin-left: -25px;">
+										<div class="periodo">
+					                		<?= $form->field($model, 'periodo')->dropDownList([],[
+            																	 			'id' => 'id-periodo',
+            																	 			'name' => 'periodo',
+            																	 			'style' => 'width: 100%;',
+                                                                 				 			'prompt' => Yii::t('backend', '-'),
+                                                                						])->label(false)
+					    					?>
+										</div>
+	        						</div>
+
+									<div class="col-sm-2" style="width: 20%;margin-left: -25px;">
+										<div class="exigibilidad">
+					                		<?= Html::input('text', 'exigibilidad', '',
+												 								[
+												 									'class' => 'form-control',
+												 									'style' => 'width: 120%;',
+												 									'disabled' => 'disabled',
+												 								])
+					    					?>
+										</div>
+	        						</div>
+
+								</div> 		<!-- Fin de row -->
+							</div>
+						</div>
+					</div>
+
+
+
+				</div>  <!-- Fin de col-sm-12 -->
+			</div>  	<!-- Fin de container-fluid -->
+
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="form-group">
+						<?= Html::submitButton(Yii::t('frontend', Yii::t('frontend', 'Create Request')),
+																			  [
+																				'id' => 'btn-create',
+																				'class' => 'btn btn-success',
+																				'value' => 4,
+																				'style' => 'width: 100%; margin-left: 100px;margin-top:20px;',
+																				'name' => 'btn-create',
+																				'disabled' => ( $activarBotonCreate == 1 ) ? false : true,
+																			  ])
+						?>
+					</div>
+				</div>
+
+
+				<div class="col-sm-3">
+					<div class="form-group">
+						<?= Html::submitButton(Yii::t('frontend', Yii::t('frontend', 'Quit')),
+																			  [
+																				'id' => 'btn-quit',
+																				'class' => 'btn btn-danger',
+																				'value' => 1,
+																				'style' => 'width: 60%; margin-left: 300px;margin-top:20px;',
+																				'name' => 'btn-quit',
+
+																			  ])
+						?>
+					</div>
+				</div>
+			</div>
+
+		</div>			<!-- Fin panel-body-->
+
+	</div>	<!-- Fin de panel panel-primary -->
+
+ 	<?php ActiveForm::end(); ?>
+</div>	 <!-- Fin de inscripcion-act-econ-form -->
+
+
