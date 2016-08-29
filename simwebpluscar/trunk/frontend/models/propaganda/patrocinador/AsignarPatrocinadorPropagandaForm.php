6f5baaf19e7d3a75b808793279f2d8a937dc9073
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-08-29 20:08:53 +0000

    actualizando

---
+++ simwebpluscar/trunk/frontend/models/propaganda/patrocinador/AsignarPatrocinadorPropagandaForm.php
@@ -0,0 +1,223 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ * 
+ *  > This library is free software; you can redistribute it and/or modify it under 
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free 
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion) 
+ *  > any later version.
+ *  > 
+ *  > This library is distributed in the hope that it will be usefull, 
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability 
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence 
+ *  > for more details.
+ *  > 
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**    
+ *  @file AsignarPatrocinadorPropagandaForm.php
+ *  
+ *  @author Manuel Alejandro Zapata Canelon
+ * 
+ *  @date 19/07/2016
+ * 
+ *  @class AsignarPatrocinadorPropagandaForm
+ *  @brief Clase que contiene las rules para validacion de la asignacion de patrocinador a la propaganda
+ *  
+ * 
+ *  
+ *  
+ *  @property
+ *
+ *  
+ *  @method
+ *  rules
+ *  scenarios
+ *  search
+ *
+ *  
+ *
+ *  @inherits
+ *  
+ */ 
+namespace frontend\models\propaganda\patrocinador;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\propaganda\Propaganda;
+use common\models\solicitudescontribuyente\SolicitudesContribuyente;
+
+
+
+/**
+ * InmueblesSearch represents the model behind the search form about `backend\models\Inmuebles`.
+ */
+class AsignarPatrocinadorPropagandaForm extends Model
+{
+    public $ano_impositivo;
+
+
+
+
+    
+   
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+
+            [[ 'ano_impositivo'], 'required'],
+            
+            ];
+    } 
+
+    /**
+     * @inheritdoc
+     */
+   
+
+    public function attributeLabels()
+    {
+        return [
+               
+                'ano_impositivo' => Yii::t('frontend', 'Año Impositivo'), 
+              
+        ];      
+    }
+
+    public function attributeSlPropagandasPatrocinadores()
+    {
+
+        return [
+            'nro_solicitud',
+            'id_contribuyente',
+            'id_impuesto',
+            'id_patrocinador',
+            'origen',
+            'usuario',
+            'fecha_hora',
+            'estatus',
+            'fecha_hora_proceso',
+            'user_funcionario',
+         
+
+            
+            
+          
+        ];
+    }
+
+
+    public function attributePropagandasPatrocinadores()
+    {
+
+        return [
+            'id_contribuyente',
+            'id_impuesto',
+            'id_patrocinador',
+            'estatus',
+     
+          
+        ];
+    }
+
+       /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search()
+    { 
+       // die('llegue a search');
+
+        $idContribuyente = yii::$app->user->identity->id_contribuyente;
+
+
+        $query = Propaganda::find();
+
+                                
+                             
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+           // die(var_dump($dataProvider)),
+        ]);
+        $query->where([
+           
+            
+            ]);
+        
+        return $dataProvider;
+
+       
+    }
+
+    public function busquedaPropaganda($idPropaganda, $idContribuyente)
+    {
+        //
+        $busqueda = Propaganda::find()
+                                ->where([
+                                    'id_contribuyente' => $idContribuyente,
+                                    'id_impuesto' => $idPropaganda,
+                                    'inactivo' => 0,
+
+                                
+                               
+                                ])
+                                ->one();
+                                //die(var_dump($busqueda));
+
+            if ($busqueda == true){
+                return $busqueda;
+            }else{
+                return false;
+            }
+    }
+
+    public function verificarSolicitud($idConfig, $idPropaganda)
+    {
+        $buscar = SolicitudesContribuyente::find()
+                                        ->where([ 
+                                          'id_impuesto' => $idPropaganda,
+                                          'id_config_solicitud' => $idConfig,
+                                          'estatus' => 0,
+                                        ])
+                                      ->all();
+
+            if($buscar == true){
+                //die('encontro');
+             return true;
+            }else{
+               // die('no encontro');
+             return false;
+            }
+        
+    }
+
+    public function getClase($dato)
+    {
+       $datos = propaganda::find('clase_propaganda')
+       ->where([
+        'id_impuesto' => $dato,
+
+
+        ])
+       ->all();
+    }
+
+
+
+   
+
+  
+}
