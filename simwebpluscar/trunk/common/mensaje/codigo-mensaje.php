450b4709de679b307f327ba633277f1b07b30e91
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-09-02 15:07:57 +0000

    actualizando

--- simwebpluscar/trunk/common/mensaje/codigo-mensaje.php
+++ simwebpluscar/trunk/common/mensaje/codigo-mensaje.php
@@ -69,6 +69,7 @@ return [
 	'996' => [Yii::t('backend', 'This Stickers lot has already been asigned'), 'danger', 'fa fa-exclamation-triangle'],
 	'997' => [Yii::t('backend', 'This Car already has a sticker asigned'), 'danger', 'fa fa-exclamation-triangle'],
 	'998' => [Yii::t('backend', 'The year you selected is not available'), 'danger', 'fa fa-exclamation-triangle'],
+	'999' => [Yii::t('backend', 'At least 1 officer still have a sticker lot asigned'), 'danger', 'fa fa-exclamation-triangle'],
 
 	'404' => [Yii::t('backend', 'The requested page does not exist.'), 'danger', 'fa fa-times'],
 	'100' => [Yii::t('backend', 'Register saved'), 'success', 'fa fa-check-circle-o'],
