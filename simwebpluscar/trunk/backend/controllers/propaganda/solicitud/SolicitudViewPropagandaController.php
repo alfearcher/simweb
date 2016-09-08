4fc026603d063003fef1423cf22bc953a893c789
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-09-08 19:28:41 +0000

    actualizando

--- simwebpluscar/trunk/backend/controllers/propaganda/solicitud/SolicitudViewPropagandaController.php
+++ simwebpluscar/trunk/backend/controllers/propaganda/solicitud/SolicitudViewPropagandaController.php
@@ -110,8 +110,16 @@
 				} elseif ( $this->model->tipo_solicitud == 42 ) {
 
 					return self::actionMostarSolicitudDesincorporacionPropaganda();
+				
+				} elseif ($this->model->tipo_solicitud == 72){
+					
+					return self::actionMostarSolicitudAsignacionPatrocinadorPropaganda();
 				}
-		 	}
+
+
+
+			 }
+		 	
 
 			return false;
 		}
@@ -235,6 +243,46 @@
 
 			return false;
 		}
+
+		/**
+		 * Metodo particular que se encarga de buscar los datos de la solicitud particular sobre
+		 * "Asignacion de patrocinadores", y de renderizar una vista del detalle de la solicitud
+		 * Se utiliza un parametro adicional "nivel de aprobacion", este determinara un nivel mas
+		 * determinante de la vista.
+		 * El nivel de aprobacion 3 renderizara un formulario con los datos originales de la solicitud
+		 * inhabilitados y solo permitira la edicion de los campos que no fueron cargados en dicha
+		 * solicitud, esto con la intencion de que el funcionario pueda complementar dicha informacion.
+		 * @return View Retorna un vista con la informacion de la solicitud sino encuentra dicha
+		 * informacion retornara false.
+		 * ---
+		 * nivel de aprobacion 1: No aplica.
+		 * nivel de aprobacion 2: la vista no permite la edicion de los campos.
+		 * 	- Esquema de esta vista:
+		 *  	* Nombre del campo : Valor del campo
+		 * nivel de aprobacion 3: Muestra inhabilitado los datos suministrados previamente y habilita
+		 * aquellos campos que no fueron cargados inicialmente.  
+		 */
+		private function actionMostarSolicitudAsignacionPatrocinadorPropaganda()
+		{
+			if ( $this->model->nivel_aprobacion == 2 ) {
+					$modelSearch = New SlPropagandasForm($this->model->id_contribuyente);
+					$model = $modelSearch->findPatrocinadorPropaganda($this->model->nro_solicitud);
+
+	//die(var_dump($model));
+
+
+					return $this->render('/propaganda/solicitudes/patrocinador/view-solicitud-asignar-patrocinador-propaganda', [
+													'caption' => Yii::t('frontend', 'Request Nro. ' . $this->model->nro_solicitud),
+													'model' => $model,
+													
+
+						]);
+			}
+
+			return false;
+		}
+
+
 
 
 
