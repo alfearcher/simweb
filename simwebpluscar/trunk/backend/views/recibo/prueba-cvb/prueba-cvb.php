a6c5531ce79157d5411fdcf2697cc1773f4db3bb
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-09-12 23:23:33 +0000

    Actualizar

--- simwebpluscar/trunk/backend/views/recibo/prueba-cvb/prueba-cvb.php
+++ simwebpluscar/trunk/backend/views/recibo/prueba-cvb/prueba-cvb.php
@@ -48,12 +48,18 @@
 	use yii\web\View;
 	use yii\widgets\DetailView;
 	use yii\grid\GridView;
+	use arturoliveira\ExcelView;
 ?>
 
 <div class="row">
 	<div class="prueba-cvb">
 		<div class="row">
-			<h3><?= Html::encode('Prueba de CVB') ?></h3>
+			<div class="row">
+				<div class="col-sm-3">
+					<h3><?= Html::encode('Prueba de CVB') ?></h3>
+				</div>
+				<h4><div class="sm-5"><?=Html::a('Exportar a Excel', Url::toRoute(['export']))?></div></h4>
+			</div>
 				<?= GridView::widget([
 					'id' => 'prueba-cvb',
 					'dataProvider' => $dataProvider,
@@ -61,13 +67,27 @@
 					'columns' => [
 						['class' => 'yii\grid\SerialColumn'],
 						[
-		                    'label' => Yii::t('backend', 'Id de la Alcaldia'),
-		                    'value' => Yii::$app->ente->getEnte(),
+		                    'label' => Yii::t('backend', 'Id Alcaldia'),
+		                    'value' => function($data) {
+    										return '0'.Yii::$app->ente->getEnte();
+										},
 		                ],
 						[
-		                    'label' => Yii::t('backend', 'Id Contribuyente'),
+		                    'label' => Yii::t('backend', 'Id Contr'),
 		                    'value' => function($data) {
     										return 0;
+										},
+		                ],
+		                [
+		                    'label' => Yii::t('backend', 'd.c.id Cont'),
+		                    'value' => function($model) {
+    										return '0';
+										},
+		                ],
+		                [
+		                    'label' => Yii::t('backend', 'lon. id. Cont'),
+		                    'value' => function($model) {
+    										return '0';
 										},
 		                ],
 						[
@@ -77,19 +97,76 @@
 										},
 		                ],
 		                [
-		                    'label' => Yii::t('backend', 'Monto a pagar'),
+		                    'label' => Yii::t('backend', 'd.c.recibo'),
+		                    'value' => function($model) {
+    										return $model->getCodigoControl((int)$model->recibo);
+										},
+		                ],
+		                [
+		                    'label' => Yii::t('backend', 'lon. recibo'),
+		                    'value' => function($model) {
+    										return strlen($model->recibo);
+										},
+		                ],
+		                [
+		                    'label' => Yii::t('backend', 'monto a pagar'),
 		                    'value' => function($data) {
     										return $data->monto;
 										},
 		                ],
 		                [
-		                    'label' => Yii::t('backend', 'Fecha'),
+		                    'label' => Yii::t('backend', 'd.c.monto'),
+		                    'value' => function($model) {
+    										return $model->getCodigoControl($model->monto);
+										},
+		                ],
+		                [
+		                    'label' => Yii::t('backend', 'lon. monto'),
+		                    'value' => function($model) {
+		                    				$long = str_replace(".", "", (string)$model->monto);
+    										return strlen($long);
+										},
+		                ],
+		                [
+		                    'label' => Yii::t('backend', 'fecha'),
 		                    'value' => function($data) {
     										return $data->fecha;
 										},
 		                ],
+		                [
+		                    'label' => Yii::t('backend', 'd.c.fecha'),
+		                    'value' => function($model) {
+		                    				$fecha = date('d-m-Y', strtotime($model->fecha));
+		                    				$fecha1 = str_replace('-', '', $fecha);
+    										return $model->getCodigoControl($fecha1);
+										},
+		                ],
+		                [
+		                    'label' => Yii::t('backend', 'lon.fecha'),
+		                    'value' => function($model) {
+		                    				$fecha = date('d-m-Y', strtotime($model->fecha));
+		                    				$fecha1 = str_replace('-', '', $fecha);
+    										return strlen($fecha1);
+										},
+		                ],
+		                [
+		                	'label' => 'CVB',
+		                	'value' => function($model) {
+		                		$idAlcaldia = '0'.Yii::$app->ente->getEnte();
+		                		$cbvID = '00';
+		                		$cvbRecibo = $model->getCodigoControl((int)$model->recibo);
+		                		$cvbRecibo = $cvbRecibo . strlen($model->recibo);
+		                		$cvbMonto = $model->getCodigoControl($model->monto);
+		                		$long = str_replace(".", "", (string)$model->monto);
+		                		$cvbMonto = $cvbMonto . strlen($long);
+		                		$f = date('d-m-Y', strtotime($model->fecha));
+		                    	$f1 = str_replace('-', '', $f);
+		                		$cvbFecha = $model->getCodigoControl($f1) . strlen($f1);
 
-		        	]
+		                		return $idAlcaldia . '-' . $cbvID . $cvbRecibo . '-' . $cvbMonto . $cvbFecha;
+		                	},
+		                ],
+		        	],
 				]);
 			?>
 		</div>
