728e49df721575dc991f18369d8ff4ba2d2b9035
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-09-14 20:51:23 +0000

    Actualizar

--- simwebpluscar/trunk/common/models/calculo/cvb/ModuloValidador.php
+++ simwebpluscar/trunk/common/models/calculo/cvb/ModuloValidador.php
@@ -63,47 +63,44 @@
 		 * acumulado. A este valor acumulado se le calculara el modulo 11.
 		 * Las cifras con montos decimales deben contener 2 digitos para la representacion
 		 * decimal.
-		 * @param string $valorConvertir cadena de digitos al cual se le
+		 * @param string $valorBase cadena de digitos al cual se le
 		 * calculara el digito modulo 11. El valor solo debe contener numeros.
 		 * @return integer retorna un entero que representa el digito validador, sino
 		 * retornara null.
 		 */
-		public function getCodigoModuloOnce($valorConvertir)
+		private function getCodigoModuloOnce($valorBase)
 		{
 			$modulo11 = null;
-			// if ( !ctype_digit($valorConvertir) ) {
-			// 	$valorConvertir = self::filtrarCaracteresFloat($valorConvertir);
-			// }
-
-			//if ( ctype_digit($valorConvertir) ) {
-				$digito = [];
-				$cadenaDigitos = '';
-
-				// Se obtiene la parte entera y la parte decimal.
-				$digito = explode(".", $valorConvertir);
-				$parteEntera = $digito[0];
-				$parteDecimal = isset($digito[1]) ? $digito[1] : null;
+			if ( ctype_digit($valorBase) ) {
+				//$digito = [];
+				//$cadenaDigitos = '';
+				$arregloDigitos = [];
+//die(var_dump($valorBase));
+				// // Se obtiene la parte entera y la parte decimal.
+				// $digito = explode(".", $valorConvertir);
+				// $parteEntera = $digito[0];
+				// $parteDecimal = isset($digito[1]) ? $digito[1] : null;
 
 				// Se determina si el valor a convertir tiene parte decimal. Sino solo se tomara la
 				// parte entera.
-				if ( $parteDecimal !== null ) {
-					// Para garantizar que se toman dos decimales.
-					// Se coloca cero al final de la parte decimal.
-					if (strlen($parteDecimal) == 1 ) {
-						$parteDecimal .= "0";
-					}
+				// if ( $parteDecimal !== null ) {
+				// 	// Para garantizar que se toman dos decimales.
+				// 	// Se coloca cero al final de la parte decimal.
+				// 	if (strlen($parteDecimal) == 1 ) {
+				// 		$parteDecimal .= "0";
+				// 	}
 
-					$cadenaDigitos = $parteEntera . $parteDecimal;
-				} else {
-					$cadenaDigitos = $parteEntera;
-				}
+				// 	$cadenaDigitos = $parteEntera . $parteDecimal;
+				// } else {
+				// 	$cadenaDigitos = $parteEntera;
+				// }
 
 				// Crea un array con los digitos resultantes.
-				$arregloDigitos = str_split($cadenaDigitos);
+				$arregloDigitos = str_split($valorBase);
 
 				// Se invierte el arreglo.
 				$arregloDigitos = array_reverse($arregloDigitos);
-
+//die(var_dump($valorBase));
 				$suma = 0;
 				$operacion = 0;
 				$ponderado = 0;
@@ -114,20 +111,68 @@
 					$suma = $operacion + $suma;
 
 					// Permite ver los parametros resultantes
-					//$v[] = [$value, $ponderado, $operacion];
+					$v[] = [$value, $ponderado, $operacion];
 				}
-
+if ( $valorBase == '252' ) {
+die(var_dump($v));
+}
 				// Se obtiene el resto de la division $suma entre 11. Modulo 11.
 				$resto = fmod($suma, 11);
 				$modulo11 = (int)(11 - $resto);
-
 				if ( $modulo11 == 10 ) {
 					$modulo11 = 1;
 				} elseif ( $modulo11 == 11 ) {
 					$modulo11 = 0;
 				}
-			//}
+			}
+
 			return $modulo11;
+		}
+
+
+
+		/***/
+		public function getDigitoControl($valor)
+		{
+			$digitoControl = null;
+			if ( is_real($valor) ) {
+				// Se convierte el valor en un entero, para facilitar la aplicacion
+				// del algortimo para encontrar el digito control.
+
+				// $digito = explode(".", $valor);
+				// $parteEntera = $digito[0];
+				// $parteDecimal = isset($digito[1]) ? $digito[1] : null;
+				// if ( $parteDecimal !== null ) {
+				//  	// Para garantizar que se toman dos decimales.
+				//  	// Se coloca cero al final de la parte decimal.
+				//  	if (strlen($parteDecimal) == 1 ) {
+				//  		$parteDecimal .= "0";
+				//  	} else {
+				//  		$parteDecimal .= "00";
+				//  	}
+				// 	$cadenaDigitos = $parteEntera . $parteDecimal;
+				// } else {
+				//  	$cadenaDigitos = $parteEntera;
+				// }
+				// $entero = $cadenaDigitos;
+
+				$entero = $valor * 100;
+				$entero = (string)$entero;
+//die(var_dump($entero));
+				$digitoControl = self::getCodigoModuloOnce($entero);
+
+			} elseif ( is_integer($valor) ) {
+				$entero = $valor;
+//die(var_dump($entero));
+				$digitoControl = self::getCodigoModuloOnce($entero);
+			} else {
+				// El valor enviado para los formato fechas deben ser ddmmyyyy
+				$entero = $valor;
+//die(var_dump($entero));
+				$digitoControl = self::getCodigoModuloOnce($entero);
+			}
+
+			return $digitoControl;
 		}
 
 
@@ -171,7 +216,7 @@
 
 
 		/***/
-		public function getPesoAgregar($posicion, $indiceModulo = 11)
+		private function getPesoAgregar($posicion, $indiceModulo = 11)
 		{
 			$indicePeso = Yii::$app->ente->getEnte();	// Identificacion de la Alcaldia.
 
@@ -192,11 +237,15 @@
 
 			$itemCount = count($peso);
 			if ( $posicion >= $itemCount ) {
-				$p = $posicion - $itemCount;
-				$posicion = $p;
+				$r = 100;
+				while ( $r > $itemCount ) {
+					$p = $posicion - $itemCount;
+					$posicion = $p;
+					$r = $p;
+				}
 			}
 
-			return $peso[$posicion] * $indicePeso;
+			return (int)$peso[$posicion];
 		}
 
 
