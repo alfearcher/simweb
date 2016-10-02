96a573ee62639eafaf84adc3aaa78426605cd1c5
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-10-02 02:09:08 +0000

    Actualizar

--- simwebpluscar/trunk/frontend/views/aaee/correccion-fecha-inicio/correccion-fecha-inicio-form.php
+++ simwebpluscar/trunk/frontend/views/aaee/correccion-fecha-inicio/correccion-fecha-inicio-form.php
@@ -54,7 +54,7 @@
  			'id' => 'correccion-fecha-inicio-fiscal-form',
  			'method' => 'post',
  			'enableClientValidation' => true,
- 			'enableAjaxValidation' => false,
+ 			'enableAjaxValidation' => true,
  			'enableClientScript' => true,
  		]);
  	?>
@@ -177,9 +177,6 @@
 																								'value' => 1,
 																								'style' => 'width: 100%',
 																								'name' => 'btn-create',
-																								'onClick' => [
-																									'class' => 'btn btn-success disabled'
-																								],
 																							  ])
 								?>
 							</div>
@@ -211,11 +208,3 @@
 	<?php ActiveForm::end(); ?>
 </div>
 
-<?php $this->registerJs(
-	'$(document).ready(function() {
-		$( "#btn-create").click(function() {
-			$("#btn-create").attr("disabled", true);
-		});
-	})
-');
-?>
