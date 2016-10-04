433886918299fb7b84dccf7756817048cec628ab
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-10-04 21:09:05 +0000

    Actualizar

--- simwebpluscar/trunk/backend/models/aaee/desincorporaramo/DesincorporarRamoSearch.php
+++ simwebpluscar/trunk/backend/models/aaee/desincorporaramo/DesincorporarRamoSearch.php
@@ -453,13 +453,15 @@
 	    								->where('id_contribuyente =:id_contribuyente',
 	    	 										['id_contribuyente' => $this->_id_contribuyente])
 	    			  				    ->andWhere('estatus =:estatus', [':estatus' => 0])
+	    			  				    ->andWhere('inactivo =:inactivo', [':inactivo' => 0])
+	    			  				    ->andWhere('bloqueado =:bloqueado', [':bloqueado' => 0])
 	    							    ->andWhere('ano_impositivo >=:ano_impositivo',
 	    							    			[':ano_impositivo' => $añoLimite])
 	    							    ->orderBy([
 	    							   		'ano_impositivo' => SORT_ASC,
 	    							   	])
 	    							    ->all();
-	    	if ( isset($findModel) ) {
+	    	if ( count($findModel) > 0 ) {
 	    		$listaAño = ArrayHelper::map($findModel, 'ano_impositivo', 'ano_impositivo');
 	    	}
 
