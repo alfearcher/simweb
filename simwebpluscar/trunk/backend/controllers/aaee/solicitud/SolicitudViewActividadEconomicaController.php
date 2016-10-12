a47766ab8658b33fffb38fc9a1a754c0267a4ffc
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-10-12 23:35:57 +0000

    Actualizar

--- simwebpluscar/trunk/backend/controllers/aaee/solicitud/SolicitudViewActividadEconomicaController.php
+++ simwebpluscar/trunk/backend/controllers/aaee/solicitud/SolicitudViewActividadEconomicaController.php
@@ -112,6 +112,10 @@
 
 					return self::actionMostarSolicitudInscripcionSucursal();
 
+				} elseif ( $this->_model->tipo_solicitud == 8 ) {
+
+					return self::actionMostarSolicitudDeclaracionDefinitiva();
+
 				} elseif ( $this->_model->tipo_solicitud == 9 ) {
 
 					return self::actionMostarSolicitudAnexarRamo();
@@ -628,6 +632,47 @@
 
 			return false;
 		}
+
+
+
+
+		/**
+		 * Metodo particular que se encarga de buscar los datos de la solicitud particular sobre
+		 * "Declaracion Definitiva", y de renderizar una vista del detalle de la solicitud
+		 * Se utiliza un parametro adicional "nivel de aprovacion", este determinara un nivel mas
+		 * determinante de la vista.
+		 * El nivel de aprobacion 3 renderizara un formulario con los datos originales de la solicitud
+		 * inhabilitados y solo permitira la edicion de los campos que no fueron cargados en dicha
+		 * solicitud, esto con la intencion de que el funcionario pueda complementar dicha informacion.
+		 * @return view retorna un vista con la informacion de la solicitud sino encuentra dicha
+		 * informacion retornara false.
+		 * ---
+		 * nivel de aprobacion 1: No aplica.
+		 * nivel de aprobacion 2: la vista no permite la edicion de los campos.
+		 * 	- Esquema de esta vista:
+		 *  	* Nombre del campo : Valor del campo
+		 * nivel de aprobacion 3: Muestra inhabilitado los datos suministrados previamente y habilita
+		 * aquellos campos que no fueron cargados inicialmente.
+		 */
+		private function actionMostarSolicitudDeclaracionDefinitiva()
+		{
+			if ( $this->_model->nivel_aprobacion == 2 ) {
+				$modelSearch = New DeclaracionBaseSearch($this->_model->id_contribuyente);
+				$model = $modelSearch->findSolicitudDeclaracion($this->_model->nro_solicitud);
+				$dataProvider = $modelSearch->getDataProviderSolicitud($this->_model->nro_solicitud);
+				if ( isset($model) ) {
+					return $this->render('@backend/views/aaee/declaracion/definitiva/view-solicitud', [
+													'caption' => Yii::t('frontend', 'Request Nro. ' . $this->_model->nro_solicitud),
+													'model' => $model,
+													'dataProvider' => $dataProvider,
+
+						]);
+				}
+			}
+
+			return false;
+		}
+
 
 
 	}
