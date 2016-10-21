449f090dd4549f57dd20eef351c0efe7aabfdd2d
manuelz <manuelz@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-10-21 15:47:08 +0000

    actualizando

---
+++ simwebpluscar/trunk/common/models/rubros/Rubros.php
@@ -0,0 +1,85 @@
+<?php
+
+
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file Rubros.php
+ *
+ *  @author Manuel Alejandro Zapata Canelon
+ *
+ *  @date 15/10/2016
+ *
+ *  @class Rubros
+ *  @brief  Modelo que instancia la conexion a la base de datos para buscar datos de la tabla rubros
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *  rules
+ *  attributeLabels
+ *  scenarios
+ *
+ *
+ *  @inherits
+ *
+ */
+
+namespace common\models\rubros\calcomaniaentregada;
+
+use Yii;
+use backend\models\utilidad\tarifa\vehiculo\TarifaVehiculo;
+
+/**
+ * This is the model class for table "causas_desincorporaciones_lote".
+ *
+ *
+ */
+class Rubros extends \yii\db\ActiveRecord
+{
+
+    /**
+    * @inheritdoc
+    */
+    public static function getDb()
+    {
+      return Yii::$app->db;
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'rubros';
+    }
+
+
+
+   
+
+
+
+}
