f4e80d12120eecb39347b96cee9762109415d3ec
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-10-23 22:16:22 +0000

    Modulo de consulta de declaraciones, que genera un pdf

---
+++ simwebpluscar/trunk/frontend/controllers/aaee/declaracion/consulta/ConsultaDeclaracionController.php
@@ -0,0 +1,223 @@
+<?php
+/**
+ *	@copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *	> This library is free software; you can redistribute it and/or modify it under
+ *	> the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *	> Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *	> any later version.
+ *  >
+ *	> This library is distributed in the hope that it will be usefull,
+ *	> but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *	> or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *	> for more details.
+ *  >
+ *	> See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *	@file ConsultaController.php
+ *
+ *	@author Jose Rafael Perez Teran
+ *
+ *	@date 22-10-2016
+ *
+ *  @class ConsultaController
+ *	@brief Clase ConsultaController del lado del contribuyente frontend.
+ *
+ *
+ *	@property
+ *
+ *
+ *	@method
+ *
+ *
+ *	@inherits
+ *
+ */
+
+
+ 	namespace frontend\controllers\aaee\declaracion\consulta;
+
+
+ 	use Yii;
+	use yii\filters\AccessControl;
+	use yii\web\Controller;
+	use yii\filters\VerbFilter;
+	use yii\web\Response;
+	use yii\helpers\Url;
+	use yii\web\NotFoundHttpException;
+	use common\mensaje\MensajeController;
+	use common\models\session\Session;
+	use yii\base\Model;
+
+	session_start();		// Iniciando session
+
+	/**
+	 * Clase principal que controla la creacion de solicitudes de Declaraciones
+	 * Solicitud que se realizara del lado del contribuyente (frontend). Se mostrara una vista
+	 * previa de la solicitud realizada por el contribuyente y se le indicara al contribuyente
+	 * que confirme la operacion o retorne a la vista inicial donde cargo la informacion para su
+	 * ajuste. Cuando el contribuyente confirme su intencion de crear la solicitud, es cuando
+	 * se guardara en base de datos.
+	 */
+	class ConsultaController extends Controller
+	{
+		public $layout = 'layout-main';				//	Layout principal del formulario
+
+
+		/**
+		 * Metodo que mostrara el formulario de cargar inicial de la solicitud, para
+		 * que el contribuyente ingrese la informacion soliictada.
+		 * @return [type] [description]
+		 */
+		public function actionIndex()
+		{
+			// Se verifica que el contribuyente haya iniciado una session.
+
+			self::actionAnularSession(['begin', 'anoOrdenanza', 'configOrdenanza']);
+			$request = Yii::$app->request;
+			$getData = $request->get();
+
+			$postData = $request->post();
+			if ( isset($postData['btn-quit']) ) {
+				if ( $postData['btn-quit'] == 1 ) {
+					$this->redirect(['quit']);
+				}
+			}
+
+			// identificador de la configuracion de la solicitud.
+			$id = $getData['id'];
+			if ( $id == self::CONFIG ) {
+				if ( isset($_SESSION['idContribuyente']) ) {
+					return $this->redirect(['check', 'id' => $id]);
+
+				} else {
+					// No esta definido el contribuyente.
+					return $this->redirect(['error-operacion', 'cod' => 404]);
+				}
+			}
+		}
+
+
+
+
+		/**
+		 * Metodo que permite obtener los periodos que existe en un año especifico,
+		 * los periodos representan el nivel de exigibilidad (cantidad de periodo)
+		 * que se debe cumplir en un año. Cada periodo representa un lapso de tiempo
+		 * especifico. Esquema:
+		 * 1 - Anual.
+		 * 2 - Semestral.
+		 * 3 - Cuatrimestre.
+		 * 4 - Trimestral.
+		 * 6 - Bimensual.
+		 * 12 - Mensual.
+		 * 99 - No definido.
+		 * @return view
+		 */
+		public function actionListaPeriodo()
+		{
+			$añoImpositivo = 0;
+			$request = Yii::$app->request;
+
+			// Se capta el año impositivo de act-econ.
+			$añoImpositivo = $request->get('id');
+
+			$idContribuyente = isset($_SESSION['idContribuyente']) ? $_SESSION['idContribuyente'] : 0;
+			if ( $idContribuyente > 0 ) {
+				$searchRamo = New DeclaracionBaseSearch($idContribuyente);
+
+				// Se espera recibir un arreglo con los atributos de la entidad respectiva.
+				$exigibilidad = $searchRamo->getExigibilidadSegunAnoImpositivo($añoImpositivo);
+				if ( count($exigibilidad) > 0 ) {
+					return $searchRamo->getViewListaExigibilidad($exigibilidad);
+				}
+			}
+			return "<option> - </option>";
+		}
+
+
+
+    	/**
+		 * Metodo salida del modulo.
+		 * @return view
+		 */
+		public function actionQuit()
+		{
+			$varSession = self::actionGetListaSessions();
+			self::actionAnularSession($varSession);
+			return $this->render('/menu/menu-vertical');
+		}
+
+
+
+		/**
+		 * Metodo que ejecuta la anulacion de las variables de session utilizados
+		 * en el modulo.
+		 * @param  array $varSessions arreglo con los nombres de las variables de
+		 * sesion que seran anuladas.
+		 * @return none.
+		 */
+		public function actionAnularSession($varSessions)
+		{
+			Session::actionDeleteSession($varSessions);
+		}
+
+
+
+		/**
+		 * Metodo que renderiza una vista indicando que le proceso se ejecuto
+		 * satisfactoriamente.
+		 * @param  integer $cod codigo que permite obtener la descripcion del
+		 * codigo de la operacion.
+		 * @return view.
+		 */
+		public function actionProcesoExitoso($cod)
+		{
+			$varSession = self::actionGetListaSessions();
+			self::actionAnularSession($varSession);
+			return MensajeController::actionMensaje($cod);
+		}
+
+
+
+		/**
+		 * Metodo que renderiza una vista que indica que ocurrio un error en la
+		 * ejecucion del proceso.
+		 * @param  integer $cod codigo que permite obtener la descripcion del
+		 * codigo de la operacion.
+		 * @return view.
+		 */
+		public function actionErrorOperacion($cod)
+		{
+			$varSession = self::actionGetListaSessions();
+			self::actionAnularSession($varSession);
+			return MensajeController::actionMensaje($cod);
+		}
+
+
+
+		/**
+		 * Metodo que permite obtener un arreglo de las variables de sesion
+		 * que seran utilizadas en el modulo, aqui se pueden agregar o quitar
+		 * los nombres de las variables de sesion.
+		 * @return array retorna un arreglo de nombres.
+		 */
+		public function actionGetListaSessions()
+		{
+			return $varSession = [
+							'postData',
+							'conf',
+							'begin',
+							'lapso'
+					];
+		}
+
+	}
+?>
