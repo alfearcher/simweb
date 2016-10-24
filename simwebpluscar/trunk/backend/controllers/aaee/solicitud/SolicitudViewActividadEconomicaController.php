ef540a80fe3f060d061d5e124f27d5ada21ea20b
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-10-24 03:08:35 +0000

    Actualizar

--- simwebpluscar/trunk/backend/controllers/aaee/solicitud/SolicitudViewActividadEconomicaController.php
+++ simwebpluscar/trunk/backend/controllers/aaee/solicitud/SolicitudViewActividadEconomicaController.php
@@ -65,6 +65,7 @@
 	use backend\models\aaee\anexoramo\AnexoRamoSearch;
 	use backend\models\aaee\desincorporaramo\DesincorporarRamoSearch;
 	use backend\models\aaee\declaracion\DeclaracionBaseSearch;
+	use backend\models\aaee\declaracion\sustitutiva\SustitutivaBaseSearch;
 	use backend\controllers\MenuController;
 
 	//session_start();		// Iniciando session
@@ -139,6 +140,10 @@
 				} elseif ( $this->_model->tipo_solicitud == 15 ) {
 
 					return self::actionMostarSolicitudCorreccionCapital();
+
+				} elseif ( $this->_model->tipo_solicitud == 17 ) {
+
+					return self::actionMostarSolicitudDeclaracionSustitutiva();
 
 				} elseif ( $this->_model->tipo_solicitud == 69 ) {
 
@@ -662,6 +667,47 @@
 				$dataProvider = $modelSearch->getDataProviderSolicitud($this->_model->nro_solicitud);
 				if ( isset($model) ) {
 					return $this->render('@backend/views/aaee/declaracion/definitiva/view-solicitud', [
+													'caption' => Yii::t('frontend', 'Request Nro. ' . $this->_model->nro_solicitud),
+													'model' => $model,
+													'dataProvider' => $dataProvider,
+
+						]);
+				}
+			}
+
+			return false;
+		}
+
+
+
+
+
+		/**
+		 * Metodo particular que se encarga de buscar los datos de la solicitud particular sobre
+		 * "Declaracion Sustitutiva", y de renderizar una vista del detalle de la solicitud
+		 * Se utiliza un parametro adicional "nivel de aprovacion", este determinara un nivel mas
+		 * determinante de la vista.
+		 * El nivel de aprobacion 3 renderizara un formulario con los datos originales de la solicitud
+		 * inhabilitados y solo permitira la edicion de los campos que no fueron cargados en dicha
+		 * solicitud, esto con la intencion de que el funcionario pueda complementar dicha informacion.
+		 * @return view retorna un vista con la informacion de la solicitud sino encuentra dicha
+		 * informacion retornara false.
+		 * ---
+		 * nivel de aprobacion 1: No aplica.
+		 * nivel de aprobacion 2: la vista no permite la edicion de los campos.
+		 * 	- Esquema de esta vista:
+		 *  	* Nombre del campo : Valor del campo
+		 * nivel de aprobacion 3: Muestra inhabilitado los datos suministrados previamente y habilita
+		 * aquellos campos que no fueron cargados inicialmente.
+		 */
+		private function actionMostarSolicitudDeclaracionSustitutiva()
+		{
+			if ( $this->_model->nivel_aprobacion == 2 ) {
+				$modelSearch = New SustitutivaBaseSearch($this->_model->id_contribuyente);
+				$model = $modelSearch->findSolicitudSustitutiva($this->_model->nro_solicitud);
+				$dataProvider = $modelSearch->getDataProviderSolicitud($this->_model->nro_solicitud);
+				if ( isset($model) ) {
+					return $this->render('@backend/views/aaee/declaracion/sustitutiva/view-solicitud', [
 													'caption' => Yii::t('frontend', 'Request Nro. ' . $this->_model->nro_solicitud),
 													'model' => $model,
 													'dataProvider' => $dataProvider,
