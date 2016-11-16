ce943e3a2141ab7e75bd8809086f7adf6db98623
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-11-16 02:15:34 +0000

    Actualizar

--- simwebpluscar/trunk/common/models/deuda/DeudaSearch.php
+++ simwebpluscar/trunk/common/models/deuda/DeudaSearch.php
@@ -1142,7 +1142,7 @@
 									'D.descripcion',
 									'D.impuesto',
 									'I.descripcion as descripcion_impuesto',
-									])
+								])
 							   ->joinWith('pagos P', false, 'INNER JOIN')
 							   ->joinWith('impuestos I', true, 'INNER JOIN')
 							   ->andWhere(['IN', 'planilla', $planillas])
@@ -1158,6 +1158,89 @@
 
 			return $deuda;
 
+		}
+
+
+
+
+		/***/
+		public function getAgruparDeudaPorImpuestoAnoImpositivo($año, $operador)
+		{
+			$findModel = self::getModelGeneral();
+			$deuda = [];
+
+			if ( in_array($operador, ['>', '=', '<', '>=', '<=']) ) {
+				$deuda = $findModel->select([
+									'P.id_contribuyente',
+									'P.id_pago',
+									'P.ente',
+									'sum(D.monto) as tmonto',
+									'sum(D.recargo) as trecargo',
+									'sum(D.interes) as tinteres',
+									'sum(D.descuento) as tdescuento',
+									'sum(D.monto_reconocimiento) as tmonto_reconocimiento',
+									'D.impuesto',
+									'I.descripcion as descripcion_impuesto',
+								])
+							   ->joinWith('pagos P', false, 'INNER JOIN')
+							   ->joinWith('impuestos I', true, 'INNER JOIN')
+							   ->andWhere('D.ano_impositivo ' . $operador .  ':ano_impositivo',
+							   					[':ano_impositivo' => $año])
+							   ->groupBy('D.impuesto')
+							   ->orderBy([
+							   		'D.impuesto' => SORT_ASC,
+
+							   	])
+							   ->asArray()
+							   ->all();
+			}
+			return $deuda;
+		}
+
+
+
+
+		/**
+		 * Metodo que seprar la deuda por impuesto segun la condicional de los
+		 * añoss y las planillas. Agrupa las deudas por impuestos y que cumplan
+		 * la condicion año-operador enviado mas la lista de planillas.
+		 * @param  integer $año año impositivo.
+		 * @param  string $operador operador.
+		 * @param  array $planillas arreglo de numero de planillas;
+		 * @return array.
+		 */
+		public function getAgruparDeudaPorImpuestoAnoImpositivoPlanilla($año, $operador, $planillas)
+		{
+			$findModel = self::getModelGeneral();
+			$deuda = [];
+
+			if ( in_array($operador, ['>', '=', '<', '>=', '<=']) ) {
+				$deuda = $findModel->select([
+									'P.id_contribuyente',
+									'P.id_pago',
+									'P.ente',
+									'sum(D.monto) as tmonto',
+									'sum(D.recargo) as trecargo',
+									'sum(D.interes) as tinteres',
+									'sum(D.descuento) as tdescuento',
+									'sum(D.monto_reconocimiento) as tmonto_reconocimiento',
+									'D.impuesto',
+									'I.descripcion as descripcion_impuesto',
+								])
+							   ->joinWith('pagos P', false, 'INNER JOIN')
+							   ->joinWith('impuestos I', true, 'INNER JOIN')
+							   ->andWhere('D.ano_impositivo ' . $operador .  ':ano_impositivo',
+							   					[':ano_impositivo' => $año])
+							   ->andWhere(['IN', 'planilla', $planillas])
+							   ->groupBy('D.impuesto')
+							   ->orderBy([
+							   		'D.impuesto' => SORT_ASC,
+
+							   	])
+							   ->asArray()
+							   ->all();
+			}
+			return $deuda;
 		}
 
 
