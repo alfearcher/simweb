745a915d85c0c1066344438498d81ff68cd5b4e4
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-11-28 22:01:54 +0000

    Actualizar

--- simwebpluscar/trunk/frontend/controllers/aaee/solvencia/SolvenciaActividadEconomicaSolicitudController.php
+++ simwebpluscar/trunk/frontend/controllers/aaee/solvencia/SolvenciaActividadEconomicaSolicitudController.php
@@ -795,6 +795,7 @@
  				$model = $findModel->all();
  				self::actionAnularSession(['id_historico']);
 
+die(var_dump($model));
  				$search = New HistoricoSolvenciaSearch($model[0]->id_contribuyente, 1);
  				$historico = $search->findHistoricoSolvenciaSegunSolicitud($model[0]->nro_solicitud);
 
