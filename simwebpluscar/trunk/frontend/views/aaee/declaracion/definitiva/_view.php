c5a876f61a05379e5b03d4b8c6b839255f88ddb8
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-12-19 23:32:28 +0000

    Actualizar

--- simwebpluscar/trunk/frontend/views/aaee/declaracion/definitiva/_view.php
+++ simwebpluscar/trunk/frontend/views/aaee/declaracion/definitiva/_view.php
@@ -54,7 +54,7 @@
 	 </div>
 	 <div class="row">
 		<div class="solicitud-creada">
-			<?= $this->render('/aaee/declaracion/definitiva/view-solicitud-create', [
+			<?= $this->render('@frontend/views/aaee/declaracion/definitiva/view-solicitud-create', [
 	    															'model' => $model,
 	    															'modelSearch' => $modelSearch,
 	    															'opciones' => $opciones,
