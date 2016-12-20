e8bb938f43934e16b8d02ca3e9888d1a661d8d5a
arivas <arivas@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-12-20 09:49:45 +0000

    Eliminar directorio

--- simwebplusteq/trunk/backend/views/deudas/deudascontribuyente/prueba.php
+++
@@ -1,1 +0,0 @@
-<p>probando pdf</p>
--- simwebplusteq/trunk/common/config/params.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'adminEmail' => 'manuel1122@hotmail.com',
-    'supportEmail' => 'support@example.com',
-    'user.passwordResetTokenExpire' => 3600,
-];
