65b3e88154611fb78b19fa8eff96a72f3712ea93
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-12-20 04:49:42 +0000

    Actualizar

--- simwebpluscar/trunk/backend/models/inmueble/solvencia/SolvenciaInmuebleForm.php
+++ simwebpluscar/trunk/backend/models/inmueble/solvencia/SolvenciaInmuebleForm.php
@@ -135,13 +135,13 @@
 	    		Yii::$app->solicitud->aprobar() => [
 	    						'estatus' => 1,
 	    						'fecha_hora_proceso' => date('Y-m-d H:i:s'),
-	    						'user_funcionario' => isset(Yii::$app->user->identity->username) ? Yii::$app->user->identity->username : Yii::$app->user->identity->login,
+	    						'user_funcionario' => Yii::$app->identidad->getUsuario(),
 
 	    		],
 	    		Yii::$app->solicitud->negar() => [
 	    						'estatus' => 9,
 	    						'fecha_hora_proceso' => date('Y-m-d H:i:s'),
-	    						'user_funcionario' => isset(Yii::$app->user->identity->username) ? Yii::$app->user->identity->username : Yii::$app->user->identity->login,
+	    						'user_funcionario' => Yii::$app->identidad->getUsuario(),
 
 	    		],
 	    	];
