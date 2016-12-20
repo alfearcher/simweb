22a93bbdf1b6df1447669087f0d4defa108215ef
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-12-20 01:54:21 +0000

    Actualizar

--- simwebpluscar/trunk/backend/models/aaee/desincorporaramo/DesincorporarRamoForm.php
+++ simwebpluscar/trunk/backend/models/aaee/desincorporaramo/DesincorporarRamoForm.php
@@ -168,7 +168,8 @@
 	     		['fecha_hora', 'default', 'value' => date('Y-m-d H:i:s')],
 	     		['fecha_hora_proceso', 'default', 'value' => '0000-00-00 00:00:00'],
 	     		['estatus', 'default', 'value' => 0],
-	     		['usuario', 'default', 'value' => Yii::$app->user->identity->login, 'on' => 'frontend'],
+	     		['usuario', 'default', 'value' => Yii::$app->identidad->getUsuario()],
+	     		//['usuario', 'default', 'value' => Yii::$app->user->identity->login, 'on' => 'frontend'],
 	     		//['usuario', 'default', 'value' => Yii::$app->user->identity->username, 'on' => 'backend'],
 	        ];
 	    }
@@ -217,13 +218,13 @@
 	    		Yii::$app->solicitud->aprobar() => [
 	    						'estatus' => 1,
 	    						'fecha_hora_proceso' => date('Y-m-d H:i:s'),
-	    						'user_funcionario' => isset(Yii::$app->user->identity->username) ? Yii::$app->user->identity->username : Yii::$app->user->identity->login,
+	    						'user_funcionario' => Yii::$app->identidad->getUsuario(),
 
 	    		],
 	    		Yii::$app->solicitud->negar() => [
 	    						'estatus' => 9,
 	    						'fecha_hora_proceso' => date('Y-m-d H:i:s'),
-	    						'user_funcionario' => isset(Yii::$app->user->identity->username) ? Yii::$app->user->identity->username : Yii::$app->user->identity->login,
+	    						'user_funcionario' => Yii::$app->identidad->getUsuario(),
 
 	    		],
 	    	];
