896dd9b3f09893796b852afd6d4db5a0f54278d1
alvarof <alvarof@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-12-20 16:17:52 +0000

    mo

--- simwebplusteq/trunk/common/config/db/dbsim.php
+++ simwebplusteq/trunk/common/config/db/dbsim.php
@@ -2,8 +2,8 @@
 
 	return [
 			//'class'    => 'yii\db\Connection',
-			//'dsn' => 'mysql:host=localhost;dbname=sim_teq',
-			'dsn' => 'mysql:host=192.168.30.7;dbname=sim_teq',
+			'dsn' => 'mysql:host=localhost;dbname=sim_teq',
+			//'dsn' => 'mysql:host=192.168.30.7;dbname=sim_car_plus',
 			//'dsn' => 'mysql:host=pinky.ddns.net;dbname=sim_car',
 			//'username' => 'adminyii',
 			//'password' => 'adminyii2015',
