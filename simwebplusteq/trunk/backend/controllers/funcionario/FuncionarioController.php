82273e1f396ce3df1ed3863f7b8c6eae132c3bd1
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2016-12-28 04:03:08 +0000

    Actualizar

--- simwebplusteq/trunk/backend/controllers/funcionario/FuncionarioController.php
+++ simwebplusteq/trunk/backend/controllers/funcionario/FuncionarioController.php
@@ -42,6 +42,7 @@
 	use yii\helpers\ArrayHelper;
 	use backend\models\utilidad\nivelfuncionario\NivelFuncionarioForm;
 	use common\models\session\Session;
+	use common\mensaje\MensajeController;
 
 
 	session_start();
@@ -106,8 +107,6 @@
 				return ActiveForm::validate($model);
 	      	}
 
-// die(var_dump($postData));
-
 
 	      	if ( $model->load($postData, $formName) ) {
 	      		if ( $model->validate() ) {
@@ -154,7 +153,7 @@
       						self::actionAnularSession(['begin']);
       						if ( $result ) {
 								$this->_transaccion->commit();
-die(var_dump($model));
+								$this->redirect(['proceso-exitoso', 'cod'=> 100]);
 								//return self::actionView($model->id_funcionario);
 							} else {
 								$this->_transaccion->rollBack();
@@ -341,84 +340,7 @@ die(var_dump($model));
 
 
 
-		/**
-		 * 	Metodo que permite crear un registro de funcionario.
-		 *  @return Vista del formulario de cargar.
-		 */
-		public function actionCreate()
-	  	{
 
-	  		$msg='';
-	      	$model = new FuncionarioForm();
-
-	      	if ( $model->load(Yii::$app->request->post()) && Yii::$app->request->isAjax ) {
-
-				Yii::$app->response->format = Response::FORMAT_JSON;
-				return ActiveForm::validate($model);
-	      	}
-
-	      	if ( $model->load(Yii::$app->request->post()) ) {
-
-	    		// Se validan los valores de los campos enviados desde el formualario.
-	    		// Cada uno debe cumplir con las reglas (rules), establecidas en el modelo (model) FuncionarioForm.
-	      		if ( $model->validate() ) {
-
-	      			// Se obtiene un arreglo de los campos con los datos para enviarlos a guardar.
-	      			$arrayDatos = $model->attributes;
-
-	      			// nombre de la tabla
-	      			$tabla = '';
-	      			$tabla = $model->tableName();	// Funcionarios.
-
-	      			$conexion = New ConexionController();
-
-	      			// Instancia de conexion hacia la base de datos.
-	      			$this->connLocal = $conexion->initConectar('db');
-	      			$this->connLocal->open();
-
-	      			// Instancia de tipo transaccion para asegurar la integridad del resguardo de los datos.
-	      			// Inicio de la transaccion.
-					$transaccion = $this->connLocal->beginTransaction();
-
-					if ( $conexion->guardarRegistro($this->connLocal, $tabla, $arrayDatos) ) {
-						$transaccion->commit();
-						$tipoError = 0;	// No error.
-						$msg = Yii::t('backend', 'SUCCESS!....WAIT.');
- 						$url =  "<meta http-equiv='refresh' content='3; ".Url::toRoute("/funcionario/funcionario/create")."'>";
-						return $this->render('/mensaje/mensaje',['msg' => $msg, 'url' => $url, 'tipoError' => $tipoError]);
-
-					} else {
-						$transaccion->rollBack();
-						$tipoError = 1; // Error.
-						$msg = "AH ERROR OCCURRED!....WAIT";
- 						$url =  "<meta http-equiv='refresh' content='3; ".Url::toRoute("/funcionario/funcionario/create")."'>";
-						return $this->render('/mensaje/mensaje',['msg' => $msg, 'url' => $url, 'tipoError' => $tipoError]);
-					}
-					$this->connLocal->close();
-
-	      		} else {
-	      			// Array de mensaje de Errores, segun las validaciones del modelo ($model).
-	      			//die(var_dump($model->getErrors()));.
-	      			$model->getErrors();
-
-	      		}
-	      	}
-
-      		// Create es una vista que redirecciona a el formulario principal de carga.
-      		return $this->render('/funcionario/create', ['model' => $model, 'msg' => $msg]);
-	  	}
-
-
-
-	  	public function actionPrueba()
-	  	{
-
-	  		$s = New TransaccionInmobiliaria();
-	  		//$s = New SolicitudPlanillaSearch(22621);
-die(var_dump($s->iniciarCalculoTransaccion(625000, 2016, 1)));
-
-
-	  	}
 	}
 
  ?>
