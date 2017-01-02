52d05f09d0956de3217fb9032e9be94f976f085e
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-02 13:55:45 +0000

    Actualizar

--- simwebplusteq/trunk/backend/models/vehiculo/liquidar/Liquidar.php
+++ simwebplusteq/trunk/backend/models/vehiculo/liquidar/Liquidar.php
@@ -326,6 +326,7 @@
 					$fechaVcto = OrdenanzaBase::getFechaVencimientoSegunFecha(date('Y-m-d'));
 
 					$recargo = New Recargo(self::IMPUESTO);
+					$interes = New Interes(self::IMPUESTO);
 
 					$j = 0;
 					for ( $i = $desdePeriodo; $i <= $hastaPeriodo; $i++) {
