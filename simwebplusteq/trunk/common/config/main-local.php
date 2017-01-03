c73e9b562807455bb7c5dbe34d4d94be0dc17d59
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-03 19:39:56 +0000

    Actualizar

--- simwebplusteq/trunk/common/config/main-local.php
+++ simwebplusteq/trunk/common/config/main-local.php
@@ -4,15 +4,15 @@ return [
 	'components' => [
 		'db'        => [
 			'class'    => 'yii\db\Connection',
-			'dsn' => 'mysql:host=localhost;dbname=sim_teq',
-			//'dsn' => 'mysql:host=192.168.30.7;dbname=sim_teq',
+			//'dsn' => 'mysql:host=localhost;dbname=sim_teq',
+			'dsn' => 'mysql:host=192.168.30.7;dbname=sim_teq',
 			//'dsn' => 'mysql:host=pinky.ddns.net:8383;dbname=sim_teq',
-			//'username' => 'adminyii',
-			//'password' => 'adminyii2015',
+			'username' => 'adminyii',
+			'password' => 'adminyii2015',
 			//'username' => 'jperez',
 			//'password' => 'jperez',
-			'username' => 'root',
-			'password' => '',
+			//'username' => 'root',
+			//'password' => '',
 			'charset'  => 'utf8',
 		],
 		'dbsim' => [
