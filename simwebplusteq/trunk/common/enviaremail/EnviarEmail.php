6c00f27a52945075f2f8c785b413b7cbe47a3704
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-03 19:35:44 +0000

    Actualizar

--- simwebplusteq/trunk/common/enviaremail/EnviarEmail.php
+++ simwebplusteq/trunk/common/enviaremail/EnviarEmail.php
@@ -6,46 +6,46 @@
  */
 
  /**
- * 
- *  > This library is free software; you can redistribute it and/or modify it under 
- *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free 
- *  > Software Foundation; either version 2 of the Licence, or (at your opinion) 
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
  *  > any later version.
- *  > 
- *  > This library is distributed in the hope that it will be usefull, 
- *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability 
- *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence 
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
  *  > for more details.
- *  > 
+ *  >
  *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
  *
  */
 
- /**    
+ /**
  *  @file EnviarEmail.php
- *  
+ *
  *  @author Manuel Alejandro Zapata Canelon
- * 
+ *
  *  @date 04/01/2016
- * 
+ *
  *  @class EnviarEmail
- *  @brief Modelo que contiene el metodo que se encarga de enviar el email al usuario automaticamente con su usuario y contraseña. 
- * 
- *  
- * 
- *  
- *  
+ *  @brief Modelo que contiene el metodo que se encarga de enviar el email al usuario automaticamente con su usuario y contraseña.
+ *
+ *
+ *
+ *
+ *
  *  @property
  *
- *  
+ *
  *  @method
- * 
+ *
  *  enviarEmail
- *  
+ *
  *
  *  @inherits
- *  
- */ 
+ *
+ */
 
 namespace common\enviaremail;
 
@@ -67,7 +67,7 @@ class EnviarEmail{
     {
 
        return Yii::$app->mailer->compose()
-        ->setFrom('manuelz0510@gmail.com')
+        ->setFrom('pruebasimteq@gmail.com')
         ->setTo($email)
         ->setSubject('Bienvenido al Servicio Online')
         ->setTextBody('Bienvenido al Servicio Online')
