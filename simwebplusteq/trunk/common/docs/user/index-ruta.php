deae14c837048f1478039b5bfe825f0edf020739
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-03 03:43:20 +0000

    Modulo de ayuda del usuario

---
+++ simwebplusteq/trunk/common/docs/user/index-ruta.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ *	@copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *	> This library is free software; you can redistribute it and/or modify it under
+ *	> the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *	> Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *	> any later version.
+ *  >
+ *	> This library is distributed in the hope that it will be usefull,
+ *	> but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *	> or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *	> for more details.
+ *  >
+ *	> See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *	@file index-ruta.php
+ *
+ *	@author Jose Rafael Perez Teran
+ *	@email jperez320@gmail.com - jperez820@hotmail.com
+ *
+ *	@date 22-12-2015
+ *
+ *	@brief Catalogo de rutas de las ayudas
+ *
+ *
+ */
+
+
+ 	$rutaBaseAAEE = '../../common/docs/user/frontend/aaee/';
+ 	$rutaBaseInmueble = '../../common/docs/user/frontend/aaee/';
+ 	$rutaBaseVehiculo = '../../common/docs/user/frontend/aaee/';
+
+	return [
+		'1' => $rutaBaseAAEE,
+		'2' => $rutaBaseAAEE,
+		'3' => $rutaBaseAAEE,
+		'4' => $rutaBaseAAEE,
+		'5' => $rutaBaseAAEE,
+		'6' => $rutaBaseAAEE,
+		'7' => $rutaBaseAAEE,
+		'8' => $rutaBaseAAEE,
+		'10' => $rutaBaseAAEE,
+		'11' => $rutaBaseAAEE,
+		'12' => $rutaBaseAAEE,
+		'13' => $rutaBaseAAEE,
+		'14' => $rutaBaseAAEE,
+		'15' => $rutaBaseAAEE,
+		'16' => $rutaBaseAAEE,
+		'17' => $rutaBaseAAEE,
+		'18' => $rutaBaseAAEE,
+		'19' => $rutaBaseAAEE,
+		'69' => $rutaBaseAAEE,
+		'70' => $rutaBaseAAEE,
+		'71' => $rutaBaseAAEE,
+		'73' => $rutaBaseAAEE,
+		'77' => $rutaBaseAAEE,
+		'78' => $rutaBaseAAEE,
+
+		'20' => $rutaBaseInmueble,
+		'21' => $rutaBaseInmueble,
+		'22' => $rutaBaseInmueble,
+		'23' => $rutaBaseInmueble,
+		'24' => $rutaBaseInmueble,
+		'25' => $rutaBaseInmueble,
+		'26' => $rutaBaseInmueble,
+		'27' => $rutaBaseInmueble,
+		'28' => $rutaBaseInmueble,
+		'29' => $rutaBaseInmueble,
+		'30' => $rutaBaseInmueble,
+		'31' => $rutaBaseInmueble,
+		'65' => $rutaBaseInmueble,
+		'67' => $rutaBaseInmueble,
+		'79' => $rutaBaseInmueble,
+		'82' => $rutaBaseInmueble,
+
+		'32' => $rutaBaseVehiculo,
+		'33' => $rutaBaseVehiculo,
+		'34' => $rutaBaseVehiculo,
+		'35' => $rutaBaseVehiculo,
+		'36' => $rutaBaseVehiculo,
+		'37' => $rutaBaseVehiculo,
+		'38' => $rutaBaseVehiculo,
+		'66' => $rutaBaseVehiculo,
+		'68' => $rutaBaseVehiculo,
+		'80' => $rutaBaseVehiculo,
+		'83' => $rutaBaseVehiculo,
+	]
+
+
+?>
