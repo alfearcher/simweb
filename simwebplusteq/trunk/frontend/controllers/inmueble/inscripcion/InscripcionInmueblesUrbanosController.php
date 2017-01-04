3b398fd46930730f2981337f144aa1f35d41ece1
alvarof <alvarof@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-04 14:51:54 +0000

    modifi

--- simwebplusteq/trunk/frontend/controllers/inmueble/inscripcion/InscripcionInmueblesUrbanosController.php
+++ simwebplusteq/trunk/frontend/controllers/inmueble/inscripcion/InscripcionInmueblesUrbanosController.php
@@ -127,7 +127,7 @@ tablas: solicitudes_contribuyente, sl_inmuebles, config_tipos_solicitudes
                  //condicionales     
                    $documento = new DocumentoSolicitud();
 
-                   $requisitos = $documento->documentosSol();
+                   $requisitos = $documento->documentos();
 
                  
                 if (!\Yii::$app->user->isGuest){                                    
