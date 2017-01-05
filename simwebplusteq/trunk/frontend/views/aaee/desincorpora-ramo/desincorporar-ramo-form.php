5b09e658436be15959a58b4d6c8548aeaab949c3
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-05 16:42:47 +0000

    Actualizar

--- simwebplusteq/trunk/frontend/views/aaee/desincorpora-ramo/desincorporar-ramo-form.php
+++ simwebplusteq/trunk/frontend/views/aaee/desincorpora-ramo/desincorporar-ramo-form.php
@@ -176,18 +176,32 @@
 			</div>  	<!-- Fin de container-fluid -->
 
 			<div class="row">
-				<div class="col-sm-3">
+				<div class="col-sm-3" style="margin-left: 150px;margin-top:20px;">
 					<div class="form-group">
 						<?= Html::submitButton(Yii::t('frontend', Yii::t('frontend', 'Quit')),
 																			  [
 																				'id' => 'btn-quit',
 																				'class' => 'btn btn-danger',
 																				'value' => 1,
-																				'style' => 'width: 100%; margin-left: 300px;margin-top:20px;',
+																				'style' => 'width: 100%;',
 																				'name' => 'btn-quit',
 
 																			  ])
 						?>
+					</div>
+				</div>
+
+				<div class="col-sm-2" style="margin-left: 50px;margin-top:20px;">
+					<div class="form-group">
+					<!-- '../../common/docs/user/ayuda.pdf'  funciona -->
+						 <?= Html::a(Yii::t('backend', 'Ayuda'), $rutaAyuda,  [
+																		'id' => 'btn-help',
+																		'class' => 'btn btn-default',
+																		'name' => 'btn-help',
+																		'target' => '_blank',
+																		'value' => 1,
+																		'style' => 'width: 100%;'
+							])?>
 					</div>
 				</div>
 			</div>
