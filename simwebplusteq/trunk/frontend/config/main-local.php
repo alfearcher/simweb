10e2cc5dc20c70a63093c5e8d2d4d4f3ebe7c7f2
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-08 20:02:20 +0000

    Actualizar

--- simwebplusteq/trunk/frontend/config/main-local.php
+++ simwebplusteq/trunk/frontend/config/main-local.php
@@ -2,26 +2,26 @@
 
 $config = [
 
-    
+
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'M6wuaP1Z3w14MqiKjJFt2EVcLhTQac88',
 
-      
 
-            
+
+
         ],
     ],
 ];
 
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+    // $config['bootstrap'][] = 'debug';
+    // $config['modules']['debug'] = 'yii\debug\Module';
 
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
+    // $config['bootstrap'][] = 'gii';
+    // $config['modules']['gii'] = 'yii\gii\Module';
 }
 
 return $config;
