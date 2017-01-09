277f56516575f3510b594b514bf30e745b0a0fe6
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-09 02:10:15 +0000

    Actualizar

--- simwebplusteq/trunk/backend/models/aaee/inscripcionactecon/InscripcionActividadEconomicaSearch.php
+++ simwebplusteq/trunk/backend/models/aaee/inscripcionactecon/InscripcionActividadEconomicaSearch.php
@@ -167,7 +167,7 @@
 	     */
 	    public function poseeDeclaracion()
 	    {
-	    	return $result = ActEcon::find()->where('id_contribuyente =:id_contribuyente',
+	    	$result = ActEcon::find()->where('id_contribuyente =:id_contribuyente',
 		    	 										[':id_contribuyente' => $this->id_contribuyente])
 		    								->andWhere('estatus =:estatus', [':estatus' => 0])
 		    								->exists();
