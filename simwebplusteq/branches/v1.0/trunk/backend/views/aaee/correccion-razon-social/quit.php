886eff04051439a4b2a13106ed165b6074e74027
arivas <arivas@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-12 01:16:45 +0000

    Borrar solo de prueba

--- simwebplusteq/branches/v1.0/trunk/backend/views/aaee/correccion-razon-social/quit.php
+++
@@ -1,6 +0,0 @@
-<?php
-	use yii\helpers\Html;
-	//use yii\web\View;
-	/* @var $this yii\web\View */
-	//return $this->render('/menu/vertical');
-?>
--- simwebplusteq/branches/v1.0/trunk/backend/views/funcionario/quit.php
+++
@@ -1,6 +0,0 @@
-<?php
-	use yii\helpers\Html;
-
-	return $this->render('/menu/menuvertical2');
-?>
-
