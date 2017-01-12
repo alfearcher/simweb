e5c6e366cbfb63f010f13103b8375a9792b8d737
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-12 19:20:49 +0000

    Actualizar

--- simwebplusteq/trunk/backend/models/aaee/listado/ListadoSolicitudDeclaracion.php
+++ simwebplusteq/trunk/backend/models/aaee/listado/ListadoSolicitudDeclaracion.php
@@ -46,7 +46,6 @@
 	use yii\web\NotFoundHttpException;
 	use backend\models\solicitud\estatus\EstatusSolicitud;
 	use backend\models\aaee\declaracion\tipodeclaracion\TipoDeclaracion;
-	use backend\models\aaee\rubro\Rubro;
 	use common\models\solicitudescontribuyente\SolicitudesContribuyente;
 	use yii\data\ArrayDataProvider;
 	use yii\data\ActiveDataProvider;
@@ -54,7 +53,10 @@
 	use yii\db\ActiveRecord;
 	use yii\base\Model;
 	use common\models\planilla\PagoDetalle;
-
+	use yii\helpers\ArrayHelper;
+	use yii\db\Query;
+	use backend\models\configuracion\tiposolicitud\TipoSolicitud;
+	use common\models\contribuyente\ContribuyenteBase;
 
 	/**
 	* Clase que gestiona la emision de un listado de las solicitudes de las declaraciones.
@@ -65,13 +67,138 @@
 		public $fecha_desde;
 		public $fecha_hasta;
 		public $tipo_solicitud;
+		public $nro_solicitud;
+		public $impuesto;
 		public $fecha_hora_creacion;
+		public $estatus_solicitud;
+		public $ordenar_declaracion;
+
+
+		const SCENARIO_NRO_SOLICITUD = 'numero_solicitud';
+		const SCENARIO_TIPO = 'tipo';
+		const SCENARIO_DEFAULT = 'default';
+
+
+		/**
+     	* @inheritdoc
+     	*/
+    	public function scenarios()
+    	{
+        	// bypass scenarios() implementation in the parent class
+        	//return Model::scenarios();
+        	return [
+	        	self::SCENARIO_NRO_SOLICITUD => [
+	        					'nro_solicitud',
+
+	        		],
+        		self::SCENARIO_TIPO => [
+        						'impuesto',
+	        					'tipo_solicitud',
+	        					'fecha_desde',
+	        					'fecha_hasta',
+	        					'estatus_solicitud',
+	        					'ordenar_declaracion',
+        		],
+        		self::SCENARIO_DEFAULT => [
+        						Model::scenarios(),
+        		],
+	        ];
+    	}
+
+
+
+
+    	/***/
+    	public function search($params)
+    	{
+    		return self::getDataProvider($params);
+
+    	}
+
+
+
+    	/***/
+    	public function getDataProvider($params)
+    	{
+			$query = New Query();
+			$rows = [];
+    		if ( $this->ordenar_declaracion == 0 ) {
+
+	    		$rows = $query->select('S.nro_solicitud,S.fecha_hora_creacion,
+	    			                    S.id_contribuyente,D.ano_impositivo,
+	    			                    T.descripcion,E.descripcion as condicion,
+	    			                    D.tipo_declaracion,
+	    			                    sum(D.monto_new) as suma')
+	    				      ->from(SolicitudesContribuyente::tableName() . ' S')
+	    				      ->join('INNER JOIN', DeclaracionBase::tableName() . ' D', 'S.nro_solicitud=D.nro_solicitud')
+	    				      ->join('INNER JOIN', EstatusSolicitud::tableName() . ' E', 'S.estatus=E.estatus_solicitud')
+	    				      ->join('INNER JOIN', TipoSolicitud::tableName() . ' T', 'S.tipo_solicitud=T.id_tipo_solicitud')
+	    				      ->where(['BETWEEN', 'date(fecha_hora_creacion)',
+	    				      		 						date('Y-m-d',strtotime($this->fecha_desde)),
+			    	      					 				date('Y-m-d',strtotime($this->fecha_hasta))])
+	    				      ->andWhere('S.tipo_solicitud =:tipo_solicitud',
+	    				      								[':tipo_solicitud' => $this->tipo_solicitud])
+	    				      ->andWhere('S.impuesto =:impuesto',
+	    				      				[':impuesto' => $this->impuesto])
+	    				      ->andWhere('S.estatus =:estatus',
+	    				      				[':estatus' => $this->estatus_solicitud])
+	    				      ->groupBy(
+	    				      		'S.nro_solicitud'
+	    				      	);
+	    				      // ->all();
+
+    		} elseif ( $this->ordenar_declaracion == 1 ) {
+
+    			$rows = $query->select('S.nro_solicitud,S.fecha_hora_creacion,
+	    			                    S.id_contribuyente,D.ano_impositivo,
+	    			                    T.descripcion,E.descripcion as condicion,
+	    			                    D.tipo_declaracion,
+	    			                    sum(D.monto_new) as suma')
+	    				      ->from(SolicitudesContribuyente::tableName() . ' S')
+	    				      ->join('INNER JOIN', DeclaracionBase::tableName() . ' D', 'S.nro_solicitud=D.nro_solicitud')
+	    				      ->join('INNER JOIN', EstatusSolicitud::tableName() . ' E', 'S.estatus=E.estatus_solicitud')
+	    				      ->join('INNER JOIN', TipoSolicitud::tableName() . ' T', 'S.tipo_solicitud=T.id_tipo_solicitud')
+	    				      ->where(['BETWEEN', 'date(fecha_hora_creacion)',
+	    				      		 						date('Y-m-d',strtotime($this->fecha_desde)),
+			    	      					 				date('Y-m-d',strtotime($this->fecha_hasta))])
+	    				      ->andWhere('S.tipo_solicitud =:tipo_solicitud',
+	    				      								[':tipo_solicitud' => $this->tipo_solicitud])
+	    				      ->andWhere('S.impuesto =:impuesto',
+	    				      				[':impuesto' => $this->impuesto])
+	    				      ->andWhere('S.estatus =:estatus',
+	    				      				[':estatus' => $this->estatus_solicitud])
+	    				      ->groupBy(
+	    				      		'S.nro_solicitud'
+	    				      	)
+	    				      ->orderBy([
+	    				      		'suma'=> SORT_DESC,
+	    				      	]);
+	    				     // ->all();
+
+    		}
+
+
+    		$dataProvider = New ActiveDataProvider([
+							'query' => $query,
+							'pagination' => [
+        						'pageSize' => 30,
+    						],
+					]);
+
+    		return $dataProvider;
+
+    	}
+
+
+
+
+
 
 
 
 
 		/***/
-		public function search($params)
+		public function search1($params)
 		{
 			$query = self::findSolicitudDeclaracionModel();
 
@@ -80,19 +207,40 @@
 							'pagination' => [
         						'pageSize' => 30,
     						],
+    						'sort' => [
+    							'sum(D.monto_new)' => SORT_ASC,
+    						],
 					]);
-			$this->load($params);
+	  //       if (!$this->validate()) {
+	  //           // uncomment the following line if you do not want to any records when validation fails
+	  //           // $query->where('0=1');
+	  //           return $dataProvider;
+	  //       }
 
-	        if (!$this->validate()) {
-	            // uncomment the following line if you do not want to any records when validation fails
-	            // $query->where('0=1');
-	            return $dataProvider;
-	        }
 
-	        // $query->andFilterWhere([
-	        //     'fecha_hora_creacion' => '',
-	        // ]);
+	    	if ( $this->tipo_solicitud > 0 ) {
+		   		$query->andFilterWhere(['=', 'tipo_solicitud1', $this->tipo_solicitud]);
+		   	}
+	    	if ( $this->fecha_desde !== null && $this->fecha_hasta !== null ) {
 
+		    	$query->andFilterWhere(['BETWEEN','date(fecha_hora_creacion)',
+		    	      					 date('Y-m-d',strtotime($this->fecha_desde)),
+		    	      					 date('Y-m-d',strtotime($this->fecha_hasta))]);
+		   	}
+		   	if ( $this->impuesto > 0 ) {
+		   		$query->andFilterWhere(['=', 'impuesto', $this->impuesto]);
+		   	}
+		   	if ( $this->nro_solicitud > 0 ) {
+		   		$query->andFilterWhere(['=', 'nro_solicitud', $this->nro_solicitud]);
+		   	}
+
+		   	if ( $this->estatus_solicitud !== null ) {
+		   		$query->andFilterWhere(['=', 'estatus_solicitud', $this->estatus_solicitud]);
+		   	}
+
+		   	if ( $this->nro_solicitud !== null ) {
+		   		$query->andFilterWhere(['=', 'nro_solicitud', $this->nro_solicitud]);
+		   	}
 
 	        return $dataProvider;
 
@@ -111,22 +259,20 @@
 		public function rules()
 	    {
 	        return [
-	        	[['tipo_solicitud','fecha_desde', 'fecha_hasta'], 'required'],
-	        	[['tipo_solicitud'], 'integer'],
+	        	[['tipo_solicitud','fecha_desde',
+	        	  'fecha_hasta', 'estatus_solicitud',],
+	        	  'required', 'on' => 'tipo',
+	        	  'message' => Yii::t('frontend', '{attribute} is required')],
+	        	 [['nro_solicitud'],
+	        	  'required', 'on' => 'numero_solicitud',
+	        	  'message' => Yii::t('frontend', '{attribute} is required')],
+	        	[['tipo_solicitud', 'nro_solicitud',
+	        	  'impuesto', 'ordenar_declaracion',
+	        	   'estatus_solicitud',],
+	        	   'integer'],
 	        ];
 	    }
 
-
-
-
-		/***/
-		public function findSolicitudDeclaracionModel()
-		{
-			return $findModel = SolicitudesContribuyente::find()->alias('S')
-													   ->where('D.estatus !=:estatus',[':estatus' => 9])
-													   ->andWhere('S.inactivo =:inactivo',[':inactivo' => 0])
-													   ->joinWith('declaracion D', true, 'INNER JOIN');
-		}
 
 
 
@@ -154,6 +300,26 @@
 
 			return $suma;
 		}
+
+
+
+		/**
+		 * Metodo que permite obtener una lista para los lista-combo
+		 * @return [type] [description]
+		 */
+		public function getListaEstatus()
+		{
+			$model = EstatusSolicitud::find()->all();
+			return ArrayHelper::map($model,'estatus_solicitud', 'descripcion');
+		}
+
+
+		/***/
+		public function getContribuyente($idContribuyente)
+		{
+			return ContribuyenteBase::getContribuyenteDescripcionSegunID($idContribuyente);
+		}
+
 
 
 	}
