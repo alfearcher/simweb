2024c3ab5c37ce05f1b8a2550deaf125ab00abb9
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-16 02:25:48 +0000

    Actualizar

--- simwebplusteq/trunk/backend/controllers/tasa/liquidar/LiquidarTasaController.php
+++ simwebplusteq/trunk/backend/controllers/tasa/liquidar/LiquidarTasaController.php
@@ -61,6 +61,12 @@
 	use backend\models\tasa\liquidar\LiquidarTasaForm;
 	use backend\models\impuesto\ImpuestoForm;
 	use backend\models\utilidad\ut\UnidadTributariaForm;
+	use common\models\planilla\Pago;
+	use common\models\planilla\PagoDetalle;
+	use common\models\ordenanza\OrdenanzaBase;
+	use common\models\planilla\NumeroPlanillaSearch;
+	use common\controllers\pdf\planilla\PlanillaPdfController;
+
 
 	session_start();		// Iniciando session
 
@@ -80,7 +86,7 @@
 
 		public function actionIndex()
 		{
-			self::actionAnularSession(['begin']);
+			self::actionAnularSession(['begin', 'postEnviado', 'planilla']);
 			$_SESSION['begin'] = 1;
 			$this->redirect(['index-create']);
 		}
@@ -101,18 +107,50 @@
 				$request = Yii::$app->request;
 				$postData = $request->post();
 
+				$model = New LiquidarTasaForm();
+
 				if ( isset($postData['btn-quit']) ) {
 					if ( $postData['btn-quit'] == 1 ) {
 						$this->redirect(['quit']);
 					}
+				} elseif ( isset($postData['btn-liquidar']) ) {
+					if ( $postData['btn-liquidar'] == 5 ) {
+
+						if ( $model->load($postData) ) {
+
+							if ( $model->validate(['id_impuesto', 'id_contribuyente']) ) {
+
+								if ( $model->resultado > 0 ) {
+
+									$result = self::actionBeginSave($model, $idContribuyente, $postData);
+									if ( $result ) {
+										$this->_transaccion->commit();
+										$this->_conn->close();
+										$this->redirect(['mostrar-tasa-creada', 'id' => $model->id_pago]);
+
+									} else {
+										$this->_transaccion->commit();
+										$this->_conn->close();
+									}
+								} else {
+									$model->addError('resultado', 'El monto del resultado debe ser mayor a cero (0)');
+								}
+							}
+						}
+					}
+				} elseif ( isset($postData['btn-back']) ) {
+					if ( $postData['btn-back'] == 3 ) {
+						$this->redirect(['index']);
+					}
 				}
 
-				$model = New LiquidarTasaForm();
+
 
 				if ( $model->load($postData)  && Yii::$app->request->isAjax ) {
 					Yii::$app->response->format = Response::FORMAT_JSON;
 					return ActiveForm::validate($model);
 		      	}
+
 
 		      	if ( $model->load($postData) ) {
 
@@ -150,6 +188,10 @@
 				$subCaption = Yii::t('backend', 'Seleccione los parametros solicitados');
 
 				$model->id_contribuyente = $idContribuyente;
+				$model->multiplicar_por = 0;
+				$model->observacion = '';
+				$model->resultado = 0;
+				$model->id_pago = 0;
 				return $this->render('/tasa/liquidar/_create',[
 													'model' => $model,
 													'caption' => $caption,
@@ -179,7 +221,6 @@
 			$subnivel = isset($request['subnivel']) ? $request['subnivel'] : 0;
 			$codigo = isset($request['codigo']) ? $request['codigo'] : 0;
 
-// die(var_dump($request));
 
 			$tasaSearch = New LiquidarTasaSearch();
 			if ( $impuesto > 0 && $año == 0 && $idCodigo == 0 && $subnivel == 0 ) {
@@ -225,6 +266,198 @@
 			return $montoUt;
 		}
 
+
+
+		/**
+		 * Metodo que inicia el proceos para guardar la liquidacion
+		 * @param PagoDetall $models arreglo de modelo de la clase PagoDetalle().
+		 * @param integer $idContribuyente identificador del contribuyente.
+		 * @return boolean retorna true o false.
+		 */
+		private function actionBeginSave($model, $idContribuyente, $postEnviado)
+		{
+
+			$result = false;
+			$idPago = 0;
+			$this->_conexion = New ConexionController();
+
+  			// Instancia de conexion hacia la base de datos.
+  			$this->_conn = $this->_conexion->initConectar('db');
+  			$this->_conn->open();
+
+  			// Instancia de tipo transaccion para asegurar la integridad del resguardo de los datos.
+  			// Inicio de la transaccion.
+			$this->_transaccion = $this->_conn->beginTransaction();
+
+			// Se genera otra planilla para los detalles de la liquiadcion.
+			$idPago = self::actionGuardarPago($this->_conexion, $this->_conn, $idContribuyente);
+			if ( $idPago > 0 ) {
+				$_SESSION['idPago'] = $idPago;
+				$fechaVcto = OrdenanzaBase::getFechaVencimientoSegunFecha(date('Y-m-d'));
+				$observacion = $postEnviado['codigo-presupuesto-descripcion'] . ' / ' .
+							   $postEnviado['codigo-descripcion'] . ' / ' .
+							   $model['observacion'];
+
+				$model->id_pago = $idPago;
+
+				$pagoDetalleModel = New PagoDetalle();
+				$pagoDetalleModel['id_pago'] = $idPago;
+				$pagoDetalleModel['id_impuesto'] = $model['id_impuesto'];
+				$pagoDetalleModel['impuesto'] = $model['impuesto'];
+				$pagoDetalleModel['ano_impositivo'] = $model['ano_impositivo'];
+				$pagoDetalleModel['trimestre'] = 0;
+				$pagoDetalleModel['monto'] = $model['resultado'];
+				$pagoDetalleModel['recargo'] = 0;
+				$pagoDetalleModel['interes'] = 0;
+				$pagoDetalleModel['descuento'] = 0;
+				$pagoDetalleModel['referencia'] = 0;
+				$pagoDetalleModel['pago'] = 0;
+				$pagoDetalleModel['fecha_emision'] = date('Y-m-d');
+				$pagoDetalleModel['fecha_pago'] = '0000-00-00';
+				$pagoDetalleModel['fecha_vcto'] = $fechaVcto;
+				$pagoDetalleModel['descripcion'] = $observacion;
+				$pagoDetalleModel['monto_reconocimiento'] = 0;
+				$pagoDetalleModel['exigibilidad_pago'] = 99;
+				$pagoDetalleModel['fecha_desde'] = '0000-00-00';
+				$pagoDetalleModel['fecha_hasta'] = '0000-00-00';
+
+				$result = self::actionGuardarDetalle($pagoDetalleModel, $this->_conexion, $this->_conn);
+			}
+
+			return $result;
+
+		}
+
+
+
+
+		/**
+		 * Metodo que guarda los detalle de la liquidacion
+		 * @param PagoDetalle $model arreglo de modelo de la clase PagoDetella().
+		 * @param ConexionController $conexion [description]
+		 * @param  [type] $conn     [description]
+		 * @return boolean retorna true o false.
+		 */
+		private function actionGuardarDetalle($model, $conexion, $conn)
+		{
+			$result = false;
+			if ( count($model) > 0 ) {
+				$tabla = $model->tableName();
+
+				$result = $conexion->guardarRegistro($conn, $tabla, $model->attributes);
+
+			}
+
+			return $result;
+		}
+
+
+
+
+
+		/**
+		 * Metodo que guarda el maetro de la planilla y ademas se genera el numero de la misma.
+		 * @param  ConexionController $conexion        [description]
+		 * @param  [type] $conn            [description]
+		 * @param  integer $idContribuyente identificador del contribuyente.
+		 * @return integer retorna el identificador de la entidad "pagos".
+		 */
+		private function actionGuardarPago($conexion, $conn, $idContribuyente)
+		{
+			$idPago = 0;
+			$numero = 0;
+			$pago = New Pago();
+			$tabla = $pago->tableName();
+
+			$planillaSearch = New NumeroPlanillaSearch();
+
+			$intento = 10;
+			while ( $intento >= 1 ) {
+				$numero = $planillaSearch->getGenerarNumeroPlanilla();
+				if ( $numero > 0 ) {
+					break;
+				} else {
+					$intento--;
+				}
+			}
+
+			if ( $numero > 0 ) {
+				$pago->ente = Yii::$app->ente->getEnte();
+				$pago->id_contribuyente = $idContribuyente;
+				$pago->planilla = $numero;
+				$pago->status_pago = 0;
+				$pago->notificado = 0;
+				$pago->ult_act = date('Y-m-d');
+				$pago->recibo = 0;
+				$pago->id_moneda = 1;
+				$pago->exigibilidad_deuda = 0;
+
+				$arregloDatos = $pago->attributes;
+
+				if ( $conexion->guardarRegistro($conn, $tabla, $arregloDatos) ) {
+					$idPago = $conn->getLastInsertID();
+				}
+			}
+
+			return $idPago;
+
+		}
+
+
+
+
+		/***/
+		public function actionMostrarTasaCreada($id)
+		{
+			if ( $_SESSION['idPago'] == $id ) {
+				self::actionAnularSession(['idPago']);
+				$model = PagoDetalle::find()->alias('D')
+				                            ->joinWith('pagos P', true, 'INNER JOIN')
+				                            ->where('D.id_pago =:id_pago',[':id_pago' => $id])
+				                            ->asArray()
+				                            ->all();
+
+				if ( $model !== null ) {
+
+					$_SESSION['planilla'] = $model[0]['pagos']['planilla'];
+					$searchTasa = New LiquidarTasaSearch();
+					$tasa = $searchTasa->findTasa($model[0]['id_impuesto']);
+
+					$caption = Yii::t('backend', 'Tasa liquidada');
+					return $this->render('/tasa/liquidar/_view-create',[
+												'model' => $model,
+												'tasa' => $tasa,
+												'caption' => $caption,
+												'codigo' => 100,
+						]);
+				}
+			} else {
+
+			}
+		}
+
+
+
+		/***/
+		public function actionGenerarPdf($p)
+		{
+			$request = Yii::$app->request;
+			$postData = $request->post();
+
+			if ( isset($_SESSION['planilla']) ) {
+
+				if ( $_SESSION['planilla'] == $p ) {
+					$planilla = $p;
+					$pdf = New PlanillaPdfController($planilla);
+					$pdf->actionGenerarPlanillaPdf();
+				} else {
+					$this->redirect(['quit']);
+				}
+			} else {
+				$this->redirect(['quit']);
+			}
+
+		}
 
 
 
