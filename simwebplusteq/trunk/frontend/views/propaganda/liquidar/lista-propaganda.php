d2ffc71112c669ca5e1c4cc0c3b893dfa1c5d509
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-22 02:33:15 +0000

    Vista de la liquidacion de la liquidacion de la propaganda comercial por parte del usuario

---
+++ simwebplusteq/trunk/frontend/views/propaganda/liquidar/lista-propaganda.php
@@ -0,0 +1,210 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file lista-propaganda.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 21-11-2016
+ *
+ *  @view lista-propaganda.php
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *
+ *  @inherits
+ *
+ */
+
+ 	use yii\web\Response;
+ 	use kartik\icons\Icon;
+ 	use yii\grid\GridView;
+	use yii\helpers\Html;
+	use yii\helpers\Url;
+	use yii\helpers\ArrayHelper;
+	use yii\widgets\ActiveForm;
+	use yii\web\View;
+
+	$typeIcon = Icon::FA;
+  	$typeLong = 'fa-2x';
+
+    Icon::map($this, $typeIcon);
+
+ ?>
+
+
+<div class="lista-propaganda-activo">
+ 	<?php
+
+ 		$form = ActiveForm::begin([
+ 			'id' => 'id-lista-propaganda-form',
+ 			'method' => 'post',
+ 			'action' => $url,
+ 			'enableClientValidation' => true,
+ 			'enableAjaxValidation' => false,
+ 			'enableClientScript' => true,
+ 		]);
+ 	?>
+
+
+	<meta http-equiv="refresh">
+    <div class="panel panel-primary" style="width: 100%;">
+        <div class="panel-heading">
+        	<h3><?= Html::encode($caption) ?></h3>
+        </div>
+
+<!-- Cuerpo del formulario -->
+<!-- style="background-color: #F9F9F9; -->
+        <div class="panel-body" >
+        	<div class="container-fluid">
+        		<div class="col-sm-12">
+
+
+					<div class="row" style="width:100%;">
+						<div class="row" style="border-bottom: 1px solid #ccc;background-color:#F1F1F1;padding-left:10px;padding-top: 0px;">
+							<h4><?=Html::encode(Yii::t('frontend', $subCaption))?></h4>
+						</div>
+						<div class="row" id="id-lista-propaganda" style="padding: 0px;">
+							<?= GridView::widget([
+								'id' => 'id-grid-lista-propaganda',
+								'dataProvider' => $dataProvider,
+								'headerRowOptions' => [
+									'class' => 'success',
+								],
+								'tableOptions' => [
+                    				'class' => 'table table-hover table-bordered',
+              					],
+								'summary' => '',
+								'columns' => [
+									['class' => 'yii\grid\SerialColumn'],
+									[
+				                        'class' => 'yii\grid\CheckboxColumn',
+				                        'name' => 'chkIdImpuesto',
+				                        'multiple' => false,
+				                    ],
+				                    [
+				                        'contentOptions' => [
+				                              'style' => 'font-size: 90%;',
+				                        ],
+				                        'label' => Yii::t('frontend', 'Id'),
+				                        'value' => function($data) {
+				                                   		return $data['id_impuesto'];
+				            			           },
+				                    ],
+				                    [
+				                        'contentOptions' => [
+				                              'style' => 'font-size: 90%;',
+				                        ],
+				                        'label' => Yii::t('frontend', 'Nombre de la Propaganda'),
+				                        'value' => function($data) {
+				                                   		return $data['nombre_propaganda'];
+				            			           },
+				                    ],
+				                    [
+				                        'contentOptions' => [
+				                              'style' => 'font-size: 90%;',
+				                        ],
+				                        'label' => Yii::t('frontend', 'direccion'),
+				                        'value' => function($data) {
+				                                   		return $data['direccion'];
+				            			           },
+				                    ],
+				                    [
+				                        'contentOptions' => [
+				                              'style' => 'font-size: 90%;',
+				                        ],
+				                        'label' => Yii::t('frontend', 'ultimo lapso'),
+				                        'value' => function($data, $ultimo) {
+				                        				$ultimo = $data['añoImpositivo'] . ' - ' . $data['periodo'] . ' - ' . $data['unidad'];
+				                                   		return $ultimo;
+				            			           },
+				                    ],
+				                    [
+				                        'contentOptions' => [
+				                              'style' => 'font-size: 90%;',
+				                        ],
+				                        'label' => Yii::t('frontend', 'planilla / condicion'),
+				                        'value' => function($data, $ultimo) {
+				                        				$ultimo = $data['planilla'] . ' / ' . $data['condicion'];
+				                                   		return $ultimo;
+				            			           },
+				                    ],
+
+					        	]
+							]);?>
+						</div>
+					</div>
+
+					<div class="row" style="border-bottom: 2px solid #ccc;padding: 0px;width: 103%;margin-left: -30px;">
+					</div>
+
+					<div class="row" style="width: 100%;padding: 0px;margin-top: 20px;">
+							<div class="col-sm-3" style="width: 25%;padding: 0px;padding-left: 15px;">
+								<div class="form-group">
+									<?= Html::submitButton(Yii::t('frontend', 'Iniciar Liquidacion'),
+																					  [
+																						'id' => 'btn-begin',
+																						'class' => 'btn btn-success',
+																						'value' => 3,
+																						'style' => 'width: 100%;',
+																						'name' => 'btn-begin',
+
+																					  ])
+									?>
+								</div>
+							</div>
+
+
+							<div class="col-sm-3" style="width: 25%;padding: 0px; padding-left: 25px;margin-left:30px;">
+								<div class="form-group">
+									<?= Html::submitButton(Yii::t('frontend', 'Quit'),
+																			  [
+																				'id' => 'btn-quit',
+																				'class' => 'btn btn-danger',
+																				'value' => 1,
+																				'style' => 'width: 100%;',
+																				'name' => 'btn-quit',
+
+																			  ])
+									?>
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+				</div>  <!-- Fin de col-sm-12 -->
+			</div>  	<!-- Fin de container-fluid -->
+
+		</div>			<!-- Fin panel-body-->
+
+	</div>	<!-- Fin de panel panel-primary -->
+
+ 	<?php ActiveForm::end(); ?>
+</div>	 <!-- Fin de inscripcion-act-econ-form -->
+
+
