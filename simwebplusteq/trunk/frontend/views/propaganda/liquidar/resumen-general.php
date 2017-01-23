3eea81054960ee3b02ec710f2af5c1a4105c0519
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-23 21:10:15 +0000

    Actualizar

--- simwebplusteq/trunk/frontend/views/propaganda/liquidar/resumen-general.php
+++ simwebplusteq/trunk/frontend/views/propaganda/liquidar/resumen-general.php
@@ -88,15 +88,7 @@
 						<div class="col-5" style="padding: 0px;">
 							<?= MensajeController::actionMensaje($codigo); ?>
 						</div>
-						<div class="col-sm-3" style="padding: 0px; padding-top:5px;">
-							<!-- Boton para ir a consulta de planilla -->
-							<?=Html::submitButton('Ir a consulta de planillas',[
-														'class' => 'btn btn-primary',
-														'id' => 'btn-consulta-planilla',
-														'name' => 'btn-consulta-planilla',
-														'value' => 9,
-							]) ?>
-						</div>
+
 	 				</div>
 
 					<div class="row" style="padding:0px;padding-top: 5px;">
