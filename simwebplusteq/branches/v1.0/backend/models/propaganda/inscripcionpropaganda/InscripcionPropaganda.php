351c901af6e5b7e53fa55a8fa79f039fd931dbb6
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-23 21:53:20 +0000

    Modelo de la inscripcion de propaganda

---
+++ simwebplusteq/branches/v1.0/backend/models/propaganda/inscripcionpropaganda/InscripcionPropaganda.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file InscripcionPropaganda.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 17-01-2017
+ *
+ *  @class InscripcionPropaganda
+ *  @brief Clase Modelo
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *  @inherits
+ *
+ */
+
+    namespace backend\models\propaganda\inscripcionpropaganda;
+
+    use Yii;
+    use yii\base\Model;
+    use yii\db\ActiveRecord;
+    use backend\models\propaganda\uso\UsoPropaganda;
+    use backend\models\propaganda\clase\ClasePropaganda;
+    use backend\models\propaganda\tipo\TipoPropaganda;
+    use backend\models\propaganda\mediotransporte\MedioTransporte;
+    use backend\models\propaganda\mediodifusion\MedioDifusion;
+    use backend\models\solicitud\estatus\EstatusSolicitud;
+    use common\models\solicitudescontribuyente\SolicitudesContribuyente;
+    use backend\models\utilidad\tiempo\Tiempo;
+    use backend\models\propaganda\Propaganda;
+
+
+
+    /**
+    *  Clase
+    */
+    class InscripcionPropaganda extends ActiveRecord
+    {
+
+        /**
+         *  Metodo que retorna el nombre de la base de datos donde se tiene la conexion actual.
+         *  Utiliza las propiedades y metodos de Yii2 para traer dicha informacion.
+         *  @return Nombre de la base de datos
+         */
+        public static function getDb()
+        {
+            return Yii::$app->db;
+        }
+
+
+        /**
+         *  Metodo que retorna el nombre de la tabla que utiliza el modelo.
+         *  @return Nombre de la tabla del modelo.
+         */
+        public static function tableName()
+        {
+            return 'sl_propagandas';
+        }
+
+
+
+        /**
+         * Relacion con la entidad "usos-propagandas".
+         * @return [type] [description]
+         */
+        public function getUsoPropaganda()
+        {
+            return $this->hasOne(UsoPropaganda::className(), ['uso_propaganda' => 'uso_propaganda']);
+        }
+
+
+
+        /**
+         * Relacion con la entidad "clases-propagandas".
+         * @return [type] [description]
+         */
+        public function getClasePropaganda()
+        {
+            return $this->hasOne(ClasePropaganda::className(), ['clase_propaganda' => 'clase_propaganda']);
+        }
+
+
+
+        /**
+         * Relacion con la entidad "tipos-propagandas".
+         * @return [type] [description]
+         */
+        public function getTipoPropaganda()
+        {
+            return $this->hasOne(TipoPropaganda::className(), ['tipo_propaganda' => 'tipo_propaganda']);
+        }
+
+
+
+        /**
+         * Relacion con la entidad "medios-transportes".
+         * @return [type] [description]
+         */
+        public function getMedioTransporte()
+        {
+            return $this->hasOne(MedioTransporte::className(), ['medio_transporte' => 'medio_transporte']);
+        }
+
+
+
+        /**
+         * Relacion con la entidad "medios-difusion".
+         * @return [type] [description]
+         */
+        public function getMedioDifusion()
+        {
+            return $this->hasOne(MedioDifusion::className(), ['medio_difusion' => 'medio_difusion']);
+        }
+
+
+        /**
+         * Relacion con la entidad "estatus-solicitudes", EstatusSolicitud
+         * @return Active Record.
+         */
+        public function getEstatusSolicitud()
+        {
+            return $this->hasOne(EstatusSolicitud::className(), ['estatus_solicitud' => 'estatus']);
+        }
+
+
+        /**
+         * Metodo que permite obtener la descripcion del tipo de solicitud
+         * @param  long $nroSolicitud identificacion de la solicitud. Autoincremental
+         * que se genera al crear la solicitud.
+         * @return string retorna la descripcion de la solicitud.
+         */
+         public function getDescripcionTipoSolicitud($nroSolicitud)
+         {
+            return $tipo = SolicitudesContribuyente::getDescripcionTipoSolicitud($nroSolicitud);
+         }
+
+
+        /**
+         * Relacion con la entidad "tiempos"
+         * @return Active Record.
+         */
+        public function getTiempo()
+        {
+            return $this->hasOne(Tiempo::className(), ['id_tiempo' => 'id_tiempo']);
+        }
+
+
+        /**
+         * Relacion con la entidad "propagandas"
+         * @return [type] [description]
+         */
+        public function getPropaganda()
+        {
+            return $this->hasOne(Propaganda::className(), ['id_impuesto' => 'id_impuesto']);
+        }
+
+
+    }
+
+?>
---
+++ simwebplusteq/branches/v1.0/backend/models/propaganda/inscripcionpropaganda/InscripcionPropagandaForm.php
@@ -0,0 +1,365 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file InscripcionPropaganda.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 17-01-2017
+ *
+ *  @class InscripcionPropaganda
+ *  @brief Clase Modelo
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *  @inherits
+ *
+ */
+
+    namespace backend\models\propaganda\inscripcionpropaganda;
+
+    use Yii;
+    use yii\base\Model;
+    use yii\db\ActiveRecord;
+    use backend\models\propaganda\inscripcionpropaganda\InscripcionPropaganda;
+
+
+
+    /**
+    *  Clase
+    */
+    class InscripcionPropagandaForm extends InscripcionPropaganda
+    {
+
+    	public $id_impuesto;
+    	public $nro_solicitud;
+    	public $id_contribuyente;
+    	public $ano_impositivo;
+    	public $direccion;
+    	public $id_cp;
+    	public $clase_propaganda;
+    	public $tipo_propaganda;
+    	public $uso_propaganda;
+    	public $medio_difusion;
+    	public $medio_transporte;
+    	public $fecha_inicio;
+    	public $cantidad_tiempo;
+    	public $id_tiempo;
+    	public $inactivo;
+    	public $id_sim;
+    	public $cantidad_base;
+    	public $base_calculo;
+    	public $cigarros;
+    	public $bebidas_alcoholicas;
+    	public $cantidad_propagandas;
+    	public $planilla;
+    	public $idioma;
+    	public $observacion;
+    	public $fecha_fin;
+    	public $fecha_hora;
+    	public $usuario;
+    	public $user_funcionario;
+    	public $fecha_hora_proceso;
+    	public $estatus;
+    	public $alto;
+    	public $ancho;
+    	public $profundidad;
+    	public $nombre_propaganda;
+    	public $mts;
+    	public $costo;
+    	public $origen;
+    	public $errorMensajeInput;
+    	public $descripcion;
+
+    	const SCENARIO_FRONTEND = 'frontend';
+		const SCENARIO_BACKEND = 'backend';
+
+
+    	/***/
+    	public function scenarios()
+    	{
+        	// bypass scenarios() implementation in the parent class
+        	//return Model::scenarios();
+        	return [
+        		self::SCENARIO_FRONTEND => [
+        					'id_contribuyente',
+        					'id_impuesto',
+        					'nro_solicitud',
+        					'ano_impositivo',
+        					'direccion',
+        					'id_cp',
+        					'clase_propaganda',
+        					'tipo_propaganda',
+        					'uso_propaganda',
+        					'medio_difusion',
+        					'medio_transporte',
+        					'cantidad_tiempo',
+        					'id_tiempo',
+        					'inactivo',
+        					'id_sim',
+        					'cantidad_base',
+        					'base_calculo',
+        					'cigarros',
+        					'bebidas_alcoholicas',
+        					'cantidad_propagandas',
+        					'planilla',
+        					'idioma',
+        					'observacion',
+        					'fecha_fin',
+        					'fecha_inicio',
+        					'fecha_hora',
+        					'usuario',
+        					'user_funcionario',
+        					'fecha_hora_proceso',
+        					'estatus',
+        					'alto',
+        					'ancho',
+        					'profundidad',
+        					'nombre_propaganda',
+        					'mts',
+        					'costo',
+        					'origen',
+        					'descripcion',
+
+        		],
+        		self::SCENARIO_BACKEND => [
+        					'id_contribuyente',
+        					'id_impuesto',
+        					'nro_solicitud',
+        					'ano_impositivo',
+        					'direccion',
+        					'id_cp',
+        					'clase_propaganda',
+        					'tipo_propaganda',
+        					'uso_propaganda',
+        					'medio_difusion',
+        					'medio_transporte',
+        					'cantidad_tiempo',
+        					'id_tiempo',
+        					'inactivo',
+        					'id_sim',
+        					'cantidad_base',
+        					'base_calculo',
+        					'cigarros',
+        					'bebidas_alcoholicas',
+        					'cantidad_propagandas',
+        					'planilla',
+        					'idioma',
+        					'observacion',
+        					'fecha_fin',
+        					'fecha_inicio',
+        					'fecha_hora',
+        					'usuario',
+        					'user_funcionario',
+        					'fecha_hora_proceso',
+        					'estatus',
+        					'alto',
+        					'ancho',
+        					'profundidad',
+        					'nombre_propaganda',
+        					'mts',
+        					'costo',
+        					'origen',
+        					'descripcion',
+        		]
+        	];
+    	}
+
+
+
+		/**
+    	 *	Metodo que permite fijar la reglas de validacion del formulario.
+    	 */
+	    public function rules()
+	    {
+	        return [
+	        	[['id_contribuyente', 'nombre_propaganda',
+	        	  'direccion',
+	        	  'clase_propaganda', 'uso_propaganda',
+	        	  'tipo_propaganda', 'id_tiempo', 'fecha_inicio',
+	        	  'fecha_fin',
+	        	  'cantidad_tiempo', 'base_calculo',
+	        	  'cantidad_propagandas'],
+	        	  'required'],
+	        	[['id_impuesto', 'id_contribuyente', 'nro_solicitud',
+	        	  'ano_impositivo', 'clase_propaganda', 'uso_propaganda',
+	        	  'tipo_propaganda', 'id_tiempo',
+	        	  'base_calculo',
+	        	  'estatus', 'inactivo', 'idioma',
+	        	  'bebidas_alcoholicas', 'planilla', 'medio_transporte',
+	        	  'medio_difusion', 'id_cp', 'cigarros',],
+	        	  'integer',
+	        	  'message' => Yii::t('backend', '{attribute} no valido')],
+	        	[['cantidad_tiempo',],
+	        	   'number',
+	        	   'message' => Yii::t('backend', '{attribute} no valido')],
+	        	[['cantidad_propagandas'],
+	        	   'number',
+	        	   'message' => Yii::t('backend', '{attribute} no valido')],
+	        	[['observacion', 'direccion',
+	        	  'descripcion', 'nombre_propaganda', 'origen',],
+	        	  'string',
+	        	  'message' => Yii::t('backend', '{attribute} no valido')],
+	        	[['alto', 'ancho', 'profundidad',
+	        	  'mts', 'costo'],
+	        	  'double',
+	        	  'message' => Yii::t('backend', '{attribute} no valido')],
+	        	[['estatus', 'inactivo', 'bebidas_alcoholicas',
+	        	  'idioma', 'id_cp', 'medio_difusion', 'medio_transporte',
+	        	  'id_sim', 'cigarros', 'planilla', 'id_cp',
+	        	  'cantidad_base', 'alto', 'ancho', 'profundidad',
+	        	  'mts', 'costo', 'id_impuesto',],
+	        	  'default',
+	        	  'value' => 0],
+	        	[['ano_impositivo',],
+	        	  'default',
+	        	  'value' => date('Y')],
+	        	[['fecha_desde', 'fecha_fin'],
+	        	  'date',
+	        	  'format' => 'dd-MM-yyyy',
+	        	  'message' => Yii::t('backend','formatted date no valid')],
+	     		['usuario', 'default', 'value' => Yii::$app->identidad->getUsuario()],
+	     		['nombre_propaganda', 'filter', 'filter' => 'strtoupper'],
+	     		[['fecha_hora', 'fecha_guardado'],
+	     		  'default',
+	     		  'value' => date('Y-m-d H:i:s')],
+	     		[['fecha_guardado'],
+	     		  'default',
+	     		  'value' => date('Y-m-d')],
+	     		['nombre_propaganda', 'filter', 'filter' => 'strtoupper'],
+	        ];
+	    }
+
+
+
+	    /**
+	     * Metodo que retorna un arreglo de atributos que seran actualizados
+	     * al momento de procesar la solicitud (aprobar o negar). Estos atributos
+	     * afectaran a la entidad respectiva de la clase.
+	     * @param String $evento, define la accion a realizar sobre la solicitud.
+	     * - Aprobar.
+	     * - Negar.
+	     * @return Array Retorna un arreglo de atributos segun el evento.
+	     */
+	    public function atributosUpDateProcesarSolicitud($evento)
+	    {
+	    	$atributos = [
+	    		Yii::$app->solicitud->aprobar() => [
+	    						'estatus' => 1,
+	    						'fecha_hora_proceso' => date('Y-m-d H:i:s'),
+	    						'user_funcionario' => Yii::$app->identidad->getUsuario(),
+
+	    		],
+	    		Yii::$app->solicitud->negar() => [
+	    						'estatus' => 9,
+	    						'fecha_hora_proceso' => date('Y-m-d H:i:s'),
+	    						'user_funcionario' => Yii::$app->identidad->getUsuario(),
+
+	    		],
+	    	];
+
+	    	return $atributos[$evento];
+	    }
+
+
+
+	    /***/
+	    public function validateInputBaseCalculo()
+	    {
+	    	$result = false;
+	    	if ( $this->base_calculo == 2 ) {
+	    		if ( $this->alto > 0 && $this->ancho > 0 ) {
+	    			$result = true;
+
+	    		} else {
+	    			$this->errorMensajeInput = Yii::t('backend', 'Debe registrar alto y ancho');
+	    		}
+
+	    	} elseif ( $this->base_calculo == 3 ) {
+	    		if ( $this->mts > 0 ) {
+	    			$result = true;
+	    		} else {
+	    			$this->errorMensajeInput = Yii::t('backend', 'Debe registrar metros');
+	    		}
+
+	    	} elseif ( $this->base_calculo == 7 ) {
+	    		if ( $this->costo > 0 ) {
+	    			$result = true;
+	    		} else {
+	    			$this->errorMensajeInput = Yii::t('backend', 'Debe registrar costo');
+	    		}
+
+	    	} else {
+	    		$result = true;
+	    	}
+
+	    	return $result;
+	    }
+
+
+
+	    /**
+    	*   Metodo attributeLabels(), retorna las etiquetas de los campos.
+    	*/
+    	public function attributeLabels()
+	    {
+	        return  [
+                'id_impuesto' => Yii::t( 'backend', 'Tax Id' ),
+                'id_contribuyente' => Yii::t( 'backend', 'Id Contribuyente' ),
+                'ano_impositivo' => Yii::t( 'backend', 'Tax Year' ),
+                'direccion' => Yii::t( 'backend', 'Address' ),
+                'id_cp' => Yii::t( 'backend', 'Location' ),
+                'clase_propaganda' => Yii::t( 'backend', 'Clase Propaganda' ),
+                'tipo_propaganda' => Yii::t( 'backend', 'Kind' ),
+                'uso_propaganda' => Yii::t( 'backend', 'Use' ),
+                'medio_difusion' => Yii::t( 'backend', 'Through Construction' ),
+                'medio_transporte' => Yii::t( 'backend', 'Transport Means' ),
+                'fecha_desde' => Yii::t( 'backend', 'Start Date' ),
+                'cantidad_tiempo' => Yii::t( 'backend', 'Quantity' ),
+                'id_tiempo' => Yii::t( 'backend', 'Lapse' ),
+                'inactivo' => Yii::t( 'backend', 'Status' ),
+                'id_sim' => Yii::t( 'backend', 'Id Sim'),
+                'cantidad_base' => Yii::t( 'backend', 'Number' ),
+                'base_calculo' => Yii::t( 'backend', 'Base'),
+                'cigarros' => Yii::t( 'backend', 'Cigarettes or Tobacco' ),
+                'bebidas_alcoholicas' => Yii::t( 'backend', 'Alcoholic Beverages' ),
+                'cantidad_propagandas' => Yii::t( 'backend', 'Units' ),
+                'planilla' => Yii::t( 'backend', 'Planilla' ),
+                'idioma' => Yii::t( 'backend', 'Foreign Language' ),
+                'observacion' => Yii::t( 'backend', 'Observation' ),
+                'ano_impo' => Yii::t( 'backend', 'Tax Year' ),
+                'fecha_fin' => Yii::t( 'backend', 'Date End' ),
+                'fecha_inicio' => Yii::t( 'backend', 'Start Date' ),
+                'fecha_guardado' => Yii::t( 'backend', 'Date Creation' ),
+            ];
+	    }
+
+
+
+    }
+
+?>
