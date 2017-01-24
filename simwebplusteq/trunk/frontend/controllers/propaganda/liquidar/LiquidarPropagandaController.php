61eb9583bef304a9f057e69d8481329c276d8f95
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-24 01:06:10 +0000

    Actualizar

--- simwebplusteq/trunk/frontend/controllers/propaganda/liquidar/LiquidarPropagandaController.php
+++ simwebplusteq/trunk/frontend/controllers/propaganda/liquidar/LiquidarPropagandaController.php
@@ -144,7 +144,7 @@
 													'url' => $url,
 								]);
 				} else {
-					return $this->redirect(['error-operacion', 'cod' => 509]);
+					return $this->redirect(['error-operacion', 'cod' => 515]);
 
 				}
 
