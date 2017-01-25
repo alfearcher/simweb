f8db7c17061d665931e6db900b3a2d594009a942
alvarof <alvarof@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-25 14:53:41 +0000

    modificacion

--- simwebplusteq/trunk/frontend/controllers/usuario/CambiarPasswordContribuyenteController.php
+++ simwebplusteq/trunk/frontend/controllers/usuario/CambiarPasswordContribuyenteController.php
@@ -228,7 +228,7 @@ class CambiarPasswordContribuyenteController extends Controller
                                 }
                             
                                 
-                            }
+                            //}
                             
                     }
                         
