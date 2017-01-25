536051173504f9bb1dfe27f146663475296d3b7d
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-25 02:56:37 +0000

    Actualizar

--- simwebplusteq/trunk/frontend/views/inmueble/liquidar/lista-inmueble.php
+++ simwebplusteq/trunk/frontend/views/inmueble/liquidar/lista-inmueble.php
@@ -95,6 +95,11 @@
 								'headerRowOptions' => [
 									'class' => 'success',
 								],
+								'rowOptions' => function($model) {
+					            	if ( $model['bloquear'] == 1 ) {
+					                	return [ 'class' => 'warning'];
+					               	}
+					            },
 								'tableOptions' => [
                     				'class' => 'table table-hover table-bordered',
               					],
@@ -105,6 +110,13 @@
 				                        'class' => 'yii\grid\CheckboxColumn',
 				                        'name' => 'chkIdImpuesto',
 				                        'multiple' => false,
+				                        'checkboxOptions' => function ($model, $key, $index, $column) {
+				                        	if ( $model['bloquear'] == 1 ) {
+					                			return [
+					                				'disabled' => 'disabled',
+					                			];
+					               			}
+				                        }
 				                    ],
 				                    [
 				                        'contentOptions' => [
