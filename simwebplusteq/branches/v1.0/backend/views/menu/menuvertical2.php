2cb2c9d0b2cafbbc9ac6701e817bdd6b2cedac74
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-01-25 19:53:02 +0000

    Actualizar

--- simwebplusteq/branches/v1.0/backend/views/menu/menuvertical2.php
+++ simwebplusteq/branches/v1.0/backend/views/menu/menuvertical2.php
@@ -187,7 +187,7 @@
 		        			'items' => [
 		        						['label' => 'Inscripción de Propagandas', 'url' => ['propaganda/inscripcionpropaganda/inscripcion-propaganda/index', 'id' => 75]],
 		        						//['label' => 'Renovación de Propagandas', 'url' => '#'],
-		        						//['label' => 'Desincorporación de Propaganda', 'url' => ['propaganda/propaganda/desincorporacion']],
+		        						['label' => 'Desincorporación de Propaganda', 'url' => ['propaganda/propaganda/desincorporacion']],
 		        						//['label' => 'Cambio de Otros Datos de Propaganda', 'url' => ['propaganda/propaganda/index']],
 		        						//['label' => 'Solvencia', 'url' => '#'],
 		        			]
