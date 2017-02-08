b8295ec75ff136ca0d44865fd12bf550ec391b1a
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-02-08 17:38:36 +0000

    Actualizar

--- simwebplusteq/trunk/backend/views/menu/menuvertical2.php
+++ simwebplusteq/trunk/backend/views/menu/menuvertical2.php
@@ -66,11 +66,18 @@
 		        		['label' => 'Solicitudes de Declaracion', 'url' => ['/aaee/listado/listado-solicitud-declaracion/index']],
 		        	],
 		        ],
+
 		        ['label' => Icon::show('fa fa-th-large',['class' => $typeLong], $typeIcon) . '&nbsp; Atención de Solicitudes Especiales',
 		        	'items' => [
-		        		['label' => 'Licencias', 'url' => ['/solicitud/especial/aaee/licencia/procesar-licencia/index']],
+		        		['label' => Icon::show('fa fa-check-square', ['class' => $typeLong], $typeIcon) . '&nbsp; Licencias',
+		        			'items' => [
+		        				['label' => 'Asignar Numero', 'url' => ['/aaee/licencia/asignar-numero-licencia/index']],
+		        				['label' => 'Aprobar Solicitudes Masivas', 'url' => ['/solicitud/especial/aaee/licencia/procesar-licencia/index']],
+		        			],
+		        		],
 		        	],
 		        ],
+
 		    	// &nbsp; espacio en blanco html
 		        ['label' => Icon::show('fa fa-book',['class' => $typeLong], $typeIcon) . '&nbsp; Registros Maestros',
 		        	'items' => [
