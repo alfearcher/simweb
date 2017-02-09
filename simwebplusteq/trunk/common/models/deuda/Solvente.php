d48e595926ad10a026911556231dc871b1ee0d0b
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-02-09 23:27:46 +0000

    prueba

--- simwebplusteq/trunk/common/models/deuda/Solvente.php
+++ simwebplusteq/trunk/common/models/deuda/Solvente.php
@@ -226,7 +226,6 @@
 			// Lo siguiente retorna un arreglo.
 			$pagos = $search->getUltimoLapsoPagoActividadEconomica();
 
-die(var_dump($pagos));
 			if ( $pagos !== null ) {
 				$lapso['a'] = $pagos['ano_impositivo'];
 				$lapso['p'] = $pagos['trimestre'];
