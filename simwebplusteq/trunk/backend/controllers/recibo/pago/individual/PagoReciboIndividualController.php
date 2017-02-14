ac471e13c15475c2dd69d4485b3211bb225584e2
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-02-14 00:01:28 +0000

    Controlador para el proceso del pago de recibo individual

---
+++ simwebplusteq/trunk/backend/controllers/recibo/pago/individual/PagoReciboIndividualController.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ *	@copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *	> This library is free software; you can redistribute it and/or modify it under
+ *	> the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *	> Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *	> any later version.
+ *  >
+ *	> This library is distributed in the hope that it will be usefull,
+ *	> but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *	> or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *	> for more details.
+ *  >
+ *	> See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *	@file PagoReciboIndividualController.php
+ *
+ *	@author Jose Rafael Perez Teran
+ *
+ *	@date 12-02-2017
+ *
+ *  @class PagoReciboIndividualController
+ *	@brief Clase
+ *
+ *
+ *	@property
+ *
+ *
+ *	@method
+ *
+ *
+ *	@inherits
+ *
+ */
+
+
+ 	namespace backend\controllers\recibo\pago\individual;
+
+
+ 	use Yii;
+	use yii\filters\AccessControl;
+	use yii\web\Controller;
+	use yii\filters\VerbFilter;
+	use yii\widgets\ActiveForm;
+	use yii\web\Response;
+	use yii\helpers\Url;
+	use yii\web\NotFoundHttpException;
+	use common\conexion\ConexionController;
+	use backend\controllers\mensaje\MensajeController;
+    use backend\models\recibo\pago\individual\BusquedaReciboForm;
+    use backend\models\recibo\pago\individual\PagoReciboIndividualSearch;
+
+
+
+	session_start();		// Iniciando session
+
+	/**
+	 *
+	 */
+	class PagoReciboIndividualController extends Controller
+	{
+		public $layout = 'layout-main';				//	Layout principal del formulario
+
+		public $connLocal;
+		public $conexion;
+		public $transaccion;
+
+
+
+
+        /***/
+		public function actionIndex()
+		{
+            $this->redirect(['mostrar-form-consulta']);
+		}
+
+
+
+
+
+        /***/
+        public function actionMostrarFormConsulta()
+        {
+            $model = New BusquedaReciboForm();
+            if ( $model->usuarioAutorizado(Yii::$app->identidad->getUsuario()) ) {
+
+                $request = Yii::$app->request;
+                $postData = $request->post();
+
+                // Permite bloquear el boton para buscar las formas de pagos del recibo.
+                $bloquearFormaPago = true;
+
+                // Mensaje que muestra las validaciones a nivel de logica de negocio que
+                // no se pasa.
+                $mensajes = [];
+                $htmlMensaje = null;
+                $htmlDatosRecibo = null;
+
+          		$formName = $model->formName();
+
+                if ( $model->load($postData) && Yii::$app->request->isAjax ) {
+					Yii::$app->response->format = Response::FORMAT_JSON;
+					return ActiveForm::validate($model);
+				}
+
+				if ( $model->load($postData) ) {
+
+					if ( $model->validate() ) {
+
+						// Se verifica que el recibo cumpla las reglas de negocio establecidas.
+						$pagoReciboSearch = New PagoReciboIndividualSearch($model->recibo);
+						$mensajes = $pagoReciboSearch->validarEvento();
+
+						// Arreglo de los provider del recibo y el de las planillas.
+						$dataProviders = $pagoReciboSearch->getDataProviders();
+
+						$htmlMensaje = $this->renderPartial('/recibo/pago/individual/warnings',[
+															'mensajes' => $mensajes,
+											]);
+
+						$htmlDatosRecibo = $this->renderPartial('/recibo/pago/individual/datos-recibo',[
+															'dataProviderRecibo' => $dataProviders[0],
+															'dataProviderReciboPlanilla' => $dataProviders[1],
+											]);
+
+						if ( count($mensajes) == 0 ) {
+							$bloquearFormaPago = false;
+							$htmlMensaje = null;
+						} else {
+
+						}
+					}
+				}
+
+				$caption = Yii::t('backend', 'Pago de Recibo');
+				$subCaption = Yii::t('backend', 'Datos del Recibo');
+
+				// Mostrar formulario de busqueda del recibo.
+				return $this->render('/recibo/pago/individual/_find', [
+											'model'=> $model,
+											'htmlMensaje' => $htmlMensaje,
+											'bloquearFormaPago' => $bloquearFormaPago,
+											'caption' => $caption,
+											'subCaption' => $subCaption,
+											'htmlDatosRecibo' => $htmlDatosRecibo,
+						]);
+
+            } else {
+                // Usuario no autorizado.
+            }
+        }
+
+
+
+
+
+
+	}
+?>
