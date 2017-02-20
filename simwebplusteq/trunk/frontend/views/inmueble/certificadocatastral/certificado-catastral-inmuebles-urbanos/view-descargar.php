6f88b3bb949e32d79a86831d7d8a7446824b513e
alvarof <alvarof@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-02-20 20:44:12 +0000

    mo

--- simwebplusteq/trunk/frontend/views/inmueble/certificadocatastral/certificado-catastral-inmuebles-urbanos/view-descargar.php
+++ simwebplusteq/trunk/frontend/views/inmueble/certificadocatastral/certificado-catastral-inmuebles-urbanos/view-descargar.php
@@ -1,100 +1,259 @@
 <?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
 
-use yii\helpers\Html;
-use yii\widgets\DetailView;
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
 
-/* @var $this yii\web\View */
-/* @var $model backend\models\Inmuebles */
+ /**
+ *  @file pre-view-datos-licencia.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 14-02-2017
+ *
+ *  @view pre-view-datos-licencia.php
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *
+ *  @inherits
+ *
+ */
 
-$this->title = Yii::t('backend', 'View Property Urban');
+    use yii\web\Response;
+    use kartik\icons\Icon;
+    use yii\grid\GridView;
+    use yii\helpers\Html;
+    use yii\helpers\Url;
+    use yii\helpers\ArrayHelper;
+    use yii\data\ArrayDataProvider;
+    use yii\widgets\ActiveForm;
+    use yii\web\View;
+    use yii\widgets\DetailView;
+    use yii\helpers\BaseHtml;
 
-?>
-<div class="inmuebles-view">
+    $typeIcon = Icon::FA;
+    $typeLong = 'fa-2x';
 
-    
+    Icon::map($this, $typeIcon);
 
-    <p>
-        <?= Html::a(Yii::t('backend', 'Nuevo Certificado catastral'), ['inmueble/certificadocatastral/certificado-catastral-inmuebles-urbanos/nuevo-certificado-catastral-inmuebles'], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('backend', 'Renovacion del Certificado catastral'), ['inmueble/certificadocatastral/certificado-catastral-inmuebles-urbanos/renovacion-certificado-catastral-inmuebles'], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('backend', 'Volver al Menu Principal'), ['/site/menu-vertical'], ['class' => 'btn btn-danger']) ?>
-    </p> 
-<div class="panel panel-primary">
-<div class="panel-heading">
-                <?= Yii::t('frontend', 'Datos del Inmueble') ?>  
-</div>
-    <?= DetailView::widget([
-        'model' => $modelInmueble,
-        'attributes' => [ 
-            'id_impuesto',
-            'id_contribuyente',
-            'catastro',
-            'id_habitante',
-            'ano_inicio',
-            'direccion',
-            //'liquidado',
-            'manzana_limite',
-            //'lote_1',
-            //'lote_2',
-            //'nivel',
-            //'lote_3',
-            'av_calle_esq_dom',
-            'casa_edf_qta_dom',
-            'piso_nivel_no_dom',
-            'apto_dom',
-            'tlf_hab',
-            'medidor',
-            'id_sim',
-            'observacion:ntext',
-            'inactivo',
-            'tipo_ejido',
-            'propiedad_horizontal',
-            'estado_catastro',
-            'municipio_catastro',
-            'parroquia_catastro',
-            'ambito_catastro',
-            'sector_catastro',
-            'manzana_catastro',
-            'parcela_catastro',
-            'subparcela_catastro',
-            'nivel_catastro',
-            'unidad_catastro',
-        ], 
-    ]) ?>
-</div>
-
-<div class="panel panel-primary">
-<div class="panel-heading">
-                <?= Yii::t('frontend', 'Datos del Avaluos') ?>  
-</div>
-    <?= DetailView::widget([
-        'model' => $modelHAvaluos,
-        'attributes' => [ 
-             'id_historico_avaluo',
-             'id_impuesto',
-             'fecha',
-             'mts',
-             'valor_por_mts2',
-             'mts2_terreno',
-             'valor_por_mts2_terreno',
-             'valor',
-             'inactivo',
-             'lindero_norte',
-             'lindero_sur',
-             'lindero_este',
-             'lindero_oeste',
-            
-        ], 
-    ]) ?>
-</div>
+ ?>
 
 
-</div>
+<div class="pre-view-datos-licencia">
+    <?php
+
+        $form = ActiveForm::begin([
+            'id' => 'id-pre-view-certificado',
+            'method' => 'post',
+            //'action' => $url,
+            'enableClientValidation' => true,
+            'enableAjaxValidation' => false,
+            'enableClientScript' => true,
+        ]);
+    ?>
 
 
-<!-- <? /*= Html::a(Yii::t('backend', 'Delete'), ['delete', 'id' => $model->id_impuesto], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('backend', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ], 
-        ]) */ ?> -->
+    <meta http-equiv="refresh">
+    <div class="panel panel-primary" style="width: 100%;">
+        <div class="panel-heading">
+            <h3><?= Html::encode(Yii::t('frontend', 'Datos preliminares del Certificado Catastral. Información de la solicitud: ' . $modelCertificado['nro_solicitud'])) ?></h3>
+        </div>
+
+<!-- Cuerpo del formulario -->
+<!-- style="background-color: #F9F9F9; -->
+        <div class="panel-body" >
+            <div class="container-fluid">
+                <div class="col-sm-12">
+
+                    <div class="row" style="padding: 0px;width:100%;">
+                    </div>
+
+<!-- INFORMACION DEL CONTRIBUYENTE -->
+
+                    <div class="row" class="informacion-contribuyente" id="informacion-contribuyente">
+                        <div class="row" style="border-bottom: 1px solid #ccc;background-color:#F1F1F1;padding: 0px;width: 100%;padding-left: 15px;">
+                            <h4><strong><?=Html::encode(Yii::t('frontend', 'Informacion del Contribuyente'))?></strong></h4>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-2" style="width: 20%;padding: 0px; padding-top: 10px;">
+                                <div class="rif" style="margin-left: 0px;">
+                                    <?= $form->field($model, 'id_rif')->textInput([
+                                                                                'id' => 'id-rif',
+                                                                                'style' => 'width:100%;background-color:white;',
+                                                                                'value' => $modelContribuyente['naturaleza']. '-'. $modelContribuyente['cedula'] . '-' . $modelContribuyente['tipo'],
+                                                                                'readOnly' => true,
+
+                                                                        ])->label('RIF') ?>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-2" style="width: 55%;padding: 0px; padding-top: 10px;padding-left: 5px;">
+                                <div class="razon-social" style="margin-left: 0px;">
+                                    <?= $form->field($model, 'razon_social')->textInput([
+                                                                                'id' => 'id-razon-social',
+                                                                                'style' => 'width:100%;background-color:white;', 
+                                                                                'value' => $descripcion,
+                                                                                'readOnly' => true,
+
+                                                                        ])->label('Razon Social / Nombres') ?>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-2" style="width: 20%;padding: 0px; padding-top: 10px;padding-left: 5px;">
+                                <div class="capital" style="margin-left: 0px;">
+                                    <?= $form->field($model, 'id_inmueble')->textInput([
+                                                                                'id' => 'id-capital',
+                                                                                'style' => 'width:100%;
+                                                                                            background-color:white;
+                                                                                            text-align:right;',
+                                                                                'value' => $modelInmueble['id_impuesto'],
+                                                                                'readOnly' => true,
+
+                                                                        ])->label('Id Inmueble') ?>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-2" style="width: 75%;padding: 0px;">
+                                <div class="domicilio" style="margin-left: 0px;">
+                                    <?= $form->field($model, 'direccion')->textInput([
+                                                                                'id' => 'id-rif',
+                                                                                'style' => 'width:100%;background-color:white;',
+                                                                                'value' => $modelInmueble['direccion'],
+                                                                                'readOnly' => true,
+
+                                                                        ])->label('Domicilio Fiscal') ?>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="row">
+                            <div class="col-sm-2" style="width: 20%;padding: 0px;">
+                                <div class="rif" style="margin-left: 0px;">
+                                    <?= $form->field($model, 'catastro')->textInput([
+                                                                                'id' => 'id-catastro',
+                                                                                'style' => 'width:100%;background-color:white;',
+                                                                                'value' => $modelInmueble['catastro'],
+                                                                                'readOnly' => true,
+
+                                                                        ])->label('Catastro') ?>
+                                </div>
+                            </div>
+
+                            
+                        </div>
+                    </div>
+
+<!-- INFORMACION Y VIGENCIA DE LA LICENCIA -->
+
+                    <div class="row" class="informacion-licencia" id="informacion-licencia">
+                        <div class="row" style="border-bottom: 1px solid #ccc;background-color:#F1F1F1;padding: 0px;width: 100%;padding-left: 15px;">
+                            <h4><strong><?=Html::encode(Yii::t('frontend', 'Identificacion y Vigencia del Certificado Catastral'))?></strong></h4>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-2" style="width: 20%;padding: 0px;padding-top: 10px;">
+                                <div class="id-contribuyente" style="margin-left: 0px;">
+                                    <?= $form->field($model, 'id_contribuyente')->textInput([
+                                                                                'id' => 'id-contribuyente',
+                                                                                'style' => 'width:100%;background-color:white;',
+                                                                                'value' => $modelContribuyente['id_contribuyente'],
+                                                                                'readOnly' => true,
+
+                                                                        ])->label('ID Contribuyente') ?>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-2" style="width: 20%;padding: 0px;padding-left: 5px;padding-top: 10px;">
+                                <div class="licencia" style="margin-left: 0px;">
+                                    <?= $form->field($model, 'certificado_catastral')->textInput([
+                                                                                'id' => 'id-licencia',
+                                                                                'style' => 'width:100%;background-color:white;',
+                                                                                'value' => $modelCertificado['certificado_catastral'],
+                                                                                'readOnly' => true,
+
+                                                                        ])->label('Nro. Certificado Catastral') ?>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-2" style="width: 15%;padding: 0px;padding-left: 5px;padding-top: 10px;">
+                                <div class="fecha-emision" style="margin-left: 0px;">
+                                    <?= $form->field($model, 'fecha_hora')->textInput([
+                                                                                'id' => 'id-fecha_emision',
+                                                                                'style' => 'width:100%;background-color:white;',
+                                                                                'value' => $modelCertificado['fecha_hora'],
+                                                                                'readOnly' => true,
+
+                                                                        ])->label('Fecha Emisión') ?>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3" style="width: 15%;padding: 0px;padding-left: 5px;padding-top: 10px;">
+                                <div class="fecha-vcto" style="margin-left: 0px;">
+                                    <?= $form->field($model, 'ano_inicio')->textInput([
+                                                                                'id' => 'id-fecha_vcto',
+                                                                                'style' => 'width:100%;background-color:white;',
+                                                                                'value' => isset($modelInmueble['ano_inicio']),
+                                                                                'readOnly' => true,
+
+                                                                        ])->label('Fecha Inicio del inmueble') ?>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+<!-- INFORMACION DE LOS RUBROS AUTORIZADOS -->
+
+                    <div class="row" class="informacion-botones-licencia" id="informacion-botones-licencia">
+                            <div class="col-sm-5" style="width: 15%;padding: 0px;padding-left: 5px;padding-top: 10px;">
+                                <div class="fecha-vcto" style="margin-left: 0px;">
+                                    <?= Html::a(Yii::t('backend', 'Descargar Certificado catastral'), ['inmueble/certificadocatastral/certificado-catastral-inmuebles-urbanos/descargar-certificado-catastral'], ['class' => 'btn btn-success','target'=> '_blank'])//'target'=> '_blank' ?>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-5" style="width: 15%;padding: 0px;padding-left: 5px;padding-top: 10px;">
+                                <div class="fecha-vcto" style="margin-left: 70px;">
+                                    <?= Html::a(Yii::t('backend', 'Salir'), ['/site/menu-vertical'], ['class' => 'btn btn-danger']) ?>
+                                </div>
+                            </div>
+                    </div>
+
+
+                    <div class="row" style="border-bottom: 2px solid #ccc;padding: 0px;width: 103%;margin-left: -30px;">
+                    </div>
+
+                </div>  <!-- Fin de col-sm-12 -->
+            </div>      <!-- Fin de container-fluid -->
+
+        </div>          <!-- Fin panel-body-->
+
+    </div>  <!-- Fin de panel panel-primary -->
+
+    <?php ActiveForm::end(); ?>
+</div>   <!-- Fin de inscripcion-act-econ-form -->
+
