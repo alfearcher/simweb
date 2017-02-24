e6b592f7045863e6ca5903bd223e921a083999bf
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-02-24 00:22:35 +0000

    Actualizar

--- simwebplusteq/trunk/backend/controllers/recibo/pago/individual/PagoReciboIndividualController.php
+++ simwebplusteq/trunk/backend/controllers/recibo/pago/individual/PagoReciboIndividualController.php
@@ -54,7 +54,9 @@
 	use yii\helpers\Url;
 	use yii\web\NotFoundHttpException;
 	use common\conexion\ConexionController;
-	use backend\controllers\mensaje\MensajeController;
+	// use backend\controllers\mensaje\MensajeController;
+	use common\mensaje\MensajeController;
+	use common\models\session\Session;
     use backend\models\recibo\pago\individual\BusquedaReciboForm;
     use backend\models\recibo\pago\individual\PagoReciboIndividualSearch;
 
@@ -110,6 +112,18 @@
 					Yii::$app->response->format = Response::FORMAT_JSON;
 					return ActiveForm::validate($model);
 				}
+// die(var_dump($postData));
+				if ( isset($postData['btn-back']) ) {
+					if ( $postData['btn-back'] == 1 ) {
+						$this->redirect(['index']);
+					}
+				}
+
+				if ( isset($postData['btn-quit']) ) {
+					if ( $postData['btn-quit'] == 1 ) {
+						$this->redirect(['quit']);
+					}
+				}
 
 				if ( $model->load($postData) ) {
 
@@ -119,27 +133,35 @@
 						$pagoReciboSearch = New PagoReciboIndividualSearch($model->recibo);
 						$mensajes = $pagoReciboSearch->validarEvento();
 
+						if ( count($mensajes) == 0 ) {
+							$bloquearFormaPago = false;
+							$htmlMensaje = null;
+						} else {
+							$htmlMensaje = $this->renderPartial('/recibo/pago/individual/warnings',[
+																	'mensajes' => $mensajes,
+											]);
+						}
+
 						// Arreglo de los provider del recibo y el de las planillas.
 						$dataProviders = $pagoReciboSearch->getDataProviders();
 
 						$totales = $pagoReciboSearch->getTotalesReciboPlanilla($dataProviders);
 
-						$htmlMensaje = $this->renderPartial('/recibo/pago/individual/warnings',[
-															'mensajes' => $mensajes,
+						$htmlRecibo = $this->renderPartial('/recibo/pago/individual/_recibo-encontrado',[
+																'dataProviderRecibo' => $dataProviders[0],
+																'dataProviderReciboPlanilla' => $dataProviders[1],
+																'totales' => $totales,
+																'htmlMensaje' => $htmlMensaje,
 											]);
 
-						$htmlDatosRecibo = $this->renderPartial('/recibo/pago/individual/datos-recibo',[
-															'dataProviderRecibo' => $dataProviders[0],
-															'dataProviderReciboPlanilla' => $dataProviders[1],
-															'totales' => $totales,
-											]);
-
-						if ( count($mensajes) == 0 ) {
-							$bloquearFormaPago = false;
-							$htmlMensaje = null;
-						} else {
-
-						}
+						$caption = Yii::t('backend', 'Pago de Recibo');
+						$subCaption = Yii::t('backend', 'Datos del Recibo');
+						return $this->render('/recibo/pago/individual/recibo', [
+													'htmlRecibo' => $htmlRecibo,
+													'caption' => $caption,
+													'subCaption' => $subCaption,
+													'bloquearFormaPago' => $bloquearFormaPago,
+								]);
 					}
 				}
 
@@ -149,17 +171,91 @@
 				// Mostrar formulario de busqueda del recibo.
 				return $this->render('/recibo/pago/individual/_find', [
 											'model'=> $model,
-											'htmlMensaje' => $htmlMensaje,
-											'bloquearFormaPago' => $bloquearFormaPago,
 											'caption' => $caption,
 											'subCaption' => $subCaption,
-											'htmlDatosRecibo' => $htmlDatosRecibo,
 						]);
 
             } else {
                 // Usuario no autorizado.
             }
         }
+
+
+
+        /**
+		 * Metodo salida del modulo.
+		 * @return view
+		 */
+		public function actionQuit()
+		{
+			$varSession = self::actionGetListaSessions();
+			self::actionAnularSession($varSession);
+			return $this->render('/menu/menuvertical2');
+		}
+
+
+
+		/**
+		 * Metodo que ejecuta la anulacion de las variables de session utilizados
+		 * en el modulo.
+		 * @param  array $varSessions arreglo con los nombres de las variables de
+		 * sesion que seran anuladas.
+		 * @return none.
+		 */
+		public function actionAnularSession($varSessions)
+		{
+			Session::actionDeleteSession($varSessions);
+		}
+
+
+
+		/**
+		 * Metodo que renderiza una vista indicando que le proceso se ejecuto
+		 * satisfactoriamente.
+		 * @param  integer $cod codigo que permite obtener la descripcion del
+		 * codigo de la operacion.
+		 * @return view.
+		 */
+		public function actionProcesoExitoso($cod)
+		{
+			$varSession = self::actionGetListaSessions();
+			self::actionAnularSession($varSession);
+			return MensajeController::actionMensaje($cod);
+		}
+
+
+
+		/**
+		 * Metodo que renderiza una vista que indica que ocurrio un error en la
+		 * ejecucion del proceso.
+		 * @param  integer $cod codigo que permite obtener la descripcion del
+		 * codigo de la operacion.
+		 * @return view.
+		 */
+		public function actionErrorOperacion($cod)
+		{
+			$varSession = self::actionGetListaSessions();
+			self::actionAnularSession($varSession);
+			return MensajeController::actionMensaje($cod);
+		}
+
+
+
+		/**
+		 * Metodo que permite obtener un arreglo de las variables de sesion
+		 * que seran utilizadas en el modulo, aqui se pueden agregar o quitar
+		 * los nombres de las variables de sesion.
+		 * @return array retorna un arreglo de nombres.
+		 */
+		public function actionGetListaSessions()
+		{
+			return $varSession = [
+							'postData',
+							'conf',
+							'begin',
+					];
+
+		}
 
 
 
