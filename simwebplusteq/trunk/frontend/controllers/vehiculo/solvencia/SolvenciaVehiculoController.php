ba54922c5ec0ead7ec3f6fd4b13823bb9ae73dfb
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-03-16 17:23:40 +0000

    Actualizar

--- simwebplusteq/trunk/frontend/controllers/vehiculo/solvencia/SolvenciaVehiculoController.php
+++ simwebplusteq/trunk/frontend/controllers/vehiculo/solvencia/SolvenciaVehiculoController.php
@@ -91,7 +91,7 @@
 		 * Identificador de  configuracion de la solicitud. Se crea cuando se
 		 * configura la solicitud que gestiona esta clase.
 		 */
-		const CONFIG = 115;
+		const CONFIG = 120;
 
 
 		/**
