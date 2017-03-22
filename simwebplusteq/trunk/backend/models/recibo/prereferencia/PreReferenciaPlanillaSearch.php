c24429ca6b90174bacc1b58fbaf58a4fba3d907e
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-03-22 17:43:35 +0000

    Modulo para las pre referencia de las planillas

---
+++ simwebplusteq/trunk/backend/models/recibo/prereferencia/PreReferenciaPlanillaSearch.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file PreReferenciaPlanillaSearch.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 22-03-2017
+ *
+ *  @class PreReferenciaPlanillaSearch
+ *  @brief Clase Modelo
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *  @inherits
+ *
+ */
+
+	namespace backend\models\recibo\prereferencia;
+
+ 	use Yii;
+	use backend\models\recibo\prereferencia\PreReferenciaPlanilla;
+	use backend\models\recibo\deposito\Deposito;
+	use common\models\planilla\Pago;
+
+
+	/**
+	* 	Clase
+	*/
+	class PreReferenciaPlanillaSearch
+	{
+
+		protected $planilla;
+		protected $recibo;
+
+
+		/***/
+		public function __construct()
+		{}
+
+
+
+	}
+
+?>
