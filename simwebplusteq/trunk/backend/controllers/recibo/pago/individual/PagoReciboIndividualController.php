e3da430d53b0a929d6587c49957c43d1544ac897
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-03-22 20:11:01 +0000

    Prueaba

--- simwebplusteq/trunk/backend/controllers/recibo/pago/individual/PagoReciboIndividualController.php
+++ simwebplusteq/trunk/backend/controllers/recibo/pago/individual/PagoReciboIndividualController.php
@@ -948,7 +948,6 @@
         private function actionBeginSaveFormaPagoTemp($model)
         {
         	$d = $model->attributes;
-die(var_dump($d));
         	$tabla = $model->tableName();
 
         	return $result = $this->_conexion->guardarRegistro($this->_conn, $tabla, $model->attributes);
