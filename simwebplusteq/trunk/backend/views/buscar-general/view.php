df5cd96e97130d857297671754a0a8f5e699f307
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-03-25 01:33:00 +0000

    Actualizar

--- simwebplusteq/trunk/backend/views/buscar-general/view.php
+++ simwebplusteq/trunk/backend/views/buscar-general/view.php
@@ -68,6 +68,16 @@ $this->title = Yii::t('backend','Id. Taxpayer') . ': ' . $model->id_contribuyent
                     </div>
                 </div>
 
+
+<!-- CONDICION -->
+                <div class="panel-body" style="margin-left: 160px; padding-top: 0px;">
+                    <div class="row">
+                        <div class="col-md-1" style="width: 150px"><b><?= Yii::t('backend', 'Condicion') . ':'; ?></b></div>
+                        <div class="col-md-1"><?= ( $model->inactivo == 0 ) ? 'ACTIVO' : 'INACTIVO'; ?></div>
+                    </div>
+                </div>
+
+
 <!-- USUARIO -->
                 <div class="panel-body" style="margin-left: 160px; padding-top: 0px;">
                     <div class="row">
