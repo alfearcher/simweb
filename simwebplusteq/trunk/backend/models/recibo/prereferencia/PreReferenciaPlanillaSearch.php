2639c186ee32fa6729d0a7e0e9ee286b69effdce
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-04-11 01:13:45 +0000

    Actualizar

--- simwebplusteq/trunk/backend/models/recibo/prereferencia/PreReferenciaPlanillaSearch.php
+++ simwebplusteq/trunk/backend/models/recibo/prereferencia/PreReferenciaPlanillaSearch.php
@@ -44,36 +44,133 @@
 
  	use Yii;
 	use backend\models\recibo\prereferencia\PreReferenciaPlanilla;
-	use backend\models\recibo\deposito\Deposito;
-	use common\models\planilla\Pago;
+	use backend\models\recibo\pago\individual\SerialReferenciaUsuario;
 	use common\models\referencia\GenerarReferenciaBancaria;
 
 
 
 
+
 	/**
-	* 	Clase
+	* Clase
 	*/
 	class PreReferenciaPlanillaSearch extends PreReferenciaPlanilla
 	{
 
-		private $planilla;
-		private $recibo;
+		private $_modelSerial;
+		private $_recibo;
 		private $_conn;
 		private $_conexion;
 
 		private $_errores;
 
 
-		/***/
-		public function __construct($recibo, $conexion, $conn)
+		/**
+		 * Metodo constructor de la clase
+		 * @param integer $recibo numero de recibo de pago.
+		 * @param ConexionController $conexion instancia de la clase.
+		 * @param connection $conn
+		 * @param SerialReferencia $modelSerial
+		 */
+		public function __construct($recibo, $conexion, $conn, $modelSerial = null)
 		{
 			$this->_recibo = $recibo;
 			$this->_conexion = $conexion;
 			$this->_conn = $conn;
+			$this->_modelSerial = $modelSerial;
+
 		}
 
 
+
+
+		/**
+		 * Metodo que inicia el proceso de guardado de las referencias
+		 * @return boolean.
+		 */
+		public function iniciarPreReferencia()
+		{
+			if ( isset($this->_conexion) && isset($this->_conn) ) {
+				if ( self::suprimirByRecibo() ) {
+					return self::guardar();
+				}
+			}
+			return false;
+		}
+
+
+
+
+		/**
+		 * Metodo
+		 * @return boolean.
+		 */
+		public function guardar()
+		{
+			$result = false;
+			$tabla = $this->tableName();
+			$referencia = self::generarReferenciaBancaria();
+			if ( count($referencia) > 0 ) {
+				foreach ( $referencia as $item ) {
+					$result = $this->_conexion->guardarRegistro($this->_conn, $tabla, $item);
+					if ( !$result ) { break; }
+				}
+			}
+			return $result;
+		}
+
+
+
+		/**
+		 * Metodo que suprime
+		 * @param array $arregloCondicion arreglo que contiene el wher condition.
+		 * @return boolean.
+		 */
+		private function suprimir($arregloCondicion)
+		{
+			$result = false;
+			if ( count($arregloCondicion) > 0 ) {
+				$tabla = $this->tableName();
+				return $this->_conexion->eliminarRegistro($this->_conn, $tabla, $arregloCondicion);
+			}
+			return $result;
+		}
+
+
+
+
+		/**
+		 * Metodo que suprime los registros de la entidad "planillas-conatbles".
+		 * @return boolean.
+		 */
+		private function suprimirByRecibo()
+		{
+			$result = false;
+			if ( $this->_recibo > 0 ) {
+				$arregloCondicion = [
+					'recibo' => $this->_recibo,
+				];
+				$result = self::suprimir($arregloCondicion);
+			}
+			return $result;
+		}
+
+
+
+		/**
+		 * Metodo que genera el arreglo con los datos que se guardaran por concepto
+		 * de distribucion presupuestario de ingresos municipales.
+		 * @return array
+		 */
+		private function generarReferenciaBancaria()
+		{
+			$generar = New GenerarReferenciaBancaria($this->_recibo, $this->_modelSerial, '');
+			$referencia = $generar->iniciarReferencia();
+			if ( count($generar->getError()) > 0 ) {
+				$referencia = [];
+			}
+			return $referencia;
+		}
 
 	}
 
