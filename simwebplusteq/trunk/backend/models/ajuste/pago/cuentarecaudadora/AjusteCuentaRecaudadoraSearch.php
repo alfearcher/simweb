2545e088218f265470fa5fb0c95830454807c6ba
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-05-11 02:04:43 +0000

    Modulo de ajuste. Modelo

---
+++ simwebplusteq/trunk/backend/models/ajuste/pago/cuentarecaudadora/AjusteCuentaRecaudadoraSearch.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file AjusteCuentaRecaudadoraSearch.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 09-05-2017
+ *
+ *  @class AjusteCuentaRecaudadoraSearch
+ *  @brief Clase Modelo
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *  @inherits
+ *
+ */
+
+	namespace backend\models\ajuste\pago\cuentarecaudadora;
+
+ 	use Yii;
+	use yii\base\Model;
+	use backend\models\recibo\deposito\Deposito;
+	use common\models\contribuyente\ContribuyenteBase;
+	use yii\data\ArrayDataProvider;
+	use yii\data\ActiveDataProvider;
+	use backend\models\recibo\depositodetalle\DepositoDetalle;
+
+
+	/**
+	* Clase
+	*/
+	class AjusteCuentaRecaudadoraSearch extends Model
+	{
+
+
+
+
+		/**
+		 * Metodo que permite generar el modelo de consulta principal sobre las entidades
+		 * "depositos" y "depositos-detalle".
+		 * @return DepositoDetalle
+		 */
+		public function findDepositoDetalleModel()
+		{
+			$findModel = DepositoDetalle::find()->alias('B')
+												->joinWith('deposito A', true, 'INNER JOIN')
+												->where('A.estatus =:estatus',
+															[':estatus' => 1]);
+
+			return $findModel;
+		}
+
+
+
+		/***/
+		public function searchDepositoDetalle($params)
+		{
+			$query = self::findDepositoDetalleModel();
+			$dataProvider = New ActiveDataProvider([
+				'query' => $query,
+				'pagination' => false,
+			]);
+			if ( $this->recibo > 0 ) {
+
+			}
+			$query->andFilterWhere([
+		        'id'=>$this->id,
+		    ]);
+
+		}
+
+	}
+?>
