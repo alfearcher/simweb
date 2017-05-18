70ce7eb00c278bbd6ff60782f12eccdf8cfabba7
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-05-18 21:47:24 +0000

    Actualizar

--- simwebplusteq/trunk/backend/controllers/aaee/solicitud/SolicitudViewActividadEconomicaController.php
+++ simwebplusteq/trunk/backend/controllers/aaee/solicitud/SolicitudViewActividadEconomicaController.php
@@ -68,6 +68,7 @@
 	use backend\models\aaee\declaracion\sustitutiva\SustitutivaBaseSearch;
 	use backend\models\aaee\licencia\LicenciaSolicitudSearch;
 	use backend\models\aaee\solvencia\SolvenciaActividadEconomicaSearch;
+	use backend\models\aaee\desincorporar\DesincorporarActividadEconomicaSearch;
 	use backend\controllers\MenuController;
 
 	//session_start();		// Iniciando session
@@ -175,6 +176,10 @@
 				} elseif ( $this->_model->tipo_solicitud == 73 ) {
 
 					return self::actionMostarSolicitudDeclaracionEstimada();
+
+				} elseif ( $this->_model->tipo_solicitud == 86 ) {
+
+					return self::actionMostarSolicitudDesincorporarActividadEconomica();
 
 				} elseif ( $this->_model->tipo_solicitud == 'd' ) {
 
@@ -873,6 +878,45 @@
 			return false;
 		}
 
+
+
+
+		/**
+		 * Metodo particular que se encarga de buscar los datos de la solicitud particular sobre
+		 * "Desincorporacion como contribuyente de actividad economica", y de renderizar una
+		 * vista del detalle de la solicitud.
+		 * Se utiliza un parametro adicional "nivel de aprovacion", este determinara un nivel mas
+		 * determinante de la vista.
+		 * El nivel de aprobacion 3 renderizara un formulario con los datos originales de la solicitud
+		 * inhabilitados y solo permitira la edicion de los campos que no fueron cargados en dicha
+		 * solicitud, esto con la intencion de que el funcionario pueda complementar dicha informacion.
+		 * @return view retorna un vista con la informacion de la solicitud sino encuentra dicha
+		 * informacion retornara false.
+		 * ---
+		 * nivel de aprobacion 1: No aplica.
+		 * nivel de aprobacion 2: la vista no permite la edicion de los campos.
+		 * 	- Esquema de esta vista:
+		 *  	* Nombre del campo : Valor del campo
+		 * nivel de aprobacion 3: Muestra inhabilitado los datos suministrados previamente y habilita
+		 * aquellos campos que no fueron cargados inicialmente.
+		 */
+		private function actionMostarSolicitudDesincorporarActividadEconomica()
+		{
+			if ( $this->_model->nivel_aprobacion == 2 ) {
+					$modelSearch = New DesincorporarActividadEconomicaSearch($this->_model->id_contribuyente);
+					$model = $modelSearch->findSolicitudDesincorporarActividadEconomica($this->_model->nro_solicitud);
+					if ( isset($model) ) {
+						$model = $model->one();
+						return $this->render('@backend/views/aaee/desincorporar-actividad-economica/view-solicitud', [
+														'caption' => Yii::t('frontend', 'Request Nro. ' . $this->_model->nro_solicitud),
+														'model' => $model,
+
+							]);
+					}
+			}
+
+			return false;
+		}
 
 
 
