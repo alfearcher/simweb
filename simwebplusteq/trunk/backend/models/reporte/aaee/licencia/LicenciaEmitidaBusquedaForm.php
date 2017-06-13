bab866decc50ecee231539b3e548ea92d5b0688d
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-06-13 19:54:49 +0000

    Modelo del modulo de consultas de licencias emitidas

---
+++ simwebplusteq/trunk/backend/models/reporte/aaee/licencia/LicenciaEmitidaBusquedaForm.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file LicenciaEmitidaBusquedaForm.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 09-06-2017
+ *
+ *  @class LicenciaEmitidaBusquedaForm
+ *  @brief Clase Modelo del formulario para buscar las licencias emitidas.
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *  @inherits
+ *
+ */
+
+	namespace backend\models\reporte\aaee\licencia;
+
+ 	use Yii;
+	use yii\base\Model;
+	use backend\models\utilidad\fecha\RangoFechaValido;
+
+
+
+	/**
+	* Clase que gestiona la politica y validacion del formulario que se utilizara para
+	* la consulta y busqueda de las solicitudes de licencias emitidas.
+	*/
+	class LicenciaEmitidaBusquedaForm extends Model
+	{
+		public $fecha_desde;
+		public $fecha_hasta;
+		public $tipo_licencia;
+		public $estatus;
+		public $usuario;
+		public $id_contribuyente;
+		public $licencia;
+
+		const SCENARIO_FECHA = 'fecha';
+		const SCENARIO_CONTRIBUYENTE = 'contribuyente';
+		const SCENARIO_LICENCIA = 'licencia';
+		const SCENARIO_DEFAULT = 'default';
+
+		/**
+     	* @inheritdoc
+     	*/
+    	public function scenarios()
+    	{
+        	// bypass scenarios() implementation in the parent class
+        	//return Model::scenarios();
+        	return [
+        		self::SCENARIO_FECHA => [
+        			'fecha_desde',
+        			'fecha_hasta',
+        			'usuario',
+        			'tipo_licencia',
+        		],
+        		self::SCENARIO_CONTRIBUYENTE => [
+        			'id_contribuyente',
+        			'usuario',
+        			'tipo_licencia',
+        		],
+        		self::SCENARIO_LICENCIA => [
+        			'licencia',
+        			'usuario',
+        			'tipo_licencia',
+        		],
+        		self::SCENARIO_DEFAULT => [
+        			Model::scenarios()
+        		]
+
+        	];
+    	}
+
+
+
+		/**
+    	 *	Metodo que permite fijar la reglas de validacion del formulario inscripcion-accionista-form.
+    	 */
+	    public function rules()
+	    {
+	        return [
+	        	[['fecha_desde', 'fecha_hasta'],
+	        	  'required', 'on' => 'fecha',
+	        	  'message' => Yii::t('backend', '{attribute} is required')],
+	        	[['id_contribuyente'],
+	        	  'required', 'on' => 'contribuyente',
+	        	  'message' => Yii::t('backend', '{attribute} is required')],
+	        	[['licencia'],
+	        	  'required', 'on' => 'licencia',
+	        	  'message' => Yii::t('backend', '{attribute} is required')],
+	        	[['tipo_licencia', 'estatus',
+	        	  'usuario', 'licencia'],
+	        	  'safe'],
+	        	[['id_contribuyente', 'estatus'],
+	        	  'integer',
+	        	  'message' => Yii::t('backend', '{attribute} not is valid')],
+	        	['fecha_desde',
+	        	 'compare',
+	        	 'compareAttribute' => 'fecha_hasta',
+	        	 'operator' => '<=',
+	        	 'enableClientValidation' => false],
+	        	['fecha_desde' , 'validarRango'],
+	        	[['fecha_desde', 'fecha_hasta'],
+	        	  'default',
+	        	  'value' => '0000-00-00'],
+	        ];
+	    }
+
+
+
+
+	    /**
+	    * 	Lista de atributos con sus respectivas etiquetas (labels), las cuales son las que aparecen en las vistas
+	    * 	@return returna arreglo de datos con los atributoe como key y las etiquetas como valor del arreglo.
+	    */
+	    public function attributeLabels()
+	    {
+	        return [
+	        	'fecha_desde' => Yii::t('backend', 'Fecha Desde'),
+	        	'fecha_hasta' => Yii::t('backend', 'Fecha Hasta'),
+	        	'tipo_licencia' => Yii::t('backend', 'Tipo de Licencia'),
+	        	'estatus' => Yii::t('backend', 'Condicion'),
+	        	'id_contribuyente' => Yii::t('backend', 'Id Contribuyente'),
+	        	'usuario' => Yii::t('backend', 'Usuario'),
+	        	'licencia' => Yii::t('backend', 'Nro. Licencia'),
+	        ];
+	    }
+
+
+	    /***/
+	    public function validarRango()
+	    {
+	    	$validarRango = New RangoFechaValido($this->fecha_desde, $this->fecha_hasta);
+	    	if ( !$validarRango->rangoValido() ) {
+	    		$this->addError('fecha_desde', Yii::t('backend', 'Rango de fecha no es valido'));
+	    	}
+	    }
+
+
+	    /***/
+	    // public function afterValidate()
+	    // {
+	    // 	if ( $this->fecha_desde !== '' || $this->fecha_desde !== '' ) {
+	    // 		$validarRango = New RangoFechaValido($this->fecha_desde, $this->fecha_desde);
+	    // 		return $validarRango->rangoValido();
+	    // 	}
+	    // 	return false;
+	    // }
+
+	}
+?>
---
+++ simwebplusteq/trunk/backend/models/reporte/aaee/licencia/LicenciaEmitidaSearch.php
@@ -0,0 +1,229 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file LicenciaEmitidaSearch.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 09-06-2017
+ *
+ *  @class LicenciaEmitidaSearch
+ *  @brief Clase Modelo
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *  @inherits
+ *
+ */
+
+	namespace backend\models\reporte\aaee\licencia;
+
+ 	use Yii;
+	use yii\data\ActiveDataProvider;
+	use yii\data\ArrayDataProvider;
+	use backend\models\reporte\aaee\licencia\LicenciaEmitidaBusquedaForm;
+	use backend\models\aaee\historico\licencia\HistoricoLicencia;
+	use yii\helpers\ArrayHelper;
+
+
+
+	/**
+	* Clase que permite realizar la consulta a traves de los parametros indicado
+	* en el formulario de consulta de las licencias emitidas. La busqueda se realiza
+	* contra el historico de licencia, pues es alli donde se guardan las licencias
+	* aprobadas o sea emitidas.
+	*/
+	class LicenciaEmitidaSearch extends LicenciaEmitidaBusquedaForm
+	{
+
+
+
+		/**
+		 * Metodo que genera el modelo de consulta basico.
+		 * @return HistoricoLicencia
+		 */
+		public function findHistoricoLicencia()
+		{
+			return HistoricoLicencia::find()->alias('H');
+		}
+
+
+
+
+		/***/
+		public function searchLicenciaEmitida($params)
+		{
+			$query = self::findHistoricoLicencia();
+
+			$dataProvider = New ActiveDataProvider([
+				'query' => $query,
+				'pagination' => [
+        			'pageSize' => 50,
+    			],
+			]);
+			$this->load($params);
+			$query->filterWhere(['BETWEEN', 'date(H.fecha_hora)',
+								   		 	date('Y-m-d',strtotime($this->fecha_desde)),
+				    	      			 	date('Y-m-d',strtotime($this->fecha_hasta))]);
+
+			if ( strtoupper(trim($this->tipo_licencia)) == 'TODAS' ) {
+				$query->andFilterWhere(['IN', 'tipo', ['RENOVACION, NUEVA']]);
+			} else {
+				$query->andFilterWhere(['=', 'tipo', $this->tipo_licencia]);
+			}
+
+			$query->andFilterWhere('=', 'H.usuario', $this->usuario)
+			      ->andFilterWhere('=', 'H.id_contribuyente', $this->id_contribuyente);
+
+			$query->all();
+
+			return $dataProvider;
+
+		}
+
+
+
+		/**
+	     * Metodo donde se fijan los usuario autorizados para utilizar esl modulo.
+	     * @return [type] [description]
+	     */
+	    private function getListaFuncionarioAutorizado()
+	    {
+	    	return [
+	    		'adminteq',
+	    		'kperez',
+	    		'pfranco',
+	    	];
+	    }
+
+
+
+	    /**
+	     * Metodo que permite determinar si un usuario esta autorizado para utilizar el modulo.
+	     * @param  string $usuario usuario logueado
+	     * @return booleam retorna true si lo esta, false en caso conatrio.
+	     */
+	    public function estaAutorizado($usuario)
+	    {
+	    	$listaUsuarioAutorizado = self::getListaFuncionarioAutorizado();
+	    	if ( count($listaUsuarioAutorizado) > 0 ) {
+	    		foreach ( $listaUsuarioAutorizado as $key => $value ) {
+	    			if ( $value == $usuario ) {
+	    				return true;
+	    			}
+	    		}
+	    	}
+	    	return false;
+	    }
+
+
+
+	    /**
+	     * Metodo que genera un arreglo con los usuario existentes en el historico
+	     * de licencias generadas.
+	     * @return array
+	     */
+	    public function getListaUsuarioRelacionadoHistorico()
+	    {
+	    	$findModel = self::findHistoricoLicencia();
+	    	$registers = $findModel->select('usuario')
+	    					       ->distinct('usuario')
+	    					       ->orderBy([
+	    						   		'usuario' => SORT_ASC,
+	    							])
+	    					  	   ->asArray()
+	    					       ->all();
+	    	return $registers;
+	    }
+
+
+
+
+	    /**
+	     * Metodo que retorna un arreglo con los usurios determinados en la consulta
+	     * y que estan relacionado al historico de la generacion de las licencias.
+	     * @return array
+	     */
+	    public function getListaUsuarioLicencia()
+	    {
+	    	$registers = self::getListaUsuarioRelacionadoHistorico();
+	    	return ArrayHelper::map($registers, 'usuario', 'usuario');
+	    }
+
+
+
+
+	    /***/
+	    public function armarConsultaHistoricoLicenciaModel()
+	    {
+	    	$findModel = self::findHistoricoLicencia();
+	    	if ( $this->validarRango($this->fecha_desde, $this->fecha_hasta) ) {
+	    		$findModel = self::findHistoricoLicencia()->where(['BETWEEN',
+	    												       'date(H.fecha_hora)',
+	    												        date('Y-m-d', strtotime($this->fecha_desde)), date('Y-m-d', strtotime($this->fecha_hasta))]);
+
+	    	} elseif ( trim($this->id_contribuyente) !== '' ) {
+	    		$findModel = $findModel->where(['=', 'H.id_contribuyente', $this->id_contribuyente]);
+
+	    	} elseif ( trim($this->licencia) !== '' ) {
+	    		$findModel = $findModel->where(['=', 'H.licencia', $this->licencia]);
+	    	}
+
+	    	if ( trim($this->tipo_licencia) !== '' ) {
+	    		$findModel = $findModel->andWhere(['LIKE', 'H.tipo', $this->tipo_licencia]);
+	    	}
+	    	if ( trim($this->usuario) !== '' ) {
+	    		$findModel = $findModel->andWhere(['=', 'H.usuario', $this->usuario]);
+	    	}
+	    	return $findModel;
+	    }
+
+
+
+	    /***/
+	    public function findDataHistoricoLicencia()
+	    {
+	    	$findModel = self::armarConsultaHistoricoLicenciaModel();
+	    	return $registers = $findModel->asArray()->all();
+	    }
+
+
+
+	    /***/
+	    public function getDataProvider()
+	    {
+	    	$query = self::armarConsultaHistoricoLicenciaModel();
+	    	$dataProvider = New ActiveDataProvider([
+	    		'query' => $query,
+	    	]);
+	    	$query->all();
+	    	return $dataProvider;
+ 	    }
+
+	}
+
+?>
