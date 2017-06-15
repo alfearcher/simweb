bca7ee0d4368f51996a402ab9abcdb636ad0275c
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-06-15 19:05:36 +0000

    Vistas para la solicitud de desincorporacion de actividad economica

---
+++ simwebplusteq/branches/v1.0/frontend/views/aaee/desincorporar-actividad-economica/desincorporar-actividad-economica-form.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file desincorporar-actividad-economica-form.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 17-05-2017
+ *
+ *  @view desincorporar-actividad-economica-form.php
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *
+ *  @inherits
+ *
+ */
+
+ 	//use yii\web\Response;
+ 	use kartik\icons\Icon;
+ 	use yii\grid\GridView;
+	use yii\helpers\Html;
+	use yii\helpers\Url;
+	use yii\helpers\ArrayHelper;
+	use yii\widgets\ActiveForm;
+	use yii\web\View;
+	use yii\jui\DatePicker;
+	use yii\widgets\Pjax;
+	use yii\widgets\DetailView;
+	use backend\controllers\utilidad\documento\DocumentoRequisitoController;
+
+	$typeIcon = Icon::FA;
+  	$typeLong = 'fa-2x';
+
+    Icon::map($this, $typeIcon);
+
+ ?>
+
+
+ <div class="desincorporar-actividad-economica">
+ 	<?php
+
+ 		$form = ActiveForm::begin([
+ 			'id' => 'id-desincorporar-actividad-economica-form',
+ 			'method' => 'post',
+ 			//'action' => $url,
+ 			'enableClientValidation' => true,
+ 			'enableAjaxValidation' => false,
+ 			'enableClientScript' => true,
+ 		]);
+ 	?>
+
+	<?=$form->field($model, 'id_contribuyente')->hiddenInput(['value' => $findModel['id_contribuyente']])->label(false);?>
+	<!-- <?//=$form->field($model, 'usuario')->hiddenInput(['value' => $model->usuario])->label(false); ?>-->
+	<!--<?//=$form->field($model, 'fecha_hora')->hiddenInput(['value' => $model->fecha_hora])->label(false); ?> -->
+	<?=$form->field($model, 'origen')->hiddenInput(['value' => $model->origen])->label(false); ?>
+	<!--<?//=$form->field($model, 'estatus')->hiddenInput(['value' => 0])->label(false); ?> -->
+
+
+	<meta http-equiv="refresh">
+    <div class="panel panel-primary" style="width: 100%;">
+        <div class="panel-heading">
+        	<h3><?= Html::encode($caption) ?></h3>
+        </div>
+
+<!-- Cuerpo del formulario -->
+        <div class="panel-body" style="background-color: #F9F9F9;">
+        	<div class="container-fluid">
+        		<div class="col-sm-12">
+<!--  -->
+		        	<div class="row">
+						<div class="panel panel-success" style="width: 103%;margin-left: -15px;">
+							<div class="panel-heading">
+					        	<b><span><?= Html::encode(Yii::t('frontend', 'Info of Taxpayer')) ?></span></b>
+					        </div>
+					        <div class="panel-body">
+					        	<div class="row">
+<!-- Datos del Contribuyente -->
+									<div class="col-sm-4" style="margin-left: 15px;width: 90%">
+										<div class="row">
+											<div class="id-contribuyente">
+												<?= $this->render('@common/views/contribuyente/datos-contribuyente', [
+							        											'model' => $findModel,
+							        							])
+												?>
+											</div>
+										</div>
+									</div>
+<!-- Fin de datos del Contribuyente -->
+
+								</div> 		<!-- Fin de row -->
+							</div>
+						</div>
+					</div>
+
+
+					<div class="row">
+						<div class="panel panel-success" style="width: 103%;margin-left: -15px;">
+							<div class="panel-heading">
+					        	<b><span><?= Html::encode(Yii::t('frontend', 'Otros datos')) ?></span></b>
+					        </div>
+					        <div class="panel-body">
+<!-- ULTIMA DECLARACION -->
+								<div class="row" style="padding: 0px;margin: 0px;padding:0px;">
+									<div class="col-sm-2" style="width: 15%;">
+										<p><strong><?= $model->getAttributeLabel('ult_declaracion') ?></strong></p>
+									</div>
+									<div class="col-sm-2" style="width: 25%;padding: 0px;margin: 0px;">
+										<?= $form->field($model, 'ult_declaracion')->textInput([
+																				'id' => 'id-ult-declaracion',
+																				'style' => 'width:100%;',
+																 			])->label(false)
+										?>
+									</div>
+								</div>
+<!-- FIN DE ULTIMA DECLARACION -->
+
+<!-- ULTIMO PAGO -->
+								<div class="row" style="padding: 0px;margin: 0px;padding:0px;">
+									<div class="col-sm-2" style="width: 15%;">
+										<p><strong><?= $model->getAttributeLabel('ult_pago') ?></strong></p>
+									</div>
+									<div class="col-sm-2" style="width: 25%;padding: 0px;margin: 0px;">
+										<?= $form->field($model, 'ult_pago')->textInput([
+																				'id' => 'id-ult-pago',
+																				'style' => 'width:100%;',
+																 			])->label(false)
+										?>
+									</div>
+								</div>
+<!-- FIN DE ULTIMO PAGO -->
+
+							</div>
+						</div>
+					</div>
+
+				</div>  <!-- Fin de col-sm-12 -->
+			</div>  	<!-- Fin de container-fluid -->
+
+			<div class="row">
+				<div class="col-sm-2" style="width: 18%;">
+					<?= Html::submitButton(Yii::t('frontend', Yii::t('frontend', 'Create Request')),
+																		  [
+																			'id' => 'btn-create',
+																			'class' => 'btn btn-success',
+																			'style' => 'width: 100%;',
+																			'name' => 'btn-create',
+																			'value' => 1,
+																		  ])
+					?>
+				</div>
+
+				<div class="col-sm-2" style="width: 18%;margin-left: 25px;">
+					<?= Html::submitButton(Yii::t('frontend', Yii::t('frontend', 'Quit')),
+																		  [
+																			'id' => 'btn-quit',
+																			'class' => 'btn btn-danger',
+																			'value' => 1,
+																			'style' => 'width: 100%;',
+																			'name' => 'btn-quit',
+																		  ])
+					?>
+				</div>
+
+				<div class="col-sm-2" style="width: 18%;margin-left: 25px;">
+					<!-- '../../common/docs/user/ayuda.pdf'  funciona -->
+					 <?= Html::a(Yii::t('backend', 'Ayuda'), $rutaAyuda,  [
+																	'id' => 'btn-help',
+																	'class' => 'btn btn-default',
+																	'name' => 'btn-help',
+																	'target' => '_blank',
+																	'value' => 1,
+																	'style' => 'width: 100%;'
+						])?>
+				</div>
+			</div>
+
+		</div>			<!-- Fin panel-body-->
+
+	</div>	<!-- Fin de panel panel-primary -->
+
+ 	<?php ActiveForm::end(); ?>
+</div>	 <!-- Fin de inscripcion-act-econ-form -->
+
+
---
+++ simwebplusteq/branches/v1.0/frontend/views/aaee/desincorporar-actividad-economica/view-solicitud-creada.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file view-solicitud-create.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 16-05-2017
+ *
+ *  @view view-solicitud-create, vista de la solicitude creada por desincorporacion
+ *  como contribuyente de actividad economica.
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *
+ *  @inherits
+ *
+ */
+
+ 	use yii\web\Response;
+ 	use kartik\icons\Icon;
+	use yii\helpers\Html;
+	use yii\helpers\Url;
+	use yii\web\View;
+	use yii\widgets\DetailView;
+	use backend\controllers\menu\MenuController;
+
+	$typeIcon = Icon::FA;
+  	$typeLong = 'fa-2x';
+
+    Icon::map($this, $typeIcon);
+
+ ?>
+
+ <div class="view-decincorporar-aaee-creada">
+	<meta http-equiv="refresh">
+    <div class="panel panel-primary"  style="width: 100%;">
+        <div class="panel-heading">
+        	<div class="row">
+	        	<div class="col-sm-4">
+	        		<h3><?= Html::encode(Yii::t('frontend', 'Request Nro. ' . $model->nro_solicitud)) ?></h3>
+	        	</div>
+	        	<div class="col-sm-3" style="width: 30%; float:right; padding-right: 50px;">
+	        		<style type="text/css">
+						.col-sm-3 > ul > li > a:hover {
+							background-color: #337AB7;
+						}
+	    			</style>
+	        		<?= MenuController::actionMenuSecundario($opciones); ?>
+	        	</div>
+        	</div>
+        </div>
+
+<!-- Cuerpo del formulario -->
+        <div class="panel-body" style="background-color: #F9F9F9;">
+        	<div class="container-fluid">
+        		<div class="col-sm-12">
+<!-- DATOS DE LA SUCURSAL -->
+					<div class="row">
+		        		<div class="panel panel-success" style="width: 100%;">
+							<div class="panel-heading">
+					        	<span><?= Html::encode(Yii::t('backend', 'Summary')) ?></span>
+					        </div>
+					        <div class="panel-body">
+					        	<div class="row" style="padding-left: 15px; width: 100%;">
+									<?= DetailView::widget([
+											'model' => $model,
+							    			'attributes' => [
+
+							    				[
+							    					'label' => $model->getAttributeLabel('nro_solicitud'),
+							    					'value' => $model->nro_solicitud,
+							    				],
+							    				[
+								                    'label' => Yii::t('frontend', 'Request Description'),
+								                    'value' => $model->getDescripcionTipoSolicitud($model->nro_solicitud),
+
+								                ],
+							    				[
+							    					'label' => $model->getAttributeLabel('id_contribuyente'),
+							    					'value' => $model->id_contribuyente,
+							    				],
+
+							    				[
+							    					'label' => Yii::t('frontend', 'Taxpayer'),
+							    					'value' => $model->contribuyente->razon_social,
+							    				],
+
+							    				// [
+							    				// 	'label' => $model->getAttributeLabel('domicilio_fiscal_v'),
+							    				// 	'value' => $model->domicilio_fiscal_v,
+							    				// ],
+							    				// [
+							    				// 	'label' => $model->getAttributeLabel('domicilio_fiscal_new'),
+							    				// 	'value' => $model->domicilio_fiscal_new,
+							    				// ],
+							    				[
+							    					'label' => $model->getAttributeLabel('usuario'),
+							    					'value' => $model->usuario,
+							    				],
+							    				[
+							    					'label' => $model->getAttributeLabel('fecha_hora'),
+							    					'value' => $model->fecha_hora,
+							    				],
+							    				[
+							    					'label' => $model->getAttributeLabel('origen'),
+							    					'value' => $model->origen,
+							    				],
+							    				[
+							    					'label' => $model->getAttributeLabel('estatus'),
+							    					'value' => $model['estatusSolicitud']->descripcion,
+							    					//'value' => $modelSearch->getDescripcionEstatus($model->estatus),
+							    				],
+
+							    			],
+										])
+									?>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>	<!-- Fin de col-sm-12 -->
+			</div> <!-- Fin de container-fluid -->
+		</div>	<!-- Fin de panel-body -->
+	</div>	<!-- Fin de panel panel-primary -->
+</div>	 <!-- Fin de inscripcion-sucursal-create -->
