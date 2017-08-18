6cf0e1837d2c9b750d530fbbaa5d8129a4571d5f
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-08-18 17:50:50 +0000

    Actualizar

--- simwebplusteq/branches/v1.0/backend/views/buscar-general/view.php
+++ simwebplusteq/branches/v1.0/backend/views/buscar-general/view.php
@@ -12,90 +12,139 @@ $this->title = Yii::t('backend','Id. Taxpayer') . ': ' . $model->id_contribuyent
 ?>
 
 <div class="contribuyente-view">
-    <div class="container" style="width: 80%">
+    <div class="container" style="width: 85%">
         <div class="container-fluid">
             <div class="panel panel-primary">
                 <div class="panel-heading">
                         <h1><?= Html::encode($this->title) ?></h1>
                 </div>
 
-                <div class="row">
-<!-- ID CONTRIBUYENTE -->
-                <div class="panel-body" style="margin-left: 160px">
-                    <div class="row">
-                        <div class="col-md-1" style="width: 150px"><b><?= $model->getAttributeLabel('id_contribuyente') . ':'; ?></b></div>
-                        <div class="col-md-1"><?= $model["id_contribuyente"] ?></div>
-                    </div>
-                </div>
+                <div class="row" style="width: 98%;padding: 0px;margin:0px;margin-left:10px;margin-top: 10px;">
+                    <?= DetailView::widget([
+                            'model' => $model,
+                            'options' => [
+                                'class' => 'table table-striped table-bordered detail-view',
+                            ],
+                            'attributes' => [
+                                [
+                                    'label' => Yii::t('backend', 'Id'),
+                                    'value' => $model['id_contribuyente'],
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Cedula/RIF'),
+                                    'value' => ContribuyenteBase::getCedulaRifSegunID($model['id_contribuyente']),
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Contribuyente'),
+                                    'value' => ContribuyenteBase::getContribuyenteDescripcionSegunID($model['id_contribuyente']),
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Type'),
+                                    'value' => ContribuyenteBase::getTipoNaturalezaDescripcion($model['tipo_naturaleza']),
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Direccion'),
+                                    'value' => $model['domicilio_fiscal'],
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Telefonos'),
+                                    'value' => implode(' / ', ContribuyenteBase::getTelefonosSegunID($model['id_contribuyente'])),
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Fax'),
+                                    'value' => $model['fax'],
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Email'),
+                                    'value' => $model['email'],
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Usuario'),
+                                    'value' => isset($model->afiliacion->login) ? $model->afiliacion->login : '',
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Licencia'),
+                                    'value' => $model['id_sim'],
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Fecha Inicio'),
+                                    'value' => ( $model['fecha_inicio'] !== null ) ? date('d-m-Y', strtotime($model['fecha_inicio'])) : '',
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Sede Principal'),
+                                    'value' => ( ContribuyenteBase::getEsUnaSedePrincipal($model['id_contribuyente']) ) ? 'SI' : 'NO',
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Condition'),
+                                    'value' => ContribuyenteBase::getActivoInctivoDescripcion($model['inactivo']),
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Registro Mercantil'),
+                                    'value' => $model['reg_mercantil'],
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'fecha'),
+                                    'value' => date('d-m-Y', strtotime($model['fecha'])),
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Numero de Registro'),
+                                    'value' => $model['num_reg'],
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Tomo'),
+                                    'value' => $model['tomo'],
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Folio'),
+                                    'value' => $model['folio'],
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Capital'),
+                                    'value' => Yii::$app->formatter->asDecimal($model['capital'], 2),
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Horario'),
+                                    'value' => $model['horario'],
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Declara'),
+                                    'value' => ($model['no_declara']) ? 'NO' : 'SI',
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Agente Retencion'),
+                                    'value' => ($model['agente_retencion']) ? 'SI' : 'NO',
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                 [
+                                    'label' => Yii::t('backend', 'Fecha Inclusion'),
+                                    'value' => ( $model['fecha_inclusion'] !== null ) ? date('d-m-Y', strtotime($model['fecha_inclusion'])) : '',
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'C.I. Representante'),
+                                    'value' => $model['naturaleza_rep'] . '' . $model['cedula_rep'],
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
+                                [
+                                    'label' => Yii::t('backend', 'Representante'),
+                                    'value' => $model['representante'],
+                                    'visible' => ( $model['tipo_naturaleza'] == 1 ) ? true : false,
+                                ],
 
-<!-- CEDULA Y/O RIF -->
-                <div class="panel-body" style="margin-left: 160px; padding-top: 0px;">
-                    <div class="row">
-                        <div class="col-md-3" style="width: 150px"><b><?= Html::encode(Yii::t('backend', 'DNI/Rif') . ':'); ?></b></div>
-                        <div class="col-md-3"><?= ContribuyenteBase::getCedulaRifDescripcion($model['tipo_naturaleza'],
-                                                                                             $model['naturaleza'],
-                                                                                             $model['cedula'],
-                                                                                             $model['tipo']) ?></div>
-                    </div>
-                </div>
-
-<!-- CONTRIBUYENTE -->
-                <div class="panel-body" style="margin-left: 160px; padding-top: 0px;">
-                    <div class="row">
-                        <div class="col-md-4" style="width: 150px"><b><?= Html::encode(Yii::t('backend', 'Taxpayer') . ':'); ?></b></div>
-                        <div class="col-md-8"><?= ContribuyenteBase::getContribuyenteDescripcion($model['tipo_naturaleza'],
-                                                                                                 $model['razon_social'],
-                                                                                                 $model['apellidos'],
-                                                                                                 $model['nombres']) ?></div>
-                    </div>
-                </div>
-
-
-<!-- DOMICILIO FISCAL -->
-                <div class="panel-body" style="margin-left: 160px; padding-top: 0px;">
-                    <div class="row">
-                        <div class="col-md-4" style="width: 150px"><b><?= Html::encode(Yii::t('backend', 'Address') . ':'); ?></b></div>
-                        <div class="col-md-8"><?= $model['domicilio_fiscal'] ?></div>
-                    </div>
-                </div>
-
-
-<!-- NATURALEZA -->
-                <div class="panel-body" style="margin-left: 160px; padding-top: 0px;">
-                    <div class="row">
-                        <div class="col-md-1" style="width: 150px"><b><?= Yii::t('backend', 'Type') . ':'; ?></b></div>
-                        <div class="col-md-1"><?= ContribuyenteBase::getTipoNaturalezaDescripcion($model['tipo_naturaleza']) ?></div>
-                    </div>
-                </div>
-
-
-<!-- CORREO -->
-                <div class="panel-body" style="margin-left: 160px; padding-top: 0px;">
-                    <div class="row">
-                        <div class="col-md-1" style="width: 150px"><b><?= Yii::t('backend', 'Correo') . ':'; ?></b></div>
-                        <div class="col-md-1"><?= $model->email; ?></div>
-                    </div>
-                </div>
-
-
-<!-- CONDICION -->
-                <div class="panel-body" style="margin-left: 160px; padding-top: 0px;">
-                    <div class="row">
-                        <div class="col-md-1" style="width: 150px"><b><?= Yii::t('backend', 'Condicion') . ':'; ?></b></div>
-                        <div class="col-md-1"><?= ( $model->inactivo == 0 ) ? 'ACTIVO' : 'INACTIVO'; ?></div>
-                    </div>
-                </div>
-
-
-<!-- USUARIO -->
-                <div class="panel-body" style="margin-left: 160px; padding-top: 0px;">
-                    <div class="row">
-                        <div class="col-md-1" style="width: 150px"><b><?= Yii::t('backend', 'Usuario') . ':'; ?></b></div>
-                        <div class="col-md-1"><?=$model->afiliacion->login; ?></div>
-                    </div>
-                </div>
-
-
+                            ],
+                        ]);
+                    ?>
                 </div>
 
                 <div class="row" style="width: 100%;padding: 0px;;margin-bottom: 50px;padding-left: 400px;">
@@ -109,26 +158,7 @@ $this->title = Yii::t('backend','Id. Taxpayer') . ': ' . $model->id_contribuyent
                     </div>
                 </div>
 
-
-
-
             </div>  <!-- Fin de panel panel-primary -->
         </div>  <!-- Fin de container-fluid -->
     </div>  <!-- Fi de container -->
 </div>   <!-- Fin de contribuyente-view -->
-
-
-
-
-
-
-
-<!--
-    <?/*= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id_contribuyente',
-
-        ],
-    ]) */?>
--->
