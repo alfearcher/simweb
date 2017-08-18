fcad53303bdebe1162095fb103240c10b32d261b
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-08-18 23:57:24 +0000

    Actualizar

--- simwebplusteq/trunk/backend/models/recibo/pago/lote/ruta-archivo-txt.php
+++ simwebplusteq/trunk/backend/models/recibo/pago/lote/ruta-archivo-txt.php
@@ -2,13 +2,16 @@
 	// Para la ruta en linux
 	// /home/banesco/
 	// /home/bod/
+	// /home/caroni/
 	// Para windows
-	// /lote/archivo/bod/
-	// /lote/archivo/banesco/
+	// 12/lote/archivo/bod/
+	// 16/lote/archivo/banesco/
+	// 14/lote/archivo/caroni/
 	$rutaBase = dirname(__DIR__);
 	//$rutaBase = '';
 	return [
 		12 => $rutaBase . '/home/bod/',
+		14 => $rutaBase . '/home/caroni/',
 		16 => $rutaBase . '/home/banesco/',
 	];
  ?>
