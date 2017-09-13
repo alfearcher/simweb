e86438a42de78e846ac7359e202192d8ec9368e4
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-09-13 17:23:19 +0000

    Actualizar

--- simwebplusteq/branches/v1.0/backend/views/solicitud/busqueda/view-maestro-solicitud.php
+++ simwebplusteq/branches/v1.0/backend/views/solicitud/busqueda/view-maestro-solicitud.php
@@ -41,72 +41,80 @@
  *
  */
 
-	use yii\helpers\Html;
-	use yii\helpers\Url;
-	use yii\helpers\ArrayHelper;
-	use yii\widgets\ActiveForm;
-	use kartik\icons\Icon;
-	use yii\web\View;
-	use backend\controllers\menu\MenuController;
-	use yii\widgets\Pjax;
-	use yii\widgets\DetailView;
+    use yii\helpers\Html;
+    use yii\helpers\Url;
+    use yii\helpers\ArrayHelper;
+    use yii\widgets\ActiveForm;
+    use kartik\icons\Icon;
+    use yii\web\View;
+    use backend\controllers\menu\MenuController;
+    use yii\widgets\Pjax;
+    use yii\widgets\DetailView;
 
 ?>
 <div class="row" style="width: 100%;">
-	<div class="maestro-solicitud">
-		<div class="row" style="width: 100%;">
-			<?= DetailView::widget([
-				'model' => $model,
-		    	'attributes' => [
-    				[
-    					'label' => Yii::t('backend', 'Nro. Solicitud'),
-    					'value' => $model->nro_solicitud,
-    				],
-    				[
-    					'label' => Yii::t('backend', 'Usuario Creador'),
-    					'value' => $model->usuario,
-    				],
-    				[
-    					'label' => Yii::t('backend', 'Fecha'),
-    					'value' => $model->fecha_hora_creacion,
-    				],
-    				[
-	                    'label' => Yii::t('backend', 'Impuesto'),
-	                    'value' => $model->impuestos->descripcion,
+    <div class="maestro-solicitud">
+        <div class="row" style="width: 100%;">
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    [
+                        'label' => Yii::t('backend', 'Nro. Solicitud'),
+                        'value' => $model->nro_solicitud,
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Usuario Creador'),
+                        'value' => $model->usuario,
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Fecha'),
+                        'value' => $model->fecha_hora_creacion,
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'ID'),
+                        'value' => $model->id_contribuyente,
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Contribuyente'),
+                        'value' => $model->getContribuyente($model->id_contribuyente),
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Impuesto'),
+                        'value' => $model->impuestos->descripcion,
 
-	                ],
-	                [
-	                    'label' => Yii::t('backend', 'Tipo/Solicitud'),
-	                    'value' => $model->tipoSolicitud->descripcion,
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Tipo/Solicitud'),
+                        'value' => $model->tipoSolicitud->descripcion,
 
-	                ],
-	                [
-    					'label' => Yii::t('backend', 'Nivel Aprobacion'),
-    					'value' => $model->nivelAprobacion->descripcion,
-    				],
-    				[
-    					'label' => Yii::t('backend', 'Condicion'),
-    					'value' => $model->estatusSolicitud->descripcion,
-    				],
-    				[
-    					'label' => Yii::t('backend', 'Causa Negacion'),
-    					'value' => ( (int)$model->causa > 0 ) ? $model->causaNegacion->descripcion : '',
-    				],
-    				[
-    					'label' => Yii::t('backend', 'Observacion'),
-    					'value' => $model->observacion,
-    				],
-    				[
-    					'label' => Yii::t('backend', 'Funcionario Responsable'),
-    					'value' => $model->user_funcionario,
-    				],
-    				[
-    					'label' => Yii::t('backend', 'Fecha Proceso'),
-    					'value' => $model->fecha_hora_proceso,
-    				],
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Nivel Aprobacion'),
+                        'value' => $model->nivelAprobacion->descripcion,
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Condicion'),
+                        'value' => $model->estatusSolicitud->descripcion,
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Causa Negacion'),
+                        'value' => ( (int)$model->causa > 0 ) ? $model->causaNegacion->descripcion : '',
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Observacion'),
+                        'value' => $model->observacion,
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Funcionario Responsable'),
+                        'value' => $model->user_funcionario,
+                    ],
+                    [
+                        'label' => Yii::t('backend', 'Fecha Proceso'),
+                        'value' => $model->fecha_hora_proceso,
+                    ],
 
-		    	],
-			])?>
-		</div>
-	</div>
+                ],
+            ])?>
+        </div>
+    </div>
 </div>
