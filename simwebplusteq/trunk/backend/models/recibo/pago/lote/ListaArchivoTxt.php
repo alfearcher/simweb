b1e6d90dbc5c63cb1c2dad240bc44afe8016b690
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-09-26 23:06:24 +0000

    Axtualizar

--- simwebplusteq/trunk/backend/models/recibo/pago/lote/ListaArchivoTxt.php
+++ simwebplusteq/trunk/backend/models/recibo/pago/lote/ListaArchivoTxt.php
@@ -186,7 +186,13 @@
 		 */
 		public function crearListaArchivo($arregloArchivo)
 		{
-die(var_dump(scandir("/var/www/html/simwebplusteq/backend/models/recibo/pago/home")));
+//die(var_dump(scandir("/var/www/html/simwebplusteq/backend/models/recibo/pago/home")));
+$directorio = "/var/www/html/simwebplusteq/backend/models/recibo/pago/home";
+$gestor_dir = opendir($directorio);
+while (false !== ($nombre_fichero = readdir($gestor_dir))) {
+    $ficheros[] = $nombre_fichero;
+}
+die(var_dump($ficheros));
 			if ( is_dir(self::getRuta()) ) {
 				$gestor = opendir(self::getRuta());
 
