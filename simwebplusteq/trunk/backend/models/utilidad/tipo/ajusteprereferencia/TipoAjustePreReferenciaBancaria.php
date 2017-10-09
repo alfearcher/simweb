3161968907b6a3a7aac382eae87e10238ea59a52
josep <josep@a33f24ba-b25e-4947-9187-be5d4a415dca>
2017-10-09 00:18:26 +0000

    Modelo de tipo de ajuste de pre referencias

---
+++ simwebplusteq/trunk/backend/models/utilidad/tipo/ajusteprereferencia/TipoAjustePreReferenciaBancaria.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *  @copyright © by ASIS CONSULTORES 2012 - 2016
+ *  All rights reserved - SIMWebPLUS
+ */
+
+ /**
+ *
+ *  > This library is free software; you can redistribute it and/or modify it under
+ *  > the terms of the GNU Lesser Gereral Public Licence as published by the Free
+ *  > Software Foundation; either version 2 of the Licence, or (at your opinion)
+ *  > any later version.
+ *  >
+ *  > This library is distributed in the hope that it will be usefull,
+ *  > but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability
+ *  > or fitness for a particular purpose. See the GNU Lesser General Public Licence
+ *  > for more details.
+ *  >
+ *  > See [LICENSE.TXT](../../LICENSE.TXT) file for more information.
+ *
+ */
+
+ /**
+ *  @file TipoAjustePreReferenciaBancaria.php
+ *
+ *  @author Jose Rafael Perez Teran
+ *
+ *  @date 06-10-2017
+ *
+ *  @class TipoAjustePreReferenciaBancaria
+ *  @brief Clase Modelo de la entidad "tipos-ajustes-pre-referencias"
+ *
+ *
+ *  @property
+ *
+ *
+ *  @method
+ *
+ *  @inherits
+ *
+ */
+
+	namespace backend\models\utilidad\tipo\ajusteprereferencia;
+
+ 	use Yii;
+	use yii\base\Model;
+	use yii\db\ActiveRecord;
+
+
+	/**
+	* Clase principal de la entidad "tipos-ajustes-pre-referencias"
+	*/
+	class TipoAjustePreReferenciaBancaria extends ActiveRecord
+	{
+
+		/**
+		 *	Metodo que retorna el nombre de la base de datos donde se tiene la conexion actual.
+		 * 	Utiliza las propiedades y metodos de Yii2 para traer dicha informacion.
+		 * 	@return Nombre de la base de datos
+		 */
+		public static function getDb()
+		{
+			return Yii::$app->db;
+		}
+
+
+		/**
+		 * 	Metodo que retorna el nombre de la tabla que utiliza el modelo.
+		 * 	@return Nombre de la tabla del modelo.
+		 */
+		public static function tableName()
+		{
+			return 'tipos_ajustes_pre_referencias';
+		}
+
+
+	}
+
+?>
